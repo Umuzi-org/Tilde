b306fdbd0176488cbc0cad1e8345356f30430cc0
Lindokuhle Momoza <lindokuhle.momoza@gmail.com>
2020-10-06 19:30:24 +0200

    styled the login page better

--- frontend/src/components/regions/Login/Presentation.js
+++ frontend/src/components/regions/Login/Presentation.js
@@ -1,28 +1,57 @@
 import React from "react";
-
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    marginLeft: "450px",
+    marginTop: "100px",
+    width: "400px",
+    height: "200px",
+  },
+  button: {
+    marginLeft: "100px",
+    marginTop: "5px",
+  },
+});
+
 export default ({ loading, error, handleLoginWithGoogle }) => {
+  const classes = useStyles();
+
   return (
-    <React.Fragment>
-      <Typography>
-        Please log in. Use your @umuzi.org email address if you have one
-      </Typography>
-
-      <Typography>
-        Note that this site uses popups for authentication.
-      </Typography>
-
-      {error && (
+    <Card className={classes.root}>
+      <CardContent>
         <Typography>
-          ERROR: {error}. You might need to refresh this page to attempt to
-          login again
+          Please log in. Use your @umuzi.org email address if you have one
         </Typography>
-      )}
 
-      <Button onClick={handleLoginWithGoogle}>Login with Google</Button>
-    </React.Fragment>
+        <br />
+        <Typography>
+          Note that this site uses popups for authentication.
+        </Typography>
+
+        {error && (
+          <Typography>
+            ERROR: {error}. You might need to refresh this page to attempt to
+            login again
+          </Typography>
+        )}
+      </CardContent>
+      <CardActions>
+        <Button
+          className={classes.button}
+          variant="contained"
+          onClick={handleLoginWithGoogle}
+        >
+          Login with Google
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
