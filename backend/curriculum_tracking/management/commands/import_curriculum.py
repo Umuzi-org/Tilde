61b56682aecaec5d5f3e3cff760d78f0059c46a8
Andy-Nkumane <nkumaneandy@gmail.com>
2020-10-13 07:10:37 +0200

    working curriculum load

--- backend/curriculum_tracking/management/commands/import_curriculum.py
+++ backend/curriculum_tracking/management/commands/import_curriculum.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
 
 def save_curriculum_to_db(json_file):
     with open(json_file) as f:
-        data = json.load(a)
+        data = json.load(f)
 
     df_dict = {i: pd.DataFrame(data[i]) if i != 'curriculum' else pd.DataFrame(pd.Series(data[i])).transpose() for i in data}
     # dataframes present in dictionary `df_dict` (4) `content_item_orders`, `content_items`, `curriculum`, `curriculum_content_requirements`
@@ -30,41 +30,42 @@ def save_curriculum_to_db(json_file):
     for i in df_dict:
         df = df_dict[i]
 
-
         for idx, row in df.iterrows():
-            if i == 'content_item_orders'
-                content_item_orders, created = ContentItemOrder.objects.get_or_create(
-                    hard_requirement = row['hard_requirement'],
-                    post = row['post'],
-                    pre = row['pre']
-                )
-            
-            elif i == 'content_items'
-                content_items, created = ContentItem.objects.get_or_create(
-                    available_flavours = row['available_flavours'],
-                    content_type = row['content_type'],
-                    continue_from_repo = row['continue_from_repo'],
-                    link_regex = row['link_regex'],
-                    project_submission_type = row['project_submission_type'],
-                    slug = row['slug'],
-                    story_points = row['story_points'],
-                    tags = row['tags'],
-                    template_repo = row['template_repo'],
-                    title = row['title'],
-                    topic_needs_review = row['topic_needs_review'],
-                    url = row['url']
-                )
-
-            elif i == 'curriculum'
+            if i == 'curriculum':
                 curriculum, created = Curriculum.objects.get_or_create(
                     name = row['name'],
                     short_name = row['short_name']
                 )
 
-            elif i == 'curriculum_content_requirements'
-                curriculum_content_requirements, created = CurriculumContentRequirement.objects.get_or_create(
-                    content_item = row['content_item'],
-                    flavours = row['flavours'],
-                    hard_requirement = row['hard_requirement'],
-                    order = row['order']
-                )
+            # elif i == 'content_item_orders':
+            #     content_item_orders, created = ContentItemOrder.objects.get_or_create(
+            #         hard_requirement = row['hard_requirement'],
+            #         post = row['post'],
+            #         pre = row['pre']
+            #     )
+            
+            # elif i == 'content_items':
+            #     content_items, created = ContentItem.objects.get_or_create(
+            #         # available_flavours = ','.join(row['available_flavours']),
+            #         content_type = row['content_type'],
+            #         continue_from_repo = find_url_index(row['continue_from_repo']),
+            #         link_regex = row['link_regex'],
+            #         project_submission_type = row['project_submission_type'],
+            #         slug = row['slug'],
+            #         story_points = row['story_points'],
+            #         # tags = ','.join(row['tags']),
+            #         template_repo = row['template_repo'],
+            #         title = row['title'],
+            #         topic_needs_review = row['topic_needs_review'],
+            #         url = row['url']
+            #     )
+
+
+
+            # elif i == 'curriculum_content_requirements':
+            #     curriculum_content_requirements, created = CurriculumContentRequirement.objects.get_or_create(
+            #         content_item = row['content_item'],
+            #         flavours = row['flavours'],
+            #         hard_requirement = row['hard_requirement'],
+            #         order = row['order']
+            #     )
