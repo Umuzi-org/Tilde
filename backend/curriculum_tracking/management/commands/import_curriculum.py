b7ba7879123d0dc5a77d1f2aa54864f1d9f49e36
Andy-Nkumane <nkumaneandy@gmail.com>
2020-10-19 18:18:37 +0200

    refactored find_url_index function to get_content_item_from_url

--- backend/curriculum_tracking/management/commands/import_curriculum.py
+++ backend/curriculum_tracking/management/commands/import_curriculum.py
@@ -25,20 +25,16 @@ def save_curriculum_to_db(json_file):
     create_curriculum(data['curriculum'])
     create_curriculum_content_requirements(data['curriculum_content_requirements'])
 
-def find_url_index(link, model):
-    if link == None:
-        return 
-    else:
-        for index, value in enumerate(model.objects.all()):
-            if value.url == link:
-                return model.objects.filter(url=link).first()
+def get_content_item_from_url(link):
+    if link:
+        return ContentItem.objects.get(url=link)
 
 def create_content_items(data):
     for content in data:
         content_items, created = ContentItem.objects.get_or_create(
             # available_flavours = ','.join(content['available_flavours']),
             content_type = content['content_type'],
-            continue_from_repo = find_url_index(content['continue_from_repo'], ContentItem),
+            continue_from_repo = get_content_item_from_url(content['continue_from_repo']),
             link_regex = content['link_regex'],
             project_submission_type = content['project_submission_type'],
             slug = content['slug'],
@@ -55,8 +51,8 @@ def create_content_item_orders(data):
     for order in data:
         content_item_orders, created = ContentItemOrder.objects.get_or_create(
             hard_requirement = order['hard_requirement'],
-            post = find_url_index(order['post'], ContentItem),
-            pre = find_url_index(order['pre'], ContentItem)
+            post = get_content_item_from_url(order['post']),
+            pre = get_content_item_from_url(order['pre'])
         )
 
 
@@ -69,7 +65,7 @@ def create_curriculum(data):
 def create_curriculum_content_requirements(data):
     for requirement in data:
         curriculum_content_requirements, created = CurriculumContentRequirement.objects.get_or_create(
-            content_item = find_url_index(requirement['content_item'], ContentItem),
+            content_item = get_content_item_from_url(requirement['content_item']),
             # flavours = requirement['flavours'],
             curriculum = Curriculum.objects.filter(short_name='tilde intro student').first(),
             hard_requirement = requirement['hard_requirement'],
