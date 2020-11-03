3299902b76c307c29ffca2b485d3cc58b925f3c7
Sheena O'Connell <sheena.oconnell@gmail.com>
2020-11-03 06:35:28 +0200

    added better image tagging

--- k8s-deploy/build-and-push-image.sh
+++ k8s-deploy/build-and-push-image.sh
@@ -2,5 +2,8 @@
 
 cd ../backend 
 
-docker build -t gcr.io/umuzi-prod/tilde-backend .
-docker push gcr.io/umuzi-prod/tilde-backend
+GIT_COMMIT="$(git show --format="%h" --no-patch)"
+
+docker build -t gcr.io/umuzi-prod/tilde-backend:$GIT_COMMIT -t gcr.io/umuzi-prod/tilde-backend:latest .
+docker push gcr.io/umuzi-prod/tilde-backend:$GIT_COMMIT
+docker push gcr.io/umuzi-prod/tilde-backend:latest
--- k8s-deploy/upload-secrets.sh
+++ k8s-deploy/upload-secrets.sh
@@ -2,7 +2,6 @@
 
 # Cloud SQL secrets
 kubectl create secret generic cloudsql-oauth-credentials --from-file=credentials.json=$GOOGLE_APPLICATION_CREDENTIALS 
-
 kubectl create secret generic cloudsql --from-literal=TILDE_SQL_USER=$TILDE_SQL_USER --from-literal=TILDE_SQL_PASS=$TILDE_SQL_PASS
 
 # Misc Django secrets 
@@ -12,5 +11,4 @@ kubectl create secret generic miscbackend --from-literal=PROD_SECRET_KEY=$PROD_S
 kubectl create secret generic gitreal --from-literal=GITHUB_CLIENT_ID=$GITHUB_CLIENT_ID --from-literal=GITHUB_CLIENT_SECRET=$GITHUB_CLIENT_SECRET
 
 # LOGIN WITH GOOGLE
-
 kubectl create secret generic google-oauth-onetime-creds --from-file=google-oauth-onetime-creds.json=$GOOGLE_OAUTH_ONE_TIME_CLIENT_SECRET_FILE 
