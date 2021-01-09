df3c9899a57d522dd0a0607921134c5ae5996ff4
Sheena O'Connell <sheena.oconnell@gmail.com>
2021-01-09 07:31:47 +0200

    added script to help with migration wrangling

---
+++ backend/restore-migrations-to-master.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+
+rm -rf core/migrations
+rm -rf attendance/migrations
+rm -rf curriculum_tracking/migrations
+rm -rf git_real/migrations
+rm -rf social_auth/migrations
+# rm -rf dev_helpers/migrations
+
+git checkout master -- core/migrations
+git checkout master -- attendance/migrations
+git checkout master -- curriculum_tracking/migrations
+git checkout master -- git_real/migrations
+git checkout master -- social_auth/migrations
+# git checkout master -- dev_helpers/migrations
