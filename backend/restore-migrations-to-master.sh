f2fe1e670f8fb2a4366797c641289da956eb1f4d
Sheena O'Connell <sheena.oconnell@gmail.com>
2021-02-01 05:02:06 +0200

    git stripts

--- backend/restore-migrations-to-master.sh
+++ backend/restore-migrations-to-master.sh
@@ -13,3 +13,21 @@ git checkout master -- curriculum_tracking/migrations
 git checkout master -- git_real/migrations
 git checkout master -- social_auth/migrations
 # git checkout master -- dev_helpers/migrations
+
+
+
+
+
+# rm -rf core/migrations
+# rm -rf attendance/migrations
+# rm -rf curriculum_tracking/migrations
+# rm -rf git_real/migrations
+# rm -rf social_auth/migrations
+# # rm -rf dev_helpers/migrations
+
+# git checkout develop -- core/migrations
+# git checkout develop -- attendance/migrations
+# git checkout develop -- curriculum_tracking/migrations
+# git checkout develop -- git_real/migrations
+# git checkout develop -- social_auth/migrations
+# # git checkout master -- dev_helpers/migrations
