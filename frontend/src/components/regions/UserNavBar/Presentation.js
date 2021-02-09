f75e7c7f8eff50d6ecbedc450e91f16db49b558b
Sheena O'Connell <sheena.oconnell@gmail.com>
2021-02-09 15:33:00 +0200

    added github icon to navbar

--- frontend/src/components/regions/UserNavBar/Presentation.js
+++ frontend/src/components/regions/UserNavBar/Presentation.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import { Typography, Toolbar } from "@material-ui/core";
+import { Typography, Toolbar, Tooltip } from "@material-ui/core";
 
 import LinkToUserBoard from "../../widgets/LinkToUserBoard";
 import LinkToUserActions from "../../widgets/LinkToUserActions";
-
+import GitHubIcon from "@material-ui/icons/GitHub";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -14,6 +14,9 @@ const useStyles = makeStyles((theme) => ({
     border: "1px solid grey",
   },
   button: { marginRight: theme.spacing(1) },
+  gitHubLink: {
+    margin: theme.spacing(1),
+  },
 }));
 
 export default ({ user, userId, userBoardSelected, UserActionsSelected }) => {
@@ -22,7 +25,20 @@ export default ({ user, userId, userBoardSelected, UserActionsSelected }) => {
   return (
     <React.Fragment>
       <Toolbar variant="dense" className={classes.toolbar}>
-        {user && <Typography>Viewing: {user.email}</Typography>}
+        {user && <Typography>{user.email}</Typography>}
+
+        {user && user.githubName && (
+          <a
+            className={classes.gitHubLink}
+            href={`https://github.com/${user.githubName}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Tooltip title={user.githubName}>
+              <GitHubIcon />
+            </Tooltip>
+          </a>
+        )}
         <div className={classes.grow} />
         <div className={classes.button}>
           <LinkToUserBoard userId={userId} selected={userBoardSelected} />
--- frontend/src/stories/UserNavBar.stories.js
+++ frontend/src/stories/UserNavBar.stories.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import UserNavBar from "../components/regions/UserNavBar/Presentation"
+import UserNavBar from "../components/regions/UserNavBar/Presentation";
 import { BrowserRouter as Router } from "react-router-dom";
 
-
 export default {
-    title: 'Tilde/UserNavBar',
-    component: UserNavBar,
-    // argTypes: {
-    //   backgroundColor: { control: 'color' },
-    // },
-  };
+  title: "Tilde/UserNavBar",
+  component: UserNavBar,
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+};
 
-
-const Template = (args) => ( <Router><UserNavBar {...args} /></Router>);
-
+const Template = (args) => (
+  <Router>
+    <UserNavBar {...args} />
+  </Router>
+);
 
 export const Primary = Template.bind({});
 Primary.args = {
-    userId: 1,
-    userBoardSelected: true,
-    userStatsSelected: false
+  userId: 1,
+  userBoardSelected: true,
+  userStatsSelected: false,
+  user: {
+    email: "sheena.oconnell@umuzi.org",
+    githubName: "sheenarbw",
+  },
 };
