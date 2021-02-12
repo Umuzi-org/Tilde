f88f002fa229252dc553dc8e3b7beb89b5673eb6
Sheena O'Connell <sheena.oconnell@gmail.com>
2021-02-12 12:09:18 +0200

    can use dramatiq for normal access to models

--- backend/long_running_request_actors.py
+++ backend/long_running_request_actors.py
@@ -3,11 +3,19 @@ These are dramatiq actors
 
 See https://dramatiq.io/guide.html
 """
+
 import dramatiq
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()
 
 
 @dramatiq.actor
 def test_long_running_request():
-    import os
 
-    os.system("python manage.py active_user_count")
+    from core.models import User
+
+    count = User.objects.filter(active=True).count()
+    print(f"Active users: {count}")
