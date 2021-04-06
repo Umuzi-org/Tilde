bcdc63c955f1ed2529b8f76eed3edaf300282dae
Sheena <sheena.oconnell@gmail.com>
2021-04-06 16:01:46 +0200

    backend/curriculum_tracking/management/commands/reinvite_repo_collab_rand_protect_master.py

--- backend/curriculum_tracking/management/commands/reinvite_repo_collab_rand_protect_master.py
+++ backend/curriculum_tracking/management/commands/reinvite_repo_collab_rand_protect_master.py
@@ -1,31 +1,37 @@
 from django.core.management.base import BaseCommand
 
-from git_real.constants import PERSONAL_GITHUB_NAME, ORGANISATION, PERSONAL_GITHUB_NAME
+from git_real.constants import GITHUB_BOT_USERNAME, ORGANISATION
 
-from git_real.helpers import add_collaborator, protect_master
+# from git_real.helpers import add_collaborator, protect_master
 from curriculum_tracking.models import RecruitProject
 from social_auth.github_api import Api
+from long_running_request_actors import add_collaborators_and_protect_master
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        api = Api(PERSONAL_GITHUB_NAME)
+        api = Api(GITHUB_BOT_USERNAME)
+        for project in RecruitProject.objects.filter(
+            repository__owner=ORGANISATION, recruit_users__active__in=[True]
+        ):
+            print(project)
+            add_collaborators_and_protect_master(project_id=project.id, api=api)
 
-        for proj in RecruitProject.objects.filter(repository__owner=ORGANISATION):
-            repo = proj.repository
-            print(repo)
+        # for proj in RecruitProject.objects.filter(repository__owner=ORGANISATION):
+        #     repo = proj.repository
+        #     print(repo)
 
-            protect_master(api, repo.full_name)
-            for user in proj.reviewer_users.all():
-                if user.active:
-                    print(user.social_profile.github_name)
-                    add_collaborator(
-                        api, repo.full_name, user.social_profile.github_name
-                    )
-            for user in proj.recruit_users.all():
-                if user.active:
-                    print(user.social_profile.github_name)
-                    add_collaborator(
-                        api, repo.full_name, user.social_profile.github_name
-                    )
-        print()
+        #     protect_master(api, repo.full_name)
+        #     for user in proj.reviewer_users.all():
+        #         if user.active:
+        #             print(user.social_profile.github_name)
+        #             add_collaborator(
+        #                 api, repo.full_name, user.social_profile.github_name
+        #             )
+        #     for user in proj.recruit_users.all():
+        #         if user.active:
+        #             print(user.social_profile.github_name)
+        #             add_collaborator(
+        #                 api, repo.full_name, user.social_profile.github_name
+        #             )
+        # print()
