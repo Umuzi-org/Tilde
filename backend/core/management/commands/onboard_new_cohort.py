3f37c9b64ee00990ee8d9eb97c1e83a393bb18b2
Sheena <sheena.oconnell@gmail.com>
2021-04-22 11:20:28 +0200

    fix onboarding script

--- backend/core/management/commands/onboard_new_cohort.py
+++ backend/core/management/commands/onboard_new_cohort.py
@@ -26,6 +26,12 @@ COURSES_BY_DEPARTMENT = {
         "Web Development - part 1",
         "Web Development - part 2",
     ],
+    "web dev provisionally accepted": [
+        "Web development boot camp",
+        "Post Bootcamp Soft Skills",
+        "Web Development - part 1",
+        "Web Development - part 2",
+    ],
     "web dev alumni": [
         "Alumni Web developement Bootcamp",
         "Post Bootcamp Soft Skills",
@@ -43,10 +49,22 @@ COURSES_BY_DEPARTMENT = {
         "Post Bootcamp Soft Skills",
         "Data Engineering - part 2",
     ],
+    "data eng provisionally accepted": [
+        "Data Engineering boot camp",
+        "Post Bootcamp Soft Skills",
+        "Data Engineering - part 1",
+        "Data Engineering - part 2",
+    ],
     "java": [
         "Java boot camp",
         "Post Bootcamp Soft Skills",
         "NCIT - Java",
+        "Java Systems Development - part 1",
+        "Java Systems Development - part 2",
+    ],
+    "java provisionally accepted": [
+        "Java boot camp",
+        "Post Bootcamp Soft Skills",
         "Java Systems Development - part 1",
         "Java Systems Development - part 2",
     ],
@@ -63,6 +81,11 @@ COURSES_BY_DEPARTMENT = {
         "Data Science boot camp",
         "Post Bootcamp Soft Skills",
         "NCIT - Python",
+        "Data Science",
+    ],
+    "data sci  provisionally accepted": [
+        "Data Science boot camp",
+        "Post Bootcamp Soft Skills",
         "Data Science",
     ],
 }
