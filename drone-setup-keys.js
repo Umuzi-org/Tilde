937bb4d73ed422e200f1008b9496601ad6bbb5ee
Sheena <sheena.oconnell@gmail.com>
2021-05-18 12:49:34 +0200

    cicd attempt

--- drone-setup-keys.js
+++ drone-setup-keys.js
@@ -2,11 +2,21 @@
 
 const fs = require('fs');
 
+const cleanPrivateKey = () => {
+
+    let key = process.env.GCLOUD_PRIVATE_KEY;
+    while (key.indexOf('\\n') !== -1){
+        key = key.replace('\\n','\n')
+    } 
+    return key
+
+}
+
 const data = {
     "type": "service_account",
     "project_id": "umuzi-prod",
     "private_key_id": process.env.GCLOUD_PRIVATE_KEY_ID,
-    "private_key": process.env.GCLOUD_PRIVATE_KEY,
+    "private_key": cleanPrivateKey(),
     "client_email": "drone-io-deploy@umuzi-prod.iam.gserviceaccount.com",
     "client_id": "117897706003738993064",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
