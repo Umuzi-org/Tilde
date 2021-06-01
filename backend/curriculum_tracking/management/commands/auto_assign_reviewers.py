97574505a2af45473497b054c814359b95792663
Sheena <sheena.oconnell@gmail.com>
2021-06-01 13:22:19 +0200

    get_possible_reviewers works

--- backend/curriculum_tracking/management/commands/auto_assign_reviewers.py
+++ backend/curriculum_tracking/management/commands/auto_assign_reviewers.py
@@ -2,7 +2,7 @@ from typing import Iterable
 from core.models import User
 from curriculum_tracking.models import AgileCard, ContentItem, RecruitProject
 from django.core.management.base import BaseCommand
-from django.db.models import Count
+from django.db.models import Count, OuterRef
 
 REQUIRED_REVIEWERS_PER_CARD = 2
 SKIP_CARD_TAGS = ["ncit"]
@@ -25,17 +25,42 @@ def get_cards_needing_reviewers() -> Iterable[AgileCard]:
         yield card
 
 
+def filter_by_flavour_match(query, flavours):
+    pks = [flavour.id for flavour in flavours]
+    query = query.annotate(flavour_count=Count("flavours")).filter(
+        flavour_count=len(pks)
+    )
+    for pk in pks:
+        query = query.filter(flavours__pk=pk)
+    return query
+
+
 def get_possible_reviewers(card):
     """
     find active users who are currently competent for that card and flavour
-    order by allocated review duties
+    order by allocated review duties like so:
+
+    count the number of cards with the same content_item and flavours where a user is the reviewer. Order = count ascending
     """
+
     project = card.recruit_project
 
-    # User.objects.filter(active=True)
-    RecruitProject.objects.filter(content_item=project.content_item).filter(
-        complete_time
+    projects: RecruitProject = RecruitProject.objects.filter(
+        content_item=project.content_item
     ).filter(recruit_users__active__in=[True])
+    projects = filter_by_flavour_match(projects, project.flavours.all())
+
+    complete_projects = projects.filter(complete_time__isnull=False)
+
+    competent_users = User.objects.filter(
+        recruit_projects__in=complete_projects
+    ).exclude(projects_to_review__in=[project])
+
+    competent_users = competent_users.annotate(duty_count=Count("projects_to_review"))
+    competent_users = competent_users.order_by("duty_count")
+    for user in competent_users:
+        # print(f"{user} - {user.projects_to_review.count()}")
+        yield user
 
 
 class Command(BaseCommand):
@@ -50,4 +75,5 @@ class Command(BaseCommand):
             )
             possible_reviewers = get_possible_reviewers(card)
             for user in possible_reviewers[:number_of_reviewers_to_add]:
-                card.add_collaborator(user=user, add_as_project_reviewer=True)
+                print(f"Add collaborator:\n\tcard = {card}\n\tnew reviewer = {user}")
+                # card.add_collaborator(user=user, add_as_project_reviewer=True)
--- backend/curriculum_tracking/tests/test_command_auto_assign_reviews.py
+++ backend/curriculum_tracking/tests/test_command_auto_assign_reviews.py
@@ -1,12 +1,21 @@
+from django.utils import timezone
 from curriculum_tracking.models import AgileCard, ContentItem
 from curriculum_tracking.management.commands.auto_assign_reviewers import (
     REQUIRED_REVIEWERS_PER_CARD,
     get_cards_needing_reviewers,
+    get_possible_reviewers,
 )
 from core.tests.factories import UserFactory
-from curriculum_tracking.tests.factories import AgileCardFactory, ContentItemFactory
+from curriculum_tracking.tests.factories import (
+    AgileCardFactory,
+    ContentItemFactory,
+    RecruitProjectFactory,
+)
 from django.test import TestCase
 from typing import List
+
+JAVASCRIPT = "js"
+TYPESCRIPT = "ts"
 
 
 class get_cards_needing_reviewers_Tests(TestCase):
@@ -48,3 +57,117 @@ class get_cards_needing_reviewers_Tests(TestCase):
 
         result = get_cards_needing_reviewers()
         self.assertEqual(list(result), [])
+
+
+class get_possible_reviewers_Tests(TestCase):
+    def test_that_only_competent_people_get_returned(self):
+
+        competent_project = RecruitProjectFactory(
+            complete_time=timezone.now(), flavours=[JAVASCRIPT]
+        )
+        content_item = competent_project.content_item
+        nyc_project = RecruitProjectFactory(
+            complete_time=None, flavours=[JAVASCRIPT], content_item=content_item
+        )
+        wrong_flavour_project = RecruitProjectFactory(
+            complete_time=timezone.now(),
+            flavours=[TYPESCRIPT],
+            content_item=content_item,
+        )
+
+        card = AgileCardFactory(
+            recruit_project=RecruitProjectFactory(
+                complete_time=None, flavours=[JAVASCRIPT], content_item=content_item
+            )
+        )
+        result = list(get_possible_reviewers(card))
+        self.assertEqual(result, [competent_project.recruit_users.first()])
+
+    def test_that_it_works_with_flavourless_projects(self):
+        competent_project = RecruitProjectFactory(
+            complete_time=timezone.now(), flavours=[]
+        )
+        content_item = competent_project.content_item
+        nyc_project = RecruitProjectFactory(
+            complete_time=None, flavours=[], content_item=content_item
+        )
+        wrong_flavour_project = RecruitProjectFactory(
+            complete_time=timezone.now(),
+            flavours=[TYPESCRIPT],
+            content_item=content_item,
+        )
+
+        card = AgileCardFactory(
+            recruit_project=RecruitProjectFactory(
+                complete_time=None, flavours=[], content_item=content_item
+            )
+        )
+        result = list(get_possible_reviewers(card))
+        self.assertEqual(result, [competent_project.recruit_users.first()])
+
+    def test_that_reviewers_returned_in_order(self):
+        content_item = ContentItemFactory()
+        competent_projects = [
+            RecruitProjectFactory(
+                complete_time=timezone.now(), flavours=[], content_item=content_item
+            )
+            for i in range(3)
+        ]
+
+        for n, project in enumerate(competent_projects):
+            reviews_to_add = 5 - n
+            for i in range(reviews_to_add):
+                nyc_card = AgileCardFactory(
+                    recruit_project=RecruitProjectFactory(
+                        complete_time=None, flavours=[], content_item=content_item
+                    )
+                )
+                project_user = project.recruit_users.first()
+                nyc_card.reviewers.add(project_user)
+                nyc_card.recruit_project.reviewer_users.add(project_user)
+
+        card = AgileCardFactory(
+            recruit_project=RecruitProjectFactory(
+                complete_time=None, flavours=[], content_item=content_item
+            )
+        )
+        expected_result = [
+            project.recruit_users.first() for project in competent_projects[::-1]
+        ]
+        assert expected_result
+        result = list(get_possible_reviewers(card))
+        self.assertEqual(
+            result,
+            expected_result,
+        )
+
+        # add some more duties to first person and make sure order is still correct
+        project_user = expected_result[0]
+        for i in range(5):
+
+            nyc_card = AgileCardFactory(
+                recruit_project=RecruitProjectFactory(
+                    complete_time=None, flavours=[], content_item=content_item
+                )
+            )
+            nyc_card.reviewers.add(project_user)
+            nyc_card.recruit_project.reviewer_users.add(project_user)
+
+        expected_result = expected_result[1:] + [expected_result[0]]
+        result = list(get_possible_reviewers(card))
+        self.assertEqual(
+            result,
+            expected_result,
+        )
+
+        # test_that_if_a_user_is_already_a_reviewer_they_are_skipped(self)
+
+        card.reviewers.add(expected_result[0])
+        card.recruit_project.reviewer_users.add(expected_result[0])
+        card.reviewers.add(expected_result[-1])
+        card.recruit_project.reviewer_users.add(expected_result[-1])
+        result = list(get_possible_reviewers(card))
+        self.assertEqual(
+            result,
+            expected_result[1:-1],
+        )
