f8a61cdac85144df36e7876c2b6152e6ceab1092
Babalwa01 <babalwambolekwa93@gmail.com>
2021-06-09 15:34:30 +0200

    Added Material UI and Chips for flavours

--- frontend/src/components/regions/UserDetails/ReviewTrustTable/Presentation.js
+++ frontend/src/components/regions/UserDetails/ReviewTrustTable/Presentation.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import Chip from '@material-ui/core/Chip';
+
+const columns = [
+    { id: 'title', label: 'Title', minWidth: 170 },
+    { id: 'flavours', label: 'Flavours', minWidth: 100 }
+];
+
+const useStyles = makeStyles({
+
+    flavourChip: {
+        marginRight: '0.5rem',
+    },
+});
 
 const ReviewTrustTable = (props) => {
 
     const { title, flavours } = props;
+    const classes = useStyles();
 
-    
     return (
-        <table className='table'>
-            <tr>
-                <th className='titleColumn'>Title</th>
-                <th className='flavoursColumn'>Flavours</th>
-            </tr>
-            <tr>
-                <td>{title}</td>
-                <td>
-                    <ul>
-                        {flavours.split(",").map((flavour) => <li>{flavour}</li>)}
-                    </ul>
-                </td>
-            </tr>
-        </table>
+        <Table className={classes.table} stickyHeader aria-label="sticky table">
+            <TableHead>
+                <TableRow>
+                    {columns.map((column) => (
+                        <TableCell
+                            key={column.id}
+                            align={column.align}
+                            style={{ minWidth: column.minWidth }}
+                        >
+                            {column.label}
+                        </TableCell>
+                    ))}
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                <TableRow>
+                    <TableCell>{title}</TableCell>
+                    <TableCell>{flavours.split(",").map((flavourChip) => (
+                        <Chip className={classes.flavourChip} label={flavourChip} />
+                    ))}</TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
     )
 }
 
