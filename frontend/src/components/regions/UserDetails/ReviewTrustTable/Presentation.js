adc3725455e59b12e8dda705593fd31b4bad4aa5
Babalwa01 <babalwambolekwa93@gmail.com>
2021-06-09 15:44:08 +0200

    Material UI table and flavour chips added

--- frontend/src/components/regions/UserDetails/ReviewTrustTable/Presentation.js
+++ frontend/src/components/regions/UserDetails/ReviewTrustTable/Presentation.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Chip from '@material-ui/core/Chip';
 
@@ -29,7 +26,7 @@ const ReviewTrustTable = (props) => {
     const classes = useStyles();
 
     return (
-        <Table className={classes.table} stickyHeader aria-label="sticky table">
+        <Table stickyHeader aria-label="sticky table">
             <TableHead>
                 <TableRow>
                     {columns.map((column) => (
