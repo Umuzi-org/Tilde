c484a05a12187a2526129ab4cd131fec112caec2
Babalwa01 <babalwambolekwa93@gmail.com>
2021-06-14 21:52:55 +0200

    added story for AgileActions component

--- frontend/src/stories/AgileCardActions.stories.js
+++ frontend/src/stories/AgileCardActions.stories.js
@@ -1,9 +1,14 @@
 import React from "react";
 import CardActions from "../components/regions/AgileBoard/AgileCard/AgileCardActions/Presentation";
+import card from "./fixtures/agileCardAction.json";
 
 export default {
     title: "Tilde/AgileCardActions",
     component: CardActions
 }
 
-export const Primary = () => <CardActions />
+const Template = (args) => <CardActions {...args} />;
+export const Primary = Template.bind({});
+Primary.args = {
+    card,
+}
