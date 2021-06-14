232b0f629ba1ff961ccb3c1f35806c8a33332f46
Babalwa01 <babalwambolekwa93@gmail.com>
2021-06-14 16:38:08 +0200

    story initialized

---
+++ frontend/src/stories/AgileCardActions.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CardActions from "../components/regions/AgileBoard/AgileCard/AgileCardActions/Presentation";
+
+export default {
+    title: "Tilde/AgileCardActions",
+    component: CardActions
+}
+
+export const Primary = () => <CardActions />
