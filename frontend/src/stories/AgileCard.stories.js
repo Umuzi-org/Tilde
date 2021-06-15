7502d3aa5e3eae39dad1741fbe4810ccb6a94c1c
Babalwa01 <babalwambolekwa93@gmail.com>
2021-06-15 16:46:58 +0200

    user data file added

--- frontend/src/stories/AgileCard.stories.js
+++ frontend/src/stories/AgileCard.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../components/regions/AgileBoard/AgileCard/Presentation";
 import card from "./fixtures/agileCard.json";
+import user from "./fixtures/user.json";
 
 export default {
     title: "Tilde/AgileCard",
@@ -11,4 +12,5 @@ const Template = (args) => <Card {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
     card,
+    authUser,
 }
