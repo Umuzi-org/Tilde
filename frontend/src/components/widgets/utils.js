521c93cb2543d99225dc67a8d7abc3a9cc85e5e8
Sheena <sheena.oconnell@gmail.com>
2021-06-30 09:43:15 +0200

    hitfix/allow null values when calculating time ago

--- frontend/src/components/widgets/utils.js
+++ frontend/src/components/widgets/utils.js
@@ -1,11 +1,12 @@
-import TimeAgo from 'javascript-time-ago';
-import en from 'javascript-time-ago/locale/en';
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
 TimeAgo.addDefaultLocale(en);
 
 export const getAgeString = (dateString) => {
-    const date = new Date(dateString);
-    const timeAgo = new TimeAgo('en-US');
-    /*Age calculated and formatted according to Javascript TimeAgo library. 
+  if (!dateString) return "";
+  const date = new Date(dateString);
+  const timeAgo = new TimeAgo("en-US");
+  /*Age calculated and formatted according to Javascript TimeAgo library.
       Docs link: https://www.npmjs.com/package/javascript-time-ago */
-    return timeAgo.format(date);
-}
+  return timeAgo.format(date);
+};
