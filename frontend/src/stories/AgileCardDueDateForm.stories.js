57b4f477d021483e9df062fde7f7c28e7a46e9e7
Babalwa01 <babalwambolekwa93@gmail.com>
2021-07-19 17:28:07 +0200

    Agile card due date form story started

---
+++ frontend/src/stories/AgileCardDueDateForm.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import AgileCardDueDateForm from "../components/regions/AgileCardDueDateForm/Presentation";
+
+export default {
+    title: "Tilde/AgileCardDueDateForm",
+    component: AgileCardDueDateForm
+}
+
+export const Primary = () => <AgileCardDueDateForm dueDateInstance={"..."}/>
