f3727bc88ef10cc756713d7d0d2664261dbdb2e1
Sheena <sheena.oconnell@gmail.com>
2021-07-20 18:25:28 +0200

    setup config model migration helpers

--- backend/restore-migrations-to-master.sh
+++ backend/restore-migrations-to-master.sh
@@ -1,10 +1,11 @@
-#!/bin/sh 
+#!/bin/sh
 
 rm -rf core/migrations
 rm -rf attendance/migrations
 rm -rf curriculum_tracking/migrations
 rm -rf git_real/migrations
 rm -rf social_auth/migrations
+rm -rf config/migrations
 # rm -rf dev_helpers/migrations
 
 git checkout master -- core/migrations
@@ -12,6 +13,7 @@ git checkout master -- attendance/migrations
 git checkout master -- curriculum_tracking/migrations
 git checkout master -- git_real/migrations
 git checkout master -- social_auth/migrations
+git checkout master -- config/migrations
 # git checkout master -- dev_helpers/migrations
 
 
