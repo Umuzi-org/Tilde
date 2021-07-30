55b32c331382ea052a9bc629ef5cf306b71b5e25
Babalwa01 <babalwambolekwa93@gmail.com>
2021-07-30 13:23:52 +0200

    DueTimeFormModal redux started

--- frontend/src/components/regions/DueTimeFormModal/redux/index.js
+++ frontend/src/components/regions/DueTimeFormModal/redux/index.js
@@ -1,5 +1,5 @@
 import dueTimeFormModalReducer from "./reducers";
 export { default as dueTimeFormModalOperations } from "./operations";
 export { default as dueTimeFormModalTypes } from "./types";
-// export { default as dueTimeFormModalSagas } from "./sagas";
+export { default as dueTimeFormModalSagas } from "./sagas";
 export default dueTimeFormModalReducer;
