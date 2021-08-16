de1c3b113353484d5d65abb10497252e35006c40
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-08-17 01:14:25 +0200

    working fix

---
+++ frontend/src/stories/ReviewsTable.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReviewsTable from "../components/regions/CardDetailsModal/ReviewsTable";
+
+export default {
+    title: "Tilde/ReviewsTable",
+    component: ReviewsTable,
+}
+
+export const Primary = () => <ReviewsTable />
