8a34a955c16bf2f9ec73b81ac61c3da3c9eff72f
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-08-16 17:52:56 +0200

    set up

---
+++ frontend/src/stories/CardDetailsModalSbonelo.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReviewsTable from "../components/regions/CardDetailsModalSbonelo/Presentation";
+
+// import reviewObject agileCard "./fixtures/.json";
+// import repoProjectCard from "./fixtures/repoProjectCard.json";
+
+export default {
+    title: "Tilde/ReviewsTable",
+    component: ReviewsTable,
+}
+
+export const Primary = () => <ReviewsTable />
+
+// const Template = (args) => <ReviewDetailsModal {...args} />;
