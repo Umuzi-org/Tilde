f4531a323cca84cb13683e640bc9e557f64e1590
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-08-16 23:20:18 +0200

    mostly done - need to figure out the badges

--- frontend/src/components/regions/CardDetailsModalSbonelo/Presentation.js
+++ frontend/src/components/regions/CardDetailsModalSbonelo/Presentation.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 // import ReactDOM from "react-dom";
-import MUIDataTable from "mui-datatables";
+import MUIDataTable, {TableFilterList} from "mui-datatables";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import { CardBadges } from "../../widgets/CardBadges";
+import Chip from "@material-ui/core/Chip";
+import TagFacesIcon from '@material-ui/icons/TagFaces';
+import { makeStyles } from '@material-ui/core/styles';
 
 const ReviewsTable = () => {
   const [responsive, setResponsive] = useState("vertical");
-  const [tableBodyHeight, setTableBodyHeight] = useState("400px");
+  const [tableBodyHeight, setTableBodyHeight] = useState("100%");
   const [tableBodyMaxHeight, setTableBodyMaxHeight] = useState("");
+
+  const timestamp = Date.now();
 
   const columns = [
     {
@@ -59,35 +65,34 @@ const ReviewsTable = () => {
     print: false,
     selectableRows: false,
     customToolbarSelect: () => {},
+    // checkboxSelection,
     responsive,
     tableBodyHeight,
     tableBodyMaxHeight,
   };
 
-  const date = new Date();
-
   const data = [
     {
-      timestamp: `${date.getDate()}`,
+      timestamp: new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp),
       status: "Excellent",
       reviewer: "sheena.oconnel@umuzi.org",
       comments:
         "This has to be lOnHgf Aadf ADFH ADJKADFH ADF DAJKADFH JHADFADF JKHA KHADF KJH AJK FFHADFHJADFH KADFHJKHFJA HA DADFAKJF KJAHADFJADFH KADFHJKADFHADJKH ADFKHADF HH ADFK ADHKjhsjkHhKHkjhLHjkHHjkjklh ajksh h as jasghj hasg gh gjklh",
     },
     {
-      timestamp: `${date.getDate()}`,
+      timestamp: new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp),
       status: "Competent",
       reviewer: "sbonelo.mkhize@umuzi.org",
       comments: "Nicely done",
     },
     {
-      timestamp: `${date.getDate()}`,
+      timestamp: new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp),
       status: "Not Yet Competent",
       reviewer: "babalwa.mbolekwa@umuzi.org",
       comments: "Good work",
     },
     {
-      timestamp: `${date.getDate()}`,
+      timestamp: new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp),
       status: "Red Flag",
       reviewer: "kaleem.mohammad@umuzi.org",
       comments: "What is this",
@@ -96,41 +101,6 @@ const ReviewsTable = () => {
 
   return (
     <React.Fragment>
-      <FormControl>
-        <InputLabel id="demo-simple-select-label">Choose Device</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={responsive}
-          style={{ width: "200px", marginBottom: "10px", marginRight: 10 }}
-          onChange={(e) => setResponsive(e.target.value)}
-        >
-          <MenuItem value={"vertical"}>vertical</MenuItem>
-          <MenuItem value={"standard"}>standard</MenuItem>
-          <MenuItem value={"simple"}>simple</MenuItem>
-
-          <MenuItem value={"scroll"}>scroll (deprecated)</MenuItem>
-          <MenuItem value={"scrollMaxHeight"}>
-            scrollMaxHeight (deprecated)
-          </MenuItem>
-          <MenuItem value={"stacked"}>stacked (deprecated)</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl>
-        <InputLabel id="demo-simple-select-label">Table Body Height</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={tableBodyHeight}
-          style={{ width: "200px", marginBottom: "10px", marginRight: 10 }}
-          onChange={(e) => setTableBodyHeight(e.target.value)}
-        >
-          <MenuItem value={""}>[blank]</MenuItem>
-          <MenuItem value={"400px"}>400px</MenuItem>
-          <MenuItem value={"800px"}>800px</MenuItem>
-          <MenuItem value={"100%"}>100%</MenuItem>
-        </Select>
-      </FormControl>
       <MUIDataTable
         title={"Reviews"}
         data={data}
@@ -142,5 +112,3 @@ const ReviewsTable = () => {
 };
 
 export default ReviewsTable;
-
-// ReactDOM.render(<App />, document.getElementById("root"));
