fdc497a4060897f27743ce576c1de06c73d78767
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-09-02 14:09:09 +0200

    removing files that are not nessessary

--- frontend/src/stories/ReviewsTable.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReviewsTable from "../components/regions/CardDetailsModal/ReviewsTable";
-
-export default {
-    title: "Tilde/ReviewsTable",
-    component: ReviewsTable,
-}
-
-export const Primary = () => <ReviewsTable />
