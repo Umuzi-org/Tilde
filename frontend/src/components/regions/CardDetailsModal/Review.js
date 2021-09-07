1143a8e91d2844aaa602698c82f15fcbaa2a0d1d
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-09-07 13:19:13 +0200

    cleanup

--- frontend/src/components/regions/CardDetailsModal/Review.js
+++ frontend/src/components/regions/CardDetailsModal/Review.js
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import Markdown from "react-markdown";
