50cc1cdc8eb0e9b32b165ddb377c093a2d3b732f
Babalwa01 <babalwambolekwa93@gmail.com>
2021-09-09 18:48:14 +0200

    #154 TeamsList and ReviewTrustList component api and redux wiring

--- frontend/src/components/regions/Dashboard/index.js
+++ frontend/src/components/regions/Dashboard/index.js
@@ -1,6 +1,37 @@
 import React from "react";
-import Presentation from "./Presentation";
+import { Grid, Paper, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ReviewTrustTable from "../UserDetails/ReviewTrustTable/Presentation";
+import TeamsTable from "../UserDetails/TeamsTable/Presentation";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(1),
+    margin: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export default () => {
-  return <Presentation />;
+  const classes = useStyles();
+  
+  return (
+    <React.Fragment>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Typography>TEAMS</Typography>
+            <TeamsTable />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Typography>REVIEW TRUSTS</Typography>
+            <ReviewTrustTable />
+          </Paper>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  )
 };
