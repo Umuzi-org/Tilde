39fe853d225bc58ba9eb9d11dba960969492a288
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-09-13 17:52:28 +0200

    initial setup

---
+++ frontend/src/stories/Login.stories.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Login from "../components/regions/Login/Presentation"
+
+export default {
+  title: "Tilde/Login",
+  component: Login
+}
+
+export const Primary = () => <Login />%
