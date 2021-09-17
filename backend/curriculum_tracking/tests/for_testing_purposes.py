9bd3b6b6756b08f90d3c1fa45d1d682cbc20adfc
eckardberry <eckard.berry@gmail.com>
2021-09-17 14:35:19 +0200

    Re-created data for testing purposes

--- backend/curriculum_tracking/tests/for_testing_purposes.py
+++ backend/curriculum_tracking/tests/for_testing_purposes.py
@@ -23,146 +23,146 @@ class CreatingDataForTesting(TestCase):
 
     def test_creating_cards(self):
         # One card with 4 reviews to follow on it
-        self.card_1 = AgileCardFactory(
+        card_1 = AgileCardFactory(
             status=AgileCard.IN_PROGRESS,
         )
 
-        self.project_1 = self.card_1.recruit_project
-        self.user = self.card_1.assignees.first()
-        assert self.card_1.status == AgileCard.IN_PROGRESS
-        assert self.project_1.content_item == self.card_1.content_item
+        project_1 = card_1.recruit_project
+        user = card_1.assignees.first()
+        assert card_1.status == AgileCard.IN_PROGRESS
+        assert project_1.content_item == card_1.content_item
 
-        self.project_1.start_time = timezone.now() - timedelta(days=5)
-        self.project_1.save()
-        assert AgileCard.derive_status_from_project(self.project_1) == AgileCard.IN_PROGRESS
+        project_1.start_time = timezone.now() - timedelta(days=5)
+        project_1.save()
+        assert AgileCard.derive_status_from_project(project_1) == AgileCard.IN_PROGRESS
 
-        self.request_review_time = self.project_1.start_time + timedelta(1)
-        self.project_1.request_review(force_timestamp=self.request_review_time)
-        time_one = self.project_1.start_time - timedelta(days=6)
-        time_two = self.project_1.start_time + timedelta(days=4)
-        time_three = self.project_1.start_time + timedelta(days=3)
-        time_four = self.project_1.start_time + timedelta(days=2)
+        request_review_time = project_1.start_time + timedelta(1)
+        project_1.request_review(force_timestamp=request_review_time)
+        time_one = project_1.start_time - timedelta(days=6)
+        time_two = project_1.start_time + timedelta(days=4)
+        time_three = project_1.start_time + timedelta(days=3)
+        time_four = project_1.start_time + timedelta(days=2)
 
 
         # Four reviews are made at different times
-        self.review_1 = RecruitProjectReviewFactory(
+        review_1 = RecruitProjectReviewFactory(
             status=NOT_YET_COMPETENT,
-            recruit_project=self.project_1,
+            recruit_project=project_1,
             timestamp=time_one
         )
-        self.review_1.timestamp = time_one
-        self.review_1.save()
+        review_1.timestamp = time_one
+        review_1.save()
 
-        self.review_2 = RecruitProjectReviewFactory(
+        review_2 = RecruitProjectReviewFactory(
             status=COMPETENT,
-            recruit_project=self.project_1,
+            recruit_project=project_1,
         )
-        self.review_2.timestamp = time_two
-        self.review_2.save()
+        review_2.timestamp = time_two
+        review_2.save()
 
-        self.review_3 = RecruitProjectReviewFactory(
+        review_3 = RecruitProjectReviewFactory(
             status=EXCELLENT,
-            recruit_project=self.project_1,
+            recruit_project=project_1,
         )
-        self.review_3.timestamp = time_three
-        self.review_3.save()
+        review_3.timestamp = time_three
+        review_3.save()
 
-        self.review_4 = RecruitProjectReviewFactory(
+        review_4 = RecruitProjectReviewFactory(
             status=NOT_YET_COMPETENT,
-            recruit_project=self.project_1,
+            recruit_project=project_1,
         )
-        self.review_4.timestamp = time_four
-        self.review_4.save()
+        review_4.timestamp = time_four
+        review_4.save()
 
 
         # Three cards in the IN_PROGRESS column
-        self.content_item = ContentItemFactory(
+        content_item = ContentItemFactory(
             content_type=ContentItem.PROJECT,
             project_submission_type=ContentItem.REPOSITORY,
         )
 
-        self.card_2 = AgileCardFactory(
-            content_item=self.content_item,
+        card_2 = AgileCardFactory(
+            content_item=content_item,
             status=AgileCard.IN_PROGRESS,
             recruit_project=None,
         )
 
-        self.card_3 = AgileCardFactory(
-            content_item=self.content_item,
+        card_3 = AgileCardFactory(
+            content_item=content_item,
             status=AgileCard.IN_PROGRESS,
             recruit_project=None,
         )
 
-        self.card_4 = AgileCardFactory(
-            content_item=self.content_item,
+        card_4 = AgileCardFactory(
+            content_item=content_item,
             status=AgileCard.IN_PROGRESS,
             recruit_project=None,
         )
 
         assignee = SocialProfileFactory().user
-        self.card_2.assignees.set([assignee])
-        self.card_3.assignees.set([assignee])
-        self.card_4.assignees.set([assignee])
+        card_2.assignees.set([assignee])
+        card_3.assignees.set([assignee])
+        card_4.assignees.set([assignee])
 
-        #self.card_2.start_project()
-        #self.card_3.start_project()
-        #self.card_4.start_project()
+        #card_2.start_project()
+        #card_3.start_project()
+        #card_4.start_project()
 
         # Four cards in the COMPLETE column
-        self.content_item_2 = ContentItemFactory(
+        content_item_2 = ContentItemFactory(
             content_type=ContentItem.TOPIC,
             project_submission_type=ContentItem.REPOSITORY,
         )
 
-        self.card_5 = AgileCardFactory(
-            content_item=self.content_item_2,
+        card_5 = AgileCardFactory(
+            content_item=content_item_2,
             status=AgileCard.COMPLETE,
             #recruit_project=None,
         )
 
-        self.card_6 = AgileCardFactory(
-            content_item=self.content_item_2,
+        card_6 = AgileCardFactory(
+            content_item=content_item_2,
             status=AgileCard.COMPLETE,
             #recruit_project=None,
         )
 
-        self.card_7 = AgileCardFactory(
-            content_item=self.content_item_2,
+        card_7 = AgileCardFactory(
+            content_item=content_item_2,
             status=AgileCard.COMPLETE,
             #recruit_project=None,
         )
 
-        self.card_8 = AgileCardFactory(
-            content_item=self.content_item_2,
+        card_8 = AgileCardFactory(
+            content_item=content_item_2,
             status=AgileCard.COMPLETE,
             #recruit_project=None,
         )
 
         assignee_ = SocialProfileFactory().user
-        self.card_5.assignees.set([assignee_])
-        self.card_6.assignees.set([assignee_])
-        self.card_7.assignees.set([assignee_])
-        self.card_8.assignees.set([assignee_])
+        card_5.assignees.set([assignee_])
+        card_6.assignees.set([assignee_])
+        card_7.assignees.set([assignee_])
+        card_8.assignees.set([assignee_])
 
 
         # Two cards in the IN_REVIEW column
-        self.content_item_3 = ContentItemFactory(
+        content_item_3 = ContentItemFactory(
             content_type=ContentItem.TOPIC,
             project_submission_type=ContentItem.REPOSITORY,
         )
 
-        self.card_9 = AgileCardFactory(
-            content_item=self.content_item_3,
+        card_9 = AgileCardFactory(
+            content_item=content_item_3,
             status=AgileCard.IN_REVIEW,
             recruit_project=None,
         )
 
-        self.card_10 = AgileCardFactory(
-            content_item=self.content_item_3,
+        card_10 = AgileCardFactory(
+            content_item=content_item_3,
             status=AgileCard.IN_REVIEW,
             recruit_project=None,
         )
 
         assignee_ = SocialProfileFactory().user
-        self.card_9.assignees.set([assignee_])
-        self.card_10.assignees.set([assignee_])
+        card_9.assignees.set([assignee_])
+        card_10.assignees.set([assignee_])
