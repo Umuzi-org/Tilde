cb6446e282e2a08f5bf9de2d9f6df1649b1660f4
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-09-21 19:58:33 +0200

    modifying code in the "Presentation" file and deleting "LoginForm.js"

--- frontend/src/components/regions/Login/Presentation.js
+++ frontend/src/components/regions/Login/Presentation.js
@@ -1,49 +1,63 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-
-import { Paper } from "@material-ui/core";
 import Alert from "@material-ui/lab/Alert";
+import LockRoundedIcon from "@material-ui/icons/LockRounded"
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    margin: theme.spacing(2),
-    // padding: theme.spacing(2),
-  },
-  button: {
-    margin: theme.spacing(1),
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: theme.spacing(2),
+    height: "100vh",
+    overflow: "hidden",
+
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "300px",
+    },
+    "& .MuiButtonBase-root": {
+      margin: theme.spacing(2),
+    },
   },
   alert: {
     margin: theme.spacing(1),
-  },
+    width: "275px"
+  }
 }));
 
-export default ({ loading, error, handleLoginWithGoogle }) => {
+const LoginForm = ({loading, error, handleLoginWithGoogle}) => {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.root}>
-      <Typography variant="h6">Please log in</Typography>
+    <form className={classes.root}>
 
-      <Alert severity="info" className={classes.alert}>
+     <Alert severity="info" className={classes.alert}>
         Note that this site uses popups for authentication
       </Alert>
-
-      {error && (
-        <Alert severity="error" className={classes.alert}>
+      {error && ( <Alert severity="error" className={classes.alert}>
           ERROR: {error}. You might need to refresh this page to attempt to
           login again
         </Alert>
       )}
-
-      <Button
-        className={classes.button}
-        variant="contained"
-        onClick={handleLoginWithGoogle}
-      >
-        Login with Google
-      </Button>
-    </Paper>
+      <LockRoundedIcon />
+      <TextField label="Email" variant="outlined" type="email" required />
+      <TextField label="Password" variant="outlined" type="password" required />
+      <div>
+        <Button type="submit" variant="contained" color="primary">
+          Login
+        </Button>
+        <Button type="submit" variant="contained" color="primary" onClick={handleLoginWithGoogle}>
+          Login with Google
+        </Button>
+      </div>
+    </form>
   );
 };
+
+export default LoginForm;
