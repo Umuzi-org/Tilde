566df3c5dbb17eb52b04e6798238b49d2eff9297
Babalwa01 <babalwambolekwa93@gmail.com>
2021-09-21 17:31:04 +0200

    #132 canSetDueTime function modified

--- frontend/src/components/regions/CardDetailsModal/utils.js
+++ frontend/src/components/regions/CardDetailsModal/utils.js
@@ -1,8 +1,9 @@
 export function canSetDueTime({ card, authUser }) {
     return (
-        !card.dueTime && authUser.email === card.assigneeNames[0] && 
-        Object.keys(authUser.permissions.teams)
-        .map((key) => authUser.permissions.teams[key].permissions[0])
-        .includes("MANAGE_CARDS") 
+        !card.dueTime && (
+            (authUser.email === card.assigneeNames[0]) ||
+            (authUser.email !== card.assigneeNames[0] && Object.keys(authUser.permissions.teams).map((key) => authUser.permissions.teams[key].permissions[0]).length !== 0) ||
+            (authUser.email !== card.assigneeNames[0] && Object.keys(authUser.permissions.teams).map((key) => authUser.permissions.teams[key].permissions[0]).includes("MANAGE_CARDS"))
+        )
     )
 }
