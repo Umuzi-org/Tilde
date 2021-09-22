52781c80af71b7f729cd39aa1765302f853edbb2
Ryan O'Connell <ryan@oconnell.org.za>
2021-09-22 17:19:53 +0200

    added skills test syllabuses to boot loader

--- backend/core/management/commands/bootcamp_loader.py
+++ backend/core/management/commands/bootcamp_loader.py
@@ -22,6 +22,10 @@ SEQUENCE_WEB = "WD"
 SEQUENCE_DATA_SCI = "DS"
 SEQUENCE_DATA_ENG = "DE"
 SEQUENCE_JAVA = "Java"
+SEQUENCE_SKILL_TEST_WEB = "Skills Test WD"
+SEQUENCE_SKILL_TEST_DATA_SCI = "Skills Test DS"
+SEQUENCE_SKILL_TEST_DATA_ENG = "Skills Test DE"
+SEQUENCE_SKILL_TEST_JAVA = "Skills Test Java"
 SEQUENCE_IT = "IT"
 SEQUENCE_EXTERNAL_WEB = "WD - Ext"
 SEQUENCE_DPD = "DPD"
@@ -30,6 +34,10 @@ TEAM_COURSE_NAME_PARTS = {  # these strings end up in the Team names
     SEQUENCE_ALUMNI_WEB: "alumni web dev",
     SEQUENCE_ALUMNI_DATA_ENG: "alumni data eng",
     SEQUENCE_ALUMNI_JAVA: "alumni java",
+    SEQUENCE_SKILL_TEST_WEB = "skills test web dev",
+    SEQUENCE_SKILL_TEST_DATA_SCI = "skills test data sci",
+    SEQUENCE_SKILL_TEST_DATA_ENG = "skills test data eng",
+    SEQUENCE_SKILL_TEST_JAVA = "skills test java",
     SEQUENCE_WEB: "web dev",
     SEQUENCE_DATA_ENG: "data eng",
     SEQUENCE_JAVA: "java",
@@ -46,6 +54,14 @@ BOOTCAMP_INTRO = "Introduction to Bootcamp and Learnership"
 COMMON_TECH_BOOT_REQUIREMENTS = "Common tech bootcamp requirements"
 POST_BOOTCAMP_SOFT_SKILLS = "Post Bootcamp Soft Skills"
 DPD_BOOTCAMP = "UX Strategy Syllabus"
+SKILL_TEST_WEB = "Employed Web Development skills test"
+SKILL_TEST_DATA_SCI = "Employed Data Science skills test"
+SKILL_TEST_DATA_ENG = "Employed Data Engineering skills test"
+SKILL_TEST_JAVA = "Employed Java skills test"
+SKILLS_TEST_TILE_1 "Employed learnership intro to Tilde part 1"
+SKILLS_TEST_TILDE_2 "Employed learnership intro to Tilde part 2"
+STILLS_TEST_COMMON_TECH_BOOT_REQUIREMENTS "Employed common tech reqirements"
+
 # PRE_BOOT_COURSES_ALUMNI = [TILDE_INTRO, COMMON_TECH_BOOT_REQUIREMENTS]
 # PRE_BOOT_COURSES_NORMAL = [TILDE_INTRO, BOOTCAMP_INTRO, COMMON_TECH_BOOT_REQUIREMENTS]
 # PRE_BOOT_COURSES_EXTERNAL = [TILDE_INTRO, COMMON_TECH_BOOT_REQUIREMENTS]
@@ -79,6 +95,30 @@ SEQUENCE_COURSES = {
     SEQUENCE_DPD: [
         TILDE_INTRO_NON_TECH,
         DPD_BOOTCAMP,
+    ],
+    SEQUENCE_SKILL_TEST_WEB: [
+        SKILLS_TEST_TILE_1
+        SKILL_TEST_WEB
+        SKILLS_TEST_TILDE_2
+        STILLS_TEST_COMMON_TECH_BOOT_REQUIREMENTS
+    ],
+    SEQUENCE_SKILL_TEST_DATA_SCI: [
+        SKILLS_TEST_TILE_1
+        SKILL_TEST_DATA_SCI
+        SKILLS_TEST_TILDE_2
+        STILLS_TEST_COMMON_TECH_BOOT_REQUIREMENTS
+    ],
+    SEQUENCE_SKILL_TEST_DATA_ENG: [
+        SKILLS_TEST_TILE_1
+        SKILL_TEST_DATA_ENG
+        SKILLS_TEST_TILDE_2
+        STILLS_TEST_COMMON_TECH_BOOT_REQUIREMENTS
+    ],
+    SEQUENCE_SKILL_TEST_JAVA: [
+        SKILLS_TEST_TILE_1
+        SKILL_TEST_JAVA
+        SKILLS_TEST_TILDE_2
+        STILLS_TEST_COMMON_TECH_BOOT_REQUIREMENTS
     ],
 }
 
