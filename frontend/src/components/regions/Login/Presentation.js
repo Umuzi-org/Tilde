07016ce56ccebb787fb1d029313411a0307867cd
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-09-22 23:13:23 +0200

    removing unused import and adding "Please login"

--- frontend/src/components/regions/Login/Presentation.js
+++ frontend/src/components/regions/Login/Presentation.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Alert from "@material-ui/lab/Alert";
-import LockRoundedIcon from "@material-ui/icons/LockRounded"
-
+import LockRoundedIcon from "@material-ui/icons/LockRounded";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,20 +26,24 @@ const useStyles = makeStyles((theme) => ({
   },
   alert: {
     margin: theme.spacing(1),
-    width: "275px"
-  }
+    width: "275px",
+  },
 }));
 
-const LoginForm = ({loading, error, handleLoginWithGoogle}) => {
+const LoginForm = ({ loading, error, handleLoginWithGoogle }) => {
   const classes = useStyles();
 
   return (
     <form className={classes.root}>
+      <Typography variant="h5" style={{ fontWeight: 600 }}>
+        Please Login
+      </Typography>
 
-     <Alert severity="info" className={classes.alert}>
+      <Alert severity="info" className={classes.alert}>
         Note that this site uses popups for authentication
       </Alert>
-      {error && ( <Alert severity="error" className={classes.alert}>
+      {error && (
+        <Alert severity="error" className={classes.alert}>
           ERROR: {error}. You might need to refresh this page to attempt to
           login again
         </Alert>
@@ -52,7 +55,12 @@ const LoginForm = ({loading, error, handleLoginWithGoogle}) => {
         <Button type="submit" variant="contained" color="primary">
           Login
         </Button>
-        <Button type="submit" variant="contained" color="primary" onClick={handleLoginWithGoogle}>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          onClick={handleLoginWithGoogle}
+        >
           Login with Google
         </Button>
       </div>
