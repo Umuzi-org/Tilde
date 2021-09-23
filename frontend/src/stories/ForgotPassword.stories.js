d030a3ec792b57f0a7276933aac0a2f324e152c7
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-09-23 13:38:19 +0200

    #163 Forgot password page

---
+++ frontend/src/stories/ForgotPassword.stories.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ForgotPassword from "../components/regions/Login/ForgotPassword/Presentation"
+
+export default {
+  title: "Tilde/ForgotPassword",
+  component: ForgotPassword
+}
+
+export const Primary = () => <ForgotPassword />
