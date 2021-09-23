29c729f5d16941dc71771e519562cd5e4422deb4
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-09-23 21:36:57 +0200

    Change password page

---
+++ frontend/src/stories/ChangePassword.stories.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ChangePassword from "../components/regions/Login/ChangePassword/Presentation"
+
+export default {
+  title: "Tilde/ChangePassword",
+  component: ChangePassword
+}
+
+export const Primary = () => <ChangePassword />
