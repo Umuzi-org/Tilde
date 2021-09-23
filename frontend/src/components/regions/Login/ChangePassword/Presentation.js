6c9a110648be8bc4be48c83f0c0e17d835ba8fab
Sbonelo01 <sbonelomkhize15@gmail.com>
2021-09-23 21:45:32 +0200

    #172 change password page

--- frontend/src/components/regions/Login/ChangePassword/Presentation.js
+++ frontend/src/components/regions/Login/ChangePassword/Presentation.js
@@ -4,7 +4,6 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Alert from "@material-ui/lab/Alert";
-import LockRoundedIcon from "@material-ui/icons/LockRounded";
 
 const useStyles = makeStyles((theme) => ({
   root: {
