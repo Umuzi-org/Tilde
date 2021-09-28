8fe7106574ab8b58985a1b60246a2285dcabacca
kaleem99 <kaleem1999@outlook.com>
2021-09-28 17:44:26 +0200

    #165 added container styles

--- frontend/src/components/regions/ResetPassword/Presentation.js
+++ frontend/src/components/regions/ResetPassword/Presentation.js
@@ -1,15 +1,45 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
 
 export default ({ email }) => {
-
-  return <div>
+  const containerStyles = {
+    width: '300px',
+    height: '500px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: "center",
+    display: "block",
+    marginTop: "10%",
+  }
+  const textInput = {
+    marginBottom: '1rem',
+  }
+  return (
+    <Container style = {containerStyles}>
       <form>
-            <p>Email: {email}</p>
-
-          <input type="password"></input>
-          <input type="password"></input>
-            <button>Submit</button>
+        <p>Email: {email}</p>
+        <TextField
+        style={textInput}
+          label="Password"
+          variant="outlined"
+          type="password"
+          required
+        />
+        <TextField
+        style={textInput}
+          label="Confirm Password"
+          variant="outlined"
+          type="password"
+          required
+        />
       </form>
-  </div>;
+      <Button variant="contained" color="primary" margin="auto">
+        Submit
+      </Button>
+    </Container>
+    
+  );
 };
+
