042aa2ddf04565eb0baca0c2d70af3b242fcbdd0
kaleem99 <kaleem1999@outlook.com>
2021-09-30 15:59:40 +0200

    #165 made changes to design and added alert box

--- frontend/src/components/regions/ResetPassword/Presentation.js
+++ frontend/src/components/regions/ResetPassword/Presentation.js
@@ -2,44 +2,59 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Container from "@material-ui/core/Container";
+import Alert from "@material-ui/lab/Alert";
 
 export default ({ email }) => {
   const containerStyles = {
-    width: '300px',
-    height: '500px',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "350px",
+    height: "500px",
+    justifyContent: "center",
+    alignItems: "center",
     textAlign: "center",
     display: "block",
-    marginTop: "10%",
-  }
+    margin: "10% auto",
+  };
   const textInput = {
-    marginBottom: '1rem',
-  }
+    marginBottom: "1rem",
+    width: "245px",
+  };
+  const alert = {
+    margin: "auto",
+    width: "245px",
+  };
+  const buttonStyle = {
+    variant: "contained",
+    margin: "auto",
+    width: "115px",
+  };
   return (
-    <Container style = {containerStyles}>
+    <Container style={containerStyles}>
+      <h2>Change Password</h2>
+      <Alert severity="info" className={alert}>
+        Please change your password below and make sure that your new password
+        does not match the previous one
+      </Alert>
+
       <form>
         <p>Email: {email}</p>
         <TextField
-        style={textInput}
-          label="Password"
+          style={textInput}
+          label="New password"
           variant="outlined"
           type="password"
           required
         />
         <TextField
-        style={textInput}
-          label="Confirm Password"
+          style={textInput}
+          label="Confirm new password"
           variant="outlined"
           type="password"
           required
         />
       </form>
-      <Button variant="contained" color="primary" margin="auto">
+      <Button variant="contained" color="primary" style={buttonStyle}>
         Submit
       </Button>
     </Container>
-    
   );
 };
-
