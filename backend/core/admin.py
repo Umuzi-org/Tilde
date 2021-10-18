295d08b665217f7ed2fa8a68092cf6336aff26ba
eckardberry <eckard.berry@gmail.com>
2021-10-18 14:02:19 +0200

    Updated test class name, updated from assert to self.assert as used in unit tests and updated function to be inside class TeamAdmin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -8,17 +8,18 @@ class UserSetInline(admin.TabularInline):
     raw_id_fields = ("user",)
 
 
-def deactivate_team_members(TeamAdminInstance, request, queryset: object):
-    for team in queryset:
-        for team_member in team.active_users:
-            team_member.active = False
-            team_member.save()
-
-
 @admin.register(models.Team)
 class TeamAdmin(GuardedModelAdmin):
+
+    def deactivate_team_members(self, request, queryset: object):
+        for team in queryset:
+            for team_member in team.active_users:
+                team_member.active = False
+                team_member.save()
+
     list_display = ["name", "active"]
     list_filter = ["active"]
+    search_fields = ["name"]
     fieldsets = (
         (
             None,
--- backend/core/tests/test_admin_functionality_make_team_members_inactive.py
+++ backend/core/tests/test_admin_functionality_make_team_members_inactive.py
@@ -1,10 +1,10 @@
 from core.tests import factories
 from django.test import TestCase
-from core.admin import deactivate_team_members
+from core.admin import TeamAdmin
 from mock import patch
 
 
-class TestMakeTeamMembersInactiveForInactiveTeams(TestCase):
+class TestMakeTeamMembersInactiveForATeamOrTeams(TestCase):
 
     def setUp(self):
         self.active_team = factories.TeamFactory()
@@ -18,25 +18,25 @@ class TestMakeTeamMembersInactiveForInactiveTeams(TestCase):
     def test_make_all_members_inactive_for_an_active_team(self, mocked_Request, mocked_Team):
 
         # Making sure the team is active and so to it's member
-        assert self.active_team.active == True
-        assert self.active_team.active_users[0].active == True
-        deactivate_team_members(mocked_Team, mocked_Request, [self.active_team])
+        self.assertTrue(self.active_team.active)
+        self.assertTrue(self.active_team.active_users[0].active)
+        TeamAdmin.deactivate_team_members(mocked_Team, mocked_Request, [self.active_team])
 
         # Checking that the team is still active but the member not
-        assert self.active_team.active == True
-        assert len(self.active_team.user_set.filter(active=False).all()) == 1
-        assert self.user in self.active_team.user_set.filter(active=False).all()
+        self.assertTrue(self.active_team.active)
+        self.assertEqual(len(self.active_team.user_set.filter(active=False).all()), 1)
+        self.assertIn(self.user, self.active_team.user_set.filter(active=False).all())
 
     @patch('core.admin.TeamAdmin')
     @patch('django.http.HttpRequest')
     def test_make_all_members_inactive_for_an_inactive_team(self, mocked_Request, mocked_Team):
 
         # Making sure the team is inactive but it's team member is active
-        assert self.inactive_team.active == False
-        assert self.inactive_team.active_users[0].active == True
-        deactivate_team_members(mocked_Team, mocked_Request, [self.inactive_team])
+        self.assertFalse(self.inactive_team.active)
+        self.assertTrue(self.inactive_team.active_users[0].active)
+        TeamAdmin.deactivate_team_members(mocked_Team, mocked_Request, [self.inactive_team])
 
         # Checking that the team is still inactive and so to it's member
-        assert self.inactive_team.active == False
-        assert len(self.inactive_team.user_set.filter(active=False).all()) == 1
-        assert self.user in self.inactive_team.user_set.filter(active=False).all()
+        self.assertFalse(self.inactive_team.active)
+        self.assertEqual(len(self.inactive_team.user_set.filter(active=False).all()), 1)
+        self.assertIn(self.user, self.inactive_team.user_set.filter(active=False).all())
