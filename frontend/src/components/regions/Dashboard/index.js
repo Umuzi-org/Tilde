16e2aaa0dd2aadfedec0f8cea7bf0d27111f16a2
Babalwa <babalwambolekwa93@gmail.com>
2021-10-21 14:12:13 +0200

    #154 Redux part started

--- frontend/src/components/regions/Dashboard/index.js
+++ frontend/src/components/regions/Dashboard/index.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Presentation from "./Presentation";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
 
-export default () => {
+function DashboardUnconnected({ user, authedUserId }) {
+  let urlParams = useParams() || {};
+  const userId = parseInt(urlParams.userId || authedUserId || 0);
+  
   return <Presentation />;
-};
+}
+
+const mapStateToProps = (state) => {
+  return {
+    authedUserId: state.App.authUser.userId,
+
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    
+  }
+}
+
+const Dashboard = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DashboardUnconnected);
+
+export default Dashboard;
