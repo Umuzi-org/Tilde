70dbbf7b397c37656a14b963c35e1195d36c1f0c
Siphiwe <siphiwe.m@protonmail.com>
2021-10-28 22:57:48 +0200

    update Command

--- backend/curriculum_tracking/management/commands/export_project_urls.py
+++ backend/curriculum_tracking/management/commands/export_project_urls.py
@@ -3,7 +3,17 @@ from core.models import Team
 from curriculum_tracking.models import RecruitProject
 import csv
 
+
+def get_email_and_url(team):
+    results = []
+    for user in team.active_users:
+        project = RecruitProject.objects.filter(recruit_users__in=[user]).first() # filter_by
+        results.append([user.email, project and (project.link_submission or project.git_url)])
+    return results[0]
+
 class Command(BaseCommand):
+    ''' python manage.py export_project_urls <team> <project>
+    '''
     def add_arguments(self, parser):
         parser.add_argument("team_name", type=str)
         parser.add_argument("content_item_title", type=str)
@@ -17,11 +27,3 @@ class Command(BaseCommand):
             writer = csv.writer(f)
             writer.writerow(['user_eamil', 'url'])
             writer.writerows(results)
-
-def get_email_and_url(team):
-    results = []
-    for user in team.active_users:
-        project = RecruitProject.objects.filter(content_item__title=options['content_item_title']).filter_by(recruit_users__in=[user]).first()
-        # results.append([user.email, project.link_submission if project else None])# if prject not completed - just leave it blank or add none
-        results.append([user.email, project and (project.link_submission or project.git_url)])
-    return results
