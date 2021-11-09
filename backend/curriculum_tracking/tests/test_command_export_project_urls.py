df0a7810d93a7f2815067178fe700fe3dc375109
Siphiwe <siphiwe.m@protonmail.com>
2021-11-09 23:17:46 +0200

    project submitted passing

--- backend/curriculum_tracking/tests/test_command_export_project_urls.py
+++ backend/curriculum_tracking/tests/test_command_export_project_urls.py
@@ -1,19 +1,33 @@
 from core.tests.factories import TeamFactory, UserFactory
-from curriculum_tracking.tests.factories import ProjectContentItemFactory
+from curriculum_tracking.tests.factories import ProjectContentItemFactory, RecruitProjectFactory
 from django.test import TestCase
 from curriculum_tracking import models
-from curriculum_tracking.management.commands.export_project_urls import get_email_and_url
+from curriculum_tracking.management.commands.export_project_urls import get_user_and_projectlink
+
+from curriculum_tracking.tests import factories
+from git_real.tests import factories as git_real_factories
+
 
 class TestCommandExportProject(TestCase):
 
     def test_project_not_submitted(self):
-        self.team = TeamFactory()
-        self.user = UserFactory()
-        self.team.users.add(self.user)
+        team = TeamFactory()
+        user = UserFactory()
+        team.users.add(user)
+        content_item = ProjectContentItemFactory()
+        result = get_user_and_projectlink(team, content_item)
+        self.assertEqual(result, [user.email, None])
 
-        project = ProjectContentItemFactory()
-        result = get_email_and_url(self.team)
-        self.assertEqual(result, [self.user.email, None])
+    def test_project_submitted(self):
+        team = TeamFactory()
+        project = factories.RecruitProjectFactory(link_submission='https://i_am_a_link')
+        content_item = project.content_item
+        link = project.link_submission  # -> <Repository: https://something.com/like_this4>
+        user = project.recruit_users.first()  # ->  <User: foo.5@example.com>
+        team.users.add(user)
 
-    def test_project_submitted(self): pass
+        result = get_user_and_projectlink(team, project.content_item)
+        self.assertEqual(result, [user.email, link])
+
+    def test_multiple_projects_submitted(self):
         
