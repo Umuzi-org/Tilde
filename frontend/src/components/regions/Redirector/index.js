2c193a4b72c059d4ef9782dd386bea17fe47771b
Sheena <sheena.oconnell@gmail.com>
2021-11-15 15:28:41 +0200

    header area present while viewing own data

---
+++ frontend/src/components/regions/Redirector/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+import { routes } from "../../../routes.js";
+
+const RedirectorUnconnected = ({ authUser }) => {
+  window.location = routes.userBoard.route.path.replace(
+    ":userId",
+    authUser.userId
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    authUser: state.App.authUser,
+    // whoAmICallStatus: state.WHO_AM_I,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+const Redirector = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RedirectorUnconnected);
+
+export default Redirector;
