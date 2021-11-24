7212aeb80ebce72564412bcb67912b7b1648ac3a
eckardberry <eckard.berry@gmail.com>
2021-11-24 22:17:58 +0200

    The command is now finished, need some clarity about which date is to be set as due date

--- backend/curriculum_tracking/management/commands/set_bulk_due_dates.py
+++ backend/curriculum_tracking/management/commands/set_bulk_due_dates.py
@@ -1,22 +1,27 @@
 from django.core.management.base import BaseCommand
-from curriculum_tracking.models import FlavourMixin, AgileCard, Team
+from curriculum_tracking.models import AgileCard, Team
 from datetime import datetime
-from ..helpers import get_users
+from ..helpers import get_team_cards
 
 
 class Command(BaseCommand):
+    """
+    Generic command structure: python manage.py set_bulk_due_dates "team_name" "content_item_id" "flavour"
+    command example: python manage.py set_bulk_due_dates bomb_squad 2 Python
+    """
+
     def add_arguments(self, parser):
         parser.add_argument("team", type=str)
         parser.add_argument("content_item_id", type=int)
         parser.add_argument("flavour", type=str)
 
     def handle(self, *args, **options):
-        team = team = Team.objects.get(name=options["team"])
+        team = Team.objects.get(name=options["team"])
         content_item_id = options["content_item_id"]
         flavours = [flavour for flavour in options["flavour"].split(",") if flavour]
+        team_cards = get_team_cards(team, content_item_id)
 
-        time = datetime.now()
-
-        for member in team.active_users:
-            breakpoint()
-            AgileCard.set_due_time(member, time)
+        for card in team_cards:
+            if card.flavour_names in flavours:
+                AgileCard.set_due_time(card, datetime.now())
+            continue
