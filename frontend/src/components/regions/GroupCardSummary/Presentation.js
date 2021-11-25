0710333a6eb9773bfd0a97f65e7d356a4f4c37eb
Vuyisanani <vmeteni@gmail.com>
2021-11-25 08:29:20 +0200

    Chnaged the background on mobile device and title styles

--- frontend/src/components/regions/GroupCardSummary/Presentation.js
+++ frontend/src/components/regions/GroupCardSummary/Presentation.js
@@ -25,8 +25,18 @@ const useStyles = makeStyles((theme) => {
       background: "#fff",
       left: 0,
       zIndex: 1,
-    },
 
+     ['@media (max-width:780px)']: { // eslint-disable-line no-useless-computed-key
+        background: "none",
+      }, 
+    },
+    title: {
+      marginLeft: "16px",
+      ['@media (max-width:780px)']: { // eslint-disable-line no-useless-computed-key
+        fontSize: "1.5rem",
+        marginLeft: "16px",
+      }, 
+    },
     container: {
       // width: 912 - 200,
       // height: 976 - 300,
@@ -48,7 +58,7 @@ export default ({
 
   return (
     <React.Fragment>
-      <Typography variant="h4">{userGroup.name}</Typography>
+      <Typography className={classes.title} variant="h4">{userGroup.name}</Typography>
 
       <div className={classes.container} onScroll={handleScroll}>
         <Table>
