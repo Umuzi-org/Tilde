7b95e2f7a9f14945ee0ac8e7643894251e5ec677
kaleem99 <kaleem1999@outlook.com>
2021-11-29 15:46:32 +0200

    #255 added stories for chart

---
+++ frontend/src/stories/UserFeedbackChart.stories.js
@@ -0,0 +1,11 @@
+import UserFeedBackChart from "../components/regions/UserFeedbackChart/Presentation";
+import React from "react";
+export default {
+    title: "Tilde/UserFeedBackChart",
+    component: UserFeedBackChart,
+}
+const Template = args => <UserFeedBackChart {...args} />
+export const Primary = Template.bind({});
+Primary.args = {
+
+}
