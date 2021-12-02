59e36b2451711130bf478b9312549f718efb9461
eckardberry <eckard.berry@gmail.com>
2021-12-02 17:07:52 +0200

    Finished with tests, still getting error that reverse for 'managmentaction' not found?

--- backend/core/tests/test_api_views.py
+++ backend/core/tests/test_api_views.py
@@ -4,7 +4,8 @@ from . import factories
 from core.models import Team
 from guardian.shortcuts import assign_perm
 from django.contrib.auth.models import Group
-
+from curriculum_tracking.tests.factories import RecruitProjectFactory, AgileCardFactory
+from curriculum_tracking.management.helpers import get_team_cards
 
 # class TestUserViewSet(APITestCase, APITestCaseMixin):
 #     LIST_URL_NAME = "user-list"
@@ -128,7 +129,7 @@ class TestTeamViewSet(APITestCase, APITestCaseMixin):
 
 class TestBulkSetDueDatesApi(APITestCase, APITestCaseMixin):
 
-    #LIST_URL_NAME = "managmentaction-list"
+    LIST_URL_NAME = "managmentaction"
     SUPPRESS_TEST_POST_TO_CREATE = True
 
     def verbose_instance_factory(self):
@@ -140,24 +141,82 @@ class TestBulkSetDueDatesApi(APITestCase, APITestCaseMixin):
 
     def setUp(self):
 
-        #self.url = self.get_list_url()
         self.api_url = '/api/managment_actions/bulk_set_due_dates/'
 
-        self.blue_team = factories.TeamFactory()
-        self.red_team = factories.TeamFactory()
+        self.blue_team = factories.TeamFactory(name='BLUE TEAM')
+        self.red_team = factories.TeamFactory(name='RED TEAM')
         self.user_one_blue = factories.UserFactory(first_name='one_blue', is_superuser=False, is_staff=False)
         self.user_two_blue = factories.UserFactory(first_name='two_blue', is_superuser=False, is_staff=False)
         self.user_one_red = factories.UserFactory(first_name='one_red', is_superuser=False, is_staff=False)
         self.user_two_red = factories.UserFactory(first_name='two_red', is_superuser=False, is_staff=False)
+        self.super_user = factories.UserFactory(first_name='super_user', is_superuser=True)
 
         self.blue_team.user_set.add(self.user_one_blue)
         self.blue_team.user_set.add(self.user_two_blue)
         self.red_team.user_set.add(self.user_one_red)
         self.red_team.user_set.add(self.user_two_red)
 
-    def test_team_members_cannot_bulk_set_due_dates(self):
+    def test_team_members_cannot_bulk_set_due_dates_for_the_team_they_belong_to(self):
 
         self.login(self.user_one_blue)
-        response = self.client.post(self.api_url)
-        breakpoint()
+        response = self.client.post(self.api_url, format="json", data={
+            'due_time': '2021-12-03T14:17', 'content_item': '2', 'team': str(self.blue_team)
+        })
+        self.assertEqual(response.data["detail"].code, "permission_denied")
 
+    def test_team_members_cannot_bulk_set_due_dates_for_teams_they_dont_belong_to(self):
+
+        self.login(self.user_one_blue)
+        response = self.client.post(self.api_url, format="json", data={
+            'due_time': '2021-12-03T14:17', 'content_item': '2', 'team': str(self.red_team)
+        })
+        self.assertEqual(response.data["detail"].code, "permission_denied")
+
+    def test_super_user_can_bulk_set_due_dates_for_a_team(self):
+
+        project = RecruitProjectFactory(due_time=None)
+        card = AgileCardFactory(recruit_project=project)
+        card.reviewers.add(self.user_one_blue)
+        card.assignees.add(self.user_two_blue)
+        self.assertIsNone(card.due_time)
+
+        self.login(self.super_user)
+        response = self.client.post(self.api_url, format='json', data={
+            'due_time': '2021-12-03T14:17', 'content_item': str(card.content_item.id), 'team': str(self.blue_team)
+        })
+        self.assertEqual(response.status_code, 200)
+        card.refresh_from_db()
+        self.assertIsNotNone(card.due_time)
+
+    def test_bulk_set_due_date_happened_for_every_card_with_the_same_content_item_for_the_team(self):
+
+        """
+        Three cards, two with the same content_item and one with a different content_item.  The one with the
+        different content_item should not have it's due_time updated, it should be left as it is.
+        """
+        project = RecruitProjectFactory(due_time=None)
+        card_1 = AgileCardFactory(recruit_project=project)
+        card_1.assignees.add(self.user_one_red)
+        card_2 = AgileCardFactory(recruit_project=RecruitProjectFactory(due_time=None), content_item=card_1.content_item)
+        card_2.assignees.add(self.user_two_red)
+        card_3 = AgileCardFactory(recruit_project=RecruitProjectFactory(due_time=None))
+        card_3.assignees.add(self.user_two_red)
+        self.assertEqual(card_1.content_item, card_2.content_item)
+        self.assertNotEqual(card_1.content_item, card_3.content_item)
+
+        self.login(self.super_user)
+        response = self.client.post(self.api_url, format='json', data={
+            'due_time': '2022-01-20T12:17', 'content_item': card_1.content_item.id, 'team': str(self.red_team)
+        })
+        self.assertEqual(response.status_code, 200)
+
+        team_cards = get_team_cards(self.red_team, card_1.content_item)
+        self.assertIn(card_1, team_cards)
+        self.assertIn(card_2, team_cards)
+        self.assertNotIn(card_3, team_cards)
+
+        for card in team_cards:
+            card.refresh_from_db()
+            self.assertIsNotNone(card.due_time)
+
+        self.assertIsNone(card_3.due_time)
