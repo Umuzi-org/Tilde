9bfc5efa65d266a19787687ee7704486edf35753
Faith Mofokeng <faithmofokeng5@gmail.com>
2021-12-08 05:23:59 +0200

    refactored utils.js of UsersAndGroups

--- frontend/src/components/regions/UsersAndGroups/utils.js
+++ frontend/src/components/regions/UsersAndGroups/utils.js
@@ -1,40 +1,28 @@
+const timeNow = new Date()
+const milliseconds = 1000 * 60 * 60 * 24
+const timeDifferenceInDays = (time) => {return Math.ceil(Math.abs(timeNow - new Date(time)) / milliseconds)}
+let prColor
+
 export const getPrColor = (oldestOpenPrTime) => {
-    const now = new Date()
-    const milliseconds = 1000 * 60 * 60 * 24
-    // console.log(now)
-    console.log("oldestOpenPrTime: ", typeof(new Date(oldestOpenPrTime)), oldestOpenPrTime)
-
-    const timeDifferenceInSeconds = Math.abs(now - new Date(oldestOpenPrTime))
-    const timeDifferenceInDays = Math.ceil(timeDifferenceInSeconds / milliseconds)
-    // console.log(timeDifferenceInDays)
-
-    let prColor
-
-    if(timeDifferenceInDays <= 2) {
+    if (timeDifferenceInDays(oldestOpenPrTime) === 1){
+        prColor = "black"
+    }else if(timeDifferenceInDays(oldestOpenPrTime) <= 2) {
         prColor = "orange"
     }else {
         prColor = "red"
     }
-
     return prColor 
 }
 
-export const getTildeReviewColor = (openPrTime) => {
-    const now = new Date()
-    const milliseconds = 1000 * 60 * 60 * 24
-    console.log("openPrTime: ", typeof(new Date(openPrTime)), openPrTime)
-    const timeDifferenceInSeconds = Math.abs(now - new Date(openPrTime))
-    console.log("time difference in seconds: ", timeDifferenceInSeconds)
-    const timeDifferenceInDays = Math.ceil(timeDifferenceInSeconds / milliseconds)
-    console.log("time difference in days: ", timeDifferenceInDays)
-
-    let tildeColor
-
-    if(timeDifferenceInDays <= 3) {
-        tildeColor = "orange"
-    }else {
-        tildeColor = "red"
+export const getTildeReviewColor = (oldestOpenPrTime) => {
+    console.log(timeDifferenceInDays(oldestOpenPrTime))
+    if(timeDifferenceInDays(oldestOpenPrTime) === 1){
+        prColor = "black"
     }
-
-    return tildeColor 
+    else if(timeDifferenceInDays(oldestOpenPrTime) <= 3) {
+        prColor = "orange"
+    }else {
+        prColor = "red"
+    }
+    return prColor 
 }
