c86f7b871d99a7e7fded634c2a29b722f1537125
kaleem99 <kaleem1999@outlook.com>
2021-12-09 12:49:20 +0200

    fixed file structure

--- frontend/src/components/regions/UserFeedbackChart/Presentation.js
+++ frontend/src/components/regions/UserFeedbackChart/Presentation.js
@@ -1,6 +1,56 @@
 import React from "react";
-import UserChart from "./assets";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import { data } from "./CardDataStats";
 
+data.map(
+  (date) =>
+    (date.timestamp = new Date(date.timestamp).toISOString().slice(0, 10))
+);
 export default () => {
-  return <UserChart />;
+  return (
+    <div>
+      <LineChart
+        width={1000}
+        height={500}
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 100,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="timestamp" interval="preserveEnd" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="cardsTotalCount"
+          stroke="black"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="projectCardsTotalCount" stroke="green" />
+        <Line
+          type="monotone"
+          dataKey="cardsInCompleteColumnTotalCount"
+          stroke="blue"
+        />
+        <Line
+          type="monotone"
+          dataKey="projectCardsInCompleteColumnTotalCount"
+          stroke="orange"
+        />
+      </LineChart>
+    </div>
+  );
 };
