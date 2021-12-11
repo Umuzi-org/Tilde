885752bdd9930a870c613f26fc5e0465ab652a52
Faith Mofokeng <faithmofokeng5@gmail.com>
2021-12-11 13:27:33 +0200

    used a color palette

--- frontend/src/components/regions/UsersAndGroups/utils.js
+++ frontend/src/components/regions/UsersAndGroups/utils.js
@@ -1,27 +1,33 @@
-const timeNow = new Date()
-const milliseconds = 1000 * 60 * 60 * 24
-const timeDifferenceInDays = (time) => {return Math.ceil(Math.abs(timeNow - new Date(time)) / milliseconds)}
-let prColor
+const palette = {
+  warning: "#ff9800",
+  error: "#ef5350",
+  default: "#212121",
+};
+const timeNow = new Date();
+const milliseconds = 1000 * 60 * 60 * 24;
+const timeDifferenceInDays = (time) => {
+  return Math.ceil(Math.abs(timeNow - new Date(time)) / milliseconds);
+};
+let prColor;
 
 export const getPrColor = (oldestOpenPrTime) => {
-    if (timeDifferenceInDays(oldestOpenPrTime) === 1){
-        prColor = "black"
-    }else if(timeDifferenceInDays(oldestOpenPrTime) <= 2) {
-        prColor = "orange"
-    }else {
-        prColor = "red"
-    }
-    return prColor 
-}
+  if (timeDifferenceInDays(oldestOpenPrTime) === 1) {
+    prColor = palette.default;
+  } else if (timeDifferenceInDays(oldestOpenPrTime) <= 2) {
+    prColor = palette.warning;
+  } else {
+    prColor = palette.error;
+  }
+  return prColor;
+};
 
 export const getTildeReviewColor = (oldestOpenPrTime) => {
-    if(timeDifferenceInDays(oldestOpenPrTime) === 1){
-        prColor = "black"
-    }
-    else if(timeDifferenceInDays(oldestOpenPrTime) <= 3) {
-        prColor = "orange"
-    }else {
-        prColor = "red"
-    }
-    return prColor 
-}
+  if (timeDifferenceInDays(oldestOpenPrTime) === 1) {
+    prColor = palette.default;
+  } else if (timeDifferenceInDays(oldestOpenPrTime) <= 3) {
+    prColor = palette.warning;
+  } else {
+    prColor = palette.error;
+  }
+  return prColor;
+};
