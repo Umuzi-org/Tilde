ca3c875ad7a6f7bb727e59e15f793d7c0c02cc7b
Sheena <sheena.oconnell@gmail.com>
2021-12-19 15:32:27 +0200

    works

--- frontend/src/apiAccess/redux/actions.js
+++
@@ -1,7 +0,0 @@
-import types from "./types.js";
-
-export default {
-  addEntitiesToStoreAsObject: ({ data, objectType }) => {
-    return { type: types.ADD_API_PAGED_OBJECTS_TO_STORE, data, objectType };
-  },
-};
