305486100e2003819cdf349480900c656b9322a3
Sbonelo01 <sbonelomkhize15@gmail.com>
2022-01-20 18:52:10 +0200

    ReviewPopUp component

---
+++ frontend/src/components/regions/CardDetails/ReviewPopUp.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Typography,
+} from "@material-ui/core";
+
+const ReviewPopUp = (props) => {
+  const { title, children, openReviewPopUp, setOpenReviewPopUp } = props;
+  return (
+    <Dialog open={openReviewPopUp}>
+      <DialogTitle>
+        <div>
+          <Typography variant="h6" component="div">
+            {title}
+          </Typography>
+        </div>
+      </DialogTitle>
+      <DialogContent dividers>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default ReviewPopUp
