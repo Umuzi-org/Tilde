7c2d1914c0b27f2f28f9f33dc34b53d48c4afdc0
Sbonelo01 <sbonelomkhize15@gmail.com>
2022-01-25 14:07:01 +0200

    adding a "utils.test.js" files to the right place

---
+++ frontend/src/components/regions/CardDetails/utils.test.js
@@ -0,0 +1,9 @@
+import { trimLongReview } from "./utils";
+
+test("A long review should be shortend", () => {
+  const longReview =
+    "This is a very decriptive, long code review\n that can become hard to read.\n We need to shorten it";
+  expect(trimLongReview(longReview)).toBe(
+    "This is a very decriptive, long code review"
+  );
+});
