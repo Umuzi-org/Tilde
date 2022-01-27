7a556bdf157426b42be2f16d3060b27a0c8af32f
FaithMo <faith.mofokeng@umuzi.org>
2022-01-27 21:43:52 +0200

    made statuses constant

--- frontend/src/components/regions/UsersAndGroups/utils.js
+++ frontend/src/components/regions/UsersAndGroups/utils.js
@@ -4,6 +4,10 @@ const TILDE_ERROR_AGE_THRESHOLD = 3
 const TILDE_WARNING_AGE_THRESHOLD = 2
 const TILDE_DEFAULT_AGE_THRESHOLD = 0
 
+const ERROR_STATUS = "error"
+const WARNING_STATUS = "warning"
+const DEFAULT_STATUS = "default"
+
 export const timeDifferenceInDays = (time) => {
   return Math.ceil(Math.abs(new Date() - new Date(time)) / (1000 * 60 * 60 * 24) - 1)
 }
@@ -11,21 +15,21 @@ export const timeDifferenceInDays = (time) => {
 export const getPrStatus = (oldestOpenPrTime) => {
   const ageInDays = timeDifferenceInDays(oldestOpenPrTime)
   if (ageInDays >= PR_ERROR_AGE_THRESHOLD){
-    return "error";
+    return ERROR_STATUS;
   }
   if(ageInDays >= PR_WARNING_AGE_THRESHOLD){
-    return "warning"
+    return WARNING_STATUS
   }
-  return "default"
+  return DEFAULT_STATUS
 };
 
 export const getTildeReviewStatus = (oldestOpenTildeReviewTime) => {
   const ageInDays = timeDifferenceInDays(oldestOpenTildeReviewTime)
   if (ageInDays >= TILDE_ERROR_AGE_THRESHOLD){
-    return "error";
+    return ERROR_STATUS;
   }
   if(ageInDays <= TILDE_WARNING_AGE_THRESHOLD && ageInDays > TILDE_DEFAULT_AGE_THRESHOLD){
-    return "warning"
+    return WARNING_STATUS
   }
-  return "default"
+  return DEFAULT_STATUS
 };
