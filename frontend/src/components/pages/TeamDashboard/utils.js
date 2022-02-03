bbf02fa2f1e8ae8fb1dbf44a488a8a26e42af2dd
Babalwa01 <babalwambolekwa93@gmail.com>
2022-02-03 07:23:51 +0200

    #300 fixed minimum and maximum date function

--- frontend/src/components/pages/TeamDashboard/utils.js
+++ frontend/src/components/pages/TeamDashboard/utils.js
@@ -1,10 +1,18 @@
 export const getMinAndMaxDate = ({ activityLogDayCounts }) => {
-  const sortedActivityLogDayCounts = activityLogDayCounts.sort(
+  const datesArray = [];
+  for (let i in activityLogDayCounts) {
+    activityLogDayCounts[i].forEach((activityLogDayCount) => {
+      datesArray.push(activityLogDayCount.date);
+    });
+  }
+
+  const sortedActivityLogDayCounts = datesArray.sort(
     (a, b) => new Date(a) - new Date(b)
   );
-  const minimumDate = sortedActivityLogDayCounts[0].date;
+
+  const minimumDate = sortedActivityLogDayCounts[0];
   const maximumDate =
-    sortedActivityLogDayCounts[sortedActivityLogDayCounts.length - 1].date;
+    sortedActivityLogDayCounts[sortedActivityLogDayCounts.length - 1];
 
   return {
     minimumDate,
