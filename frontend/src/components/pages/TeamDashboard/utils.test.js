bab935fe8c2a5c09827bc3f8add7dc9a471fa0f9
Babalwa01 <babalwambolekwa93@gmail.com>
2022-02-03 07:30:55 +0200

    #300 getMinAndMaxDate function tests made to pass

--- frontend/src/components/pages/TeamDashboard/utils.test.js
+++ frontend/src/components/pages/TeamDashboard/utils.test.js
@@ -1,30 +1,80 @@
 import { getMinAndMaxDate } from "./utils";
 
 test("getMinAndMaxDate returns the correct minimum and maximum dates", () => {
-  const activityLogDayCounts = [
-    {
-      id:
-        "date=2020-04-28&limit=20&offset=0&event_type__name=COMPETENCE_REVIEW_DONE&actor_user=236",
-      date: "2020-04-28",
-      total: 1,
-    },
-    {
-      id:
-        "date=2020-04-28&limit=20&offset=0&event_type__name=COMPETENCE_REVIEW_DONE&actor_user=2360",
-      date: "2020-04-29",
-      total: 1,
-    },
-    {
-      id:
-        "date=2020-04-28&limit=20&offset=0&event_type__name=COMPETENCE_REVIEW_DONE&actor_user=2360",
-      date: "2021-04-29",
-      total: 1,
-    },
-  ];
+  const activityLogDayCounts = {
+    84: [
+      {
+        date: "2021-07-14",
+        COMPETENCE_REVIEW_DONE: 5,
+        PR_REVIEWED: 3,
+      },
+      {
+        date: "2021-07-16",
+        COMPETENCE_REVIEW_DONE: 6,
+        PR_REVIEWED: 20,
+      },
+      {
+        date: "2021-07-17",
+        COMPETENCE_REVIEW_DONE: 20,
+        PR_REVIEWED: 0,
+      },
+      {
+        date: "2021-07-18",
+        COMPETENCE_REVIEW_DONE: 20,
+        PR_REVIEWED: 0,
+      },
+      {
+        date: "2021-07-19",
+        COMPETENCE_REVIEW_DONE: 20,
+        PR_REVIEWED: 0,
+      },
+      {
+        date: "2021-07-20",
+        COMPETENCE_REVIEW_DONE: 20,
+        PR_REVIEWED: 0,
+      },
+    ],
+
+    26: [
+      {
+        date: "2021-07-15",
+        COMPETENCE_REVIEW_DONE: 5,
+        PR_REVIEWED: 3,
+      },
+      {
+        date: "2021-07-16",
+        COMPETENCE_REVIEW_DONE: 6,
+        PR_REVIEWED: 2,
+      },
+      {
+        date: "2021-07-17",
+        COMPETENCE_REVIEW_DONE: 20,
+        PR_REVIEWED: 1,
+      },
+    ],
+
+    132: [
+      {
+        date: "2021-07-15",
+        COMPETENCE_REVIEW_DONE: 50,
+        PR_REVIEWED: 3,
+      },
+      {
+        date: "2021-07-16",
+        COMPETENCE_REVIEW_DONE: 20,
+        PR_REVIEWED: 2,
+      },
+      {
+        date: "2021-07-17",
+        COMPETENCE_REVIEW_DONE: 1,
+        PR_REVIEWED: 1,
+      },
+    ],
+  };
 
   const { minimumDate, maximumDate } = getMinAndMaxDate({
     activityLogDayCounts,
   });
-  expect(minimumDate).toBe("2020-04-28");
-  expect(maximumDate).toBe("2021-04-29");
+  expect(minimumDate).toBe("2021-07-14");
+  expect(maximumDate).toBe("2021-07-20");
 });
