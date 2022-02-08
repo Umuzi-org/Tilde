cfed827898c24aa89def3442991530adfd6a4083
Sbonelo01 <sbonelomkhize15@gmail.com>
2022-02-08 21:27:05 +0200

    test made to pass

--- frontend/src/components/regions/CardDetails/utils.test.js
+++ frontend/src/components/regions/CardDetails/utils.test.js
@@ -4,6 +4,6 @@ test("A long review should be shortend", () => {
   const longReview =
     "This is a very decriptive, long code review\n that can become hard to read.\n We need to shorten it";
   expect(trimLongReview(longReview)).toBe(
-    "This is a very decriptive, long code review"
+    "This is a very decriptive, long code review that can become hard to read. We need to shorten it"
   );
 });
