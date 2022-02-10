7561057f7deee7741dac8966ecc7aac7de06230a
kaleem99 <kaleem1999@outlook.com>
2022-02-10 14:28:33 +0200

    added router to story

--- frontend/src/stories/TeamsTable.stories.js
+++ frontend/src/stories/TeamsTable.stories.js
@@ -1,16 +1,19 @@
 import React from "react";
 import TeamsTable from "../components/regions/UserDetails/TeamsTable/Presentation";
 import authUser from "./fixtures/authUser.json";
+import user from "./fixtures/user.json";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default {
     title: "Tilde/UserTeamsTable",
     component: TeamsTable,
 }
 
-const Template = args => <TeamsTable {...args} />
+const Template = args => <Router><TeamsTable {...args} /></Router>
 
 export const Primary = Template.bind({});
 Primary.args = {
-  teams: authUser.teamMemberships,
-  authUser: authUser
+  teams: user.teamMemberships,
+  authUser
 }
+
