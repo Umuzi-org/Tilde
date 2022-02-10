db773bf09903f968f915bfed9a069a6872b64c2a
Babalwa01 <babalwambolekwa93@gmail.com>
2022-02-10 10:16:16 +0200

    #347 getAgeString function tests updated

--- frontend/src/components/widgets/utils.test.js
+++ frontend/src/components/widgets/utils.test.js
@@ -6,49 +6,37 @@ test("getAgeString function doesn't break if null date", () => {
 });
 
 test("getAgeString function returns correct string when age is 1 month", () => {
-  const date = new Date();
-  const card = {
-    oldestOpenPrUpdatedTime: date.setDate(date.getDate() - 31),
-  };
-  expect(getAgeString(card.oldestOpenPrUpdatedTime)).toBe("1 month ago");
+  const today = new Date();
+  const date = today.setDate(today.getDate() - 31);
+  expect(getAgeString(date)).toBe("1 month ago");
 });
 
 test("getAgeString function returns correct string when age is 2 weeks", () => {
-  const date = new Date();
-  const card = {
-    oldestOpenPrUpdatedTime: date.setDate(date.getDate() - 14),
-  };
-  expect(getAgeString(card.oldestOpenPrUpdatedTime)).toBe("2 weeks ago");
+  const today = new Date();
+  const date = today.setDate(today.getDate() - 14);
+  expect(getAgeString(date)).toBe("2 weeks ago");
 });
 
 test("getAgeString function returns correct string when age is 2 days", () => {
-  const date = new Date();
-  const card = {
-    oldestOpenPrUpdatedTime: date.setDate(date.getDate() - 2),
-  };
-  expect(getAgeString(card.oldestOpenPrUpdatedTime)).toBe("2 days ago");
+  const today = new Date();
+  const date = today.setDate(today.getDate() - 2);
+  expect(getAgeString(date)).toBe("2 days ago");
 });
 
 test("getAgeString function returns correct string when age is 5 hours", () => {
-  const date = new Date();
-  const card = {
-    oldestOpenPrUpdatedTime: date.setHours(date.getHours() - 5),
-  };
-  expect(getAgeString(card.oldestOpenPrUpdatedTime)).toBe("5 hours ago");
+  const today = new Date();
+  const date = today.setHours(today.getHours() - 5);
+  expect(getAgeString(date)).toBe("5 hours ago");
 });
 
 test("getAgeString function returns correct string when age is 30 minutes", () => {
-  const date = new Date();
-  const card = {
-    oldestOpenPrUpdatedTime: date.setMinutes(date.getMinutes() - 30),
-  };
-  expect(getAgeString(card.oldestOpenPrUpdatedTime)).toBe("30 minutes ago");
+  const today = new Date();
+  const date = today.setMinutes(today.getMinutes() - 30);
+  expect(getAgeString(date)).toBe("30 minutes ago");
 });
 
 test("getAgeString function returns correct string when age is less than a minute", () => {
-  const date = new Date();
-  const card = {
-    oldestOpenPrUpdatedTime: date.setSeconds(date.getSeconds() - 15),
-  };
-  expect(getAgeString(card.oldestOpenPrUpdatedTime)).toBe("just now");
+  const today = new Date();
+  const date = today.setSeconds(today.getSeconds() - 15);
+  expect(getAgeString(date)).toBe("just now");
 });
