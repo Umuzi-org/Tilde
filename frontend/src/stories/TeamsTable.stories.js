6c3c0da77b29d5aaa544acb4df00fc4cc3033b07
Kaleem Mohammad <62307224+kaleem99@users.noreply.github.com>
2022-02-15 08:41:40 +0200

    fixed broken story userTeamsTable (#342)
    * fixed broken link
    
    * fixed bug
    
    * removed print statement
    
    * fixed changes
    
    * small changes to import
    
    * added router to story

--- frontend/src/stories/TeamsTable.stories.js
+++ frontend/src/stories/TeamsTable.stories.js
@@ -1,15 +1,19 @@
 import React from "react";
 import TeamsTable from "../components/regions/UserDetails/TeamsTable/Presentation";
+import authUser from "./fixtures/authUser.json";
 import user from "./fixtures/user.json";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default {
     title: "Tilde/UserTeamsTable",
     component: TeamsTable,
 }
 
-const Template = args => <TeamsTable {...args} />
+const Template = args => <Router><TeamsTable {...args} /></Router>
 
 export const Primary = Template.bind({});
 Primary.args = {
   teams: user.teamMemberships,
+  authUser
 }
+
