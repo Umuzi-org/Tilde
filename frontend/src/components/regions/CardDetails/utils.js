c18637e518c788649b2839d7c365127420914a8e
Sbonelo01 <sbonelomkhize15@gmail.com>
2022-02-15 21:35:53 +0200

    added a function to clean "Markdown" characters

--- frontend/src/components/regions/CardDetails/utils.js
+++ frontend/src/components/regions/CardDetails/utils.js
@@ -1,3 +1,15 @@
 export const trimLongReview = (review) => {
-  return review.split("\n").join("");
+  return review.split("\n").join(" ");
 };
+
+/*
+  The reason markdown is removed from a short review is because we want to keep
+  the "summary review" of a long review clean an streamlined (like gmail) - so we 
+  return a plain string instead of markdown.
+  The clean markdown function attempts to clean the "summary review", leaving it
+  markdown free while ignoring common english puntuation marks.
+*/
+ 
+export const cleanMarkdown = (review) => {
+  return review.replace(/[^A-Za-z;!.,:]/g, " ");
+}
