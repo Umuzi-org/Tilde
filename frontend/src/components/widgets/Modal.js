ad38d820c26429389ac8c0a6c98646894650e59f
Babalwa01 <babalwambolekwa93@gmail.com>
2022-03-09 20:28:52 +0200

    #402 started

--- frontend/src/components/widgets/Modal.js
+++ frontend/src/components/widgets/Modal.js
@@ -2,6 +2,9 @@ import React from "react";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Box from "@material-ui/core/Box";
 
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -35,6 +38,11 @@ export default ({ open, onClose, children }) => {
         },
       }}
     >
+      {/* <Box>
+        <IconButton onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      </Box> */}
       <Fade in={open}>{children}</Fade>
     </Modal>
   );
