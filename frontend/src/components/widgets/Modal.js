c0fda29bd3cc66964d8941d18f8e830cb994dcba
Babalwa01 <babalwambolekwa93@gmail.com>
2022-03-11 12:36:31 +0200

    #402 added functionality for close button

--- frontend/src/components/widgets/Modal.js
+++ frontend/src/components/widgets/Modal.js
@@ -2,6 +2,8 @@ import React from "react";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
+import CloseIcon from "@material-ui/icons/Close";
+// import { Typography } from "@material-ui/core";
 
 import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
@@ -12,8 +14,18 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     justifyContent: "center",
   },
+
   backDrop: {
     background: "rgba(0,0,0,0.15)",
+  },
+
+  exitIcon: {
+    // top: "5px",
+    // right: "1px",
+    // backgroundColor: "white",
+    // "&:hover": {
+    //   backgroundColor: "white",
+    // },
   },
 }));
 
@@ -36,8 +48,16 @@ export default ({ open, onClose, children }) => {
         },
       }}
     >
-      <Fade in={open}>{children}</Fade>
-      <Button variant="outlined" onClick={onClose}>X</Button>
+      <React.Fragment>
+        <Fade in={open}>{children}</Fade>
+        <Button
+          variant="outlined"
+          onClick={onClose}
+          className={classes.exitIcon}
+        >
+          <CloseIcon />
+        </Button>
+      </React.Fragment>
     </Modal>
   );
 };
