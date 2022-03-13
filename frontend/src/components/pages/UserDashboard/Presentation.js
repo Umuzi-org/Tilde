7ce6065a1f7363a6b37d86d149e9827e15f89d07
Khalid Ebrahim <khalid.ebrahim@umuzi.org>
2022-03-13 12:56:15 -0700

    Fixed Rogue comment

--- frontend/src/components/pages/UserDashboard/Presentation.js
+++ frontend/src/components/pages/UserDashboard/Presentation.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Grid, Paper } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-// import TeamsTable from "../UserDetails/TeamsTable";
 import TeamsTable from "../../regions/UserDetails/TeamsTable";
 import UserDetailedStats from "./UserDetailedStats";
 
