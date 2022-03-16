31fa73a9e37cf650c35f6439bf4d565e8138bcbc
FaithMo <faith.mofokeng@umuzi.org>
2022-03-16 14:17:28 +0200

    changes file externtions

--- frontend/src/components/pages/UsersAndGroups/index.jsx
+++ frontend/src/components/pages/UsersAndGroups/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import Presentation from "./Presentation.js";
+import Presentation from "./Presentation.jsx";
 import { apiReduxApps } from "../../../apiAccess/apiApps";
 
 import useMaterialUiFormState from "../../../utils/useMaterialUiFormState";
