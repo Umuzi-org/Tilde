80030afc3fcceba70244def3fb5f7b354bf37d45
Sheena <sheena.oconnell@gmail.com>
2022-03-29 15:53:35 +0200

    typo

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -37,9 +37,9 @@ router.register(r"teams", core_views.TeamViewSet, "team")
 
 
 router.register(
-    r"managment_actions",
+    r"management_actions",
     curriculum_tracking_api_views.ManagementActionsViewSet,
-    "managmentaction-list",
+    "managementaction-list",
 )
 
 router.register(
