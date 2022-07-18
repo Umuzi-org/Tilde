5335f1a62e88332c791d09c2ccbe1e27b5b08002
FaithMo <faith.mofokeng@umuzi.org>
2022-07-18 12:09:20 +0200

    cleaned

--- frontend/src/components/widgets/Button.jsx
+++ frontend/src/components/widgets/Button.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import LaunchIcon from "@material-ui/icons/Launch";
 
 const useStyles = makeStyles((theme) => ({
   button: {
-    color: "red",
+    // color: "red",
     border: "1px solid grey",
     borderRadius: theme.spacing(0.5),
     borderColor: theme.palette.grey[500],
@@ -13,8 +14,9 @@ const useStyles = makeStyles((theme) => ({
 
 export default ({ children }) => {
   const classes = useStyles();
+  console.log(Button);
   return (
-    <Button className={classes.button} displayName={"display"}>
+    <Button className={classes.button} displayName={"display"} startIcon={""}>
       {children}
     </Button>
   );
