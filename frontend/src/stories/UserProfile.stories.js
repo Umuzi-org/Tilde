058445d43e8940ac5f5dffddea3aa97182dd44e7
Sbonelo01 <sbonelomkhize15@gmail.com>
2022-08-01 21:40:38 +0200

    set up story

---
+++ frontend/src/stories/UserProfile.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Presentation from "../components/pages/UserProfile/Presentation";
+
+export default {
+    title: "Tilde/userProfile",
+    component: Presentation,
+};
+
+export const Primary = () => <Presentation />;
