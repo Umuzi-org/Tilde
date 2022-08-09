10563a0e2f350437b758466b7aed7ef481738103
KhalidUmuzi <100670662+KhalidUmuzi@users.noreply.github.com>
2022-08-09 11:27:56 +0200

    Upgrade React to v18.2 (#574)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import "typeface-roboto";
@@ -7,15 +7,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import { store } from "./redux";
 import { Provider } from "react-redux";
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
+root.render(
   //   <React.StrictMode>
   <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
