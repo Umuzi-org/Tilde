403b6d1639fb9f905a502d44d4ee5e75dc016d79
Ngoakor12 <ngoakor12@gmail.com>
2022-08-17 19:43:07 +0200

    looking better

--- frontend/src/components/widgets/UserAvatarLink.jsx
+++ frontend/src/components/widgets/UserAvatarLink.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Avatar from "@material-ui/core/Avatar";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/styles";
+import { getUrl as getUserBoardUrl } from "./LinkToUserBoard";
+
+const useStyles = makeStyles({
+  avatarLink: {
+    textDecoration: "none",
+  },
+  container: {
+    display: "flex",
+    alignItems: "center",
+    gap: 8,
+  },
+});
+
+// TODO: helper for getting the two letters for avatar
+// - if the user has an umuzi email:
+//      - use get the first letter in the firstname and lastname of the email
+// - else:
+//      - use first two characters of the email
 
 function UserAvatarLink({ email, userId }) {
+  const classes = useStyles();
   return (
-    <div>
-      <Avatar>{email.slice(0, 2).toUpperCase()}</Avatar>
+    <div className={classes.container}>
+      <Link to={getUserBoardUrl({ userId })} className={classes.avatarLink}>
+        <Avatar>{email.slice(0, 2).toUpperCase()}</Avatar>
+      </Link>
       <Typography>{email}</Typography>
     </div>
   );
--- frontend/src/stories/UserAvatarLink.stories.js
+++ frontend/src/stories/UserAvatarLink.stories.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import UserAvatarLink from "../components/widgets/UserAvatarLink.jsx";
 
 export default {
   title: "Tilde/UserAvatarLink",
   component: UserAvatarLink,
 };
-const Template = (args) => <UserAvatarLink {...args} />;
+
+const Template = (args) => (
+  <Router>
+    <UserAvatarLink {...args} />
+  </Router>
+);
 
 export const Default = Template.bind({});
+
 Default.args = {
   email: "ngoako.ramokgopa@umuzi.org",
   userId: 5,
