1e29ed63a670c047d905782fb3c0e3364c0e443a
Ngoakor12 <ngoakor12@gmail.com>
2022-08-17 12:56:29 +0200

    setup a basic component with storybook

---
+++ frontend/src/components/widgets/UserAvatarLink.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserAvatarLink() {
+  return <h1>user avatar link</h1>;
+}
+
+export default UserAvatarLink;
---
+++ frontend/src/stories/UserAvatarLink.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import UserAvatarLink from "../components/widgets/UserAvatarLink.jsx";
+
+export default {
+  title: "Tilde/UserAvatarLink",
+  component: UserAvatarLink,
+};
+const Template = () => <UserAvatarLink />;
+
+export const Default = Template.bind({});
