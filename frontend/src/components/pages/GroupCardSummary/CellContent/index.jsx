8eaec3d2d079b1164661fb9b9bc5c9d9752a52d5
Sbonelo01 <sbonelomkhize15@gmail.com>
2022-08-23 09:01:00 +0200

    Dashboard renders

--- frontend/src/components/pages/GroupCardSummary/CellContent/index.jsx
+++ frontend/src/components/pages/GroupCardSummary/CellContent/index.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
+import { useTraceUpdate } from "../../../../hooks";
 
 import Presentation from "./Presentation";
 
-function SummaryCardUnconnected ({ card }) {
+function SummaryCardUnconnected({ card }) {
+
+  useTraceUpdate({ card });
+
   if (card === undefined) return <div></div>;
 
   const props = {
     card,
   };
   return <Presentation {...props} />;
-};
+}
 
 const mapStateToProps = (state) => {
   return {};
