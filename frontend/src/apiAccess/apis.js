547afb4458975bbd263d6542b625af70db84409d
FaithMo <faith.mofokeng@umuzi.org>
2022-08-31 10:09:03 +0200

    include the effected user

--- frontend/src/apiAccess/apis.js
+++ frontend/src/apiAccess/apis.js
@@ -353,7 +353,12 @@ async function setProjectLinkSubmission({ cardId, linkSubmission }) {
   return { response, responseData };
 }
 
-async function activityLogDayCountsPage({ eventTypeName, actorUser, page }) {
+async function activityLogDayCountsPage({
+  eventTypeName,
+  actorUser,
+  effectedUser,
+  page,
+}) {
   const limit = 20;
   const offset = calculateOffset({ page, limit });
   let params = {
@@ -363,6 +368,7 @@ async function activityLogDayCountsPage({ eventTypeName, actorUser, page }) {
 
   if (eventTypeName) params["event_type__name"] = eventTypeName;
   if (actorUser) params["actor_user"] = actorUser;
+  if (effectedUser) params["effected_user"] = effectedUser;
   const getParams = objectToGetQueryString(params);
 
   const url = `${API_BASE_URL}/api/activity_log_day_counts/?${getParams}`;
