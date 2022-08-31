75d3ff9be3a4ed722db638b5deca4b5162e591a4
FaithMo <faith.mofokeng@umuzi.org>
2022-08-31 09:26:38 +0200

    used a graph

--- frontend/src/components/widgets/EntryListEndpoint.jsx
+++ frontend/src/components/widgets/EntryListEndpoint.jsx
@@ -42,20 +42,64 @@ const result = getActivityLogCountsByDayForSingleUser({
   ],
 });
 const useStyles = makeStyles((theme) => ({}));
-console.log("hello", result[0].COMPETENCE_REVIEW_DONE);
+const data = [
+  {
+    s: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    s: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    s: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    s: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    s: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    s: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    s: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+console.log("hello", data);
 
 export default ({ args }) => {
   const classes = useStyles();
   // console.log("hello", UserActions({ args }));
   // return <Grid>hello</Grid>;
   return (
-    <div className={classes.column}>
-      <ResponsiveContainer>
-        <BarChart width={150} height={40} data={result}>
-          dl
-          {/* <Bar dataKey="uv" fill="#8884d8" /> fkj */}
-        </BarChart>
-      </ResponsiveContainer>
+    <div>
+      {/* <ResponsiveContainer> */}
+      <BarChart width={150} height={40} data={data}>
+        <Bar dataKey="uv" fill="#8884d8" />
+      </BarChart>
+      {/* </ResponsiveContainer> */}
     </div>
   );
 };
