6421c6333f033645909dd670efc65f77c9613afe
Sbonelo01 <sbonelomkhize15@gmail.com>
2022-09-09 14:31:10 +0200

    Prettier

--- frontend/src/components/regions/UserActions/Presentation.jsx
+++ frontend/src/components/regions/UserActions/Presentation.jsx
@@ -4,7 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import Loading from "../../widgets/Loading";
 import { makeStyles } from "@material-ui/core/styles";
 import DayLog from "./DayLog";
-import ActivityDashboardBarGraph from "../../pages/UserDashboard/ActivityDashboardBarGraph";
+import ActivityDashboardBarGraph from "./ActivityDashboardBarGraph";
 
 const useStyles = makeStyles((theme) => ({
   column: {
