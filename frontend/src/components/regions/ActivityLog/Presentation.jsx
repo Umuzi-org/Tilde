978e18d4f2c8bbc9906fe1c4fccc73225505ca15
FaithMo <faith.mofokeng@umuzi.org>
2022-09-09 15:48:15 +0200

    added sortByDates

--- frontend/src/components/regions/ActivityLog/Presentation.jsx
+++ frontend/src/components/regions/ActivityLog/Presentation.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 
 import { eventTypeColors } from "../../../colors";
@@ -15,114 +14,22 @@ const useStyles = makeStyles((theme) => ({
   fillerStyles: {
     height: "100%",
     textAlign: "right",
-    // width: "10%",
     display: "flex",
     flexDirection: "row",
   },
   labelStyles: {
-    // height: "100%",
     width: 20,
     color: "black",
-    // fontWeight: 900,
   },
   content: {
-    // height: "100%",
-    // padding: 10,
     width: "90%",
   },
 }));
 
-const eventTypesWithIds = [
-  {
-    id: 5,
-    name: "CARD_MOVED_TO_COMPLETE",
-  },
-  {
-    id: 6,
-    name: "CARD_MOVED_TO_REVIEW_FEEDBACK",
-  },
-  {
-    id: 3,
-    name: "CARD_REVIEW_REQUEST_CANCELLED",
-  },
-  {
-    id: 2,
-    name: "CARD_REVIEW_REQUESTED",
-  },
-  {
-    id: 1,
-    name: "CARD_STARTED",
-  },
-  {
-    id: 4,
-    name: "COMPETENCE_REVIEW_DONE",
-  },
-];
-const activity = [
-  {
-    id: 116283,
-    eventType: 2,
-    actorUser: 352,
-    effectedUser: 802,
-    object1ContentTypeName: "git_real | pull request review",
-    object1Id: 53534,
-    object2ContentTypeName: "git_real | repository",
-    object2Id: 7250,
-  },
-  {
-    id: 116282,
-    eventType: 1,
-    actorUser: 132,
-    effectedUser: 896,
-    object1ContentTypeName: "curriculum_tracking | recruit project review",
-    object1Id: 45489,
-    object2ContentTypeName: "curriculum_tracking | recruit project",
-    object2Id: 15112,
-  },
-  {
-    id: 116281,
-    eventType: 1,
-    actorUser: 132,
-    effectedUser: 1023,
-    object1ContentTypeName: "curriculum_tracking | recruit project review",
-    object1Id: 45488,
-    object2ContentTypeName: "curriculum_tracking | recruit project",
-    object2Id: 17024,
-  },
-];
-
-const two = activity.map((item, index) => {
-  let matchingIds = eventTypesWithIds.find(
-    (elem) => elem.id === item.eventType
-  );
-  const result = {
-    ...item,
-    eventName: matchingIds ? matchingIds.name : "",
-  };
-  return result;
-});
-
-const v = () => {
-  const arr = [];
-  for (const color in eventTypeColors) {
-    arr.push({ event: color, color: eventTypeColors[color] });
-  }
-  return arr;
-};
-const eventTypesWithColors = v();
-
-const three = two.map((item, index) => {
-  let matchingIds = eventTypesWithColors.find(
-    (elem) => elem.event === item.eventName
-  );
-  const result = {
-    ...item,
-    eventColor: matchingIds ? matchingIds.color : "",
-  };
-  return result;
-});
-
-function LogBar({ color, event, object1ContentTypeName }) {
+function sortByDates(data) {
+  data.sort((action1, action2) => action2.timestamp - action1.timestamp);
+}
+function LogBar({ color, event }) {
   const classes = useStyles();
   const adjustStyle = {
     backgroundColor: color,
@@ -133,13 +40,14 @@ function LogBar({ color, event, object1ContentTypeName }) {
         <span className={classes.labelStyles} style={adjustStyle} />
         <div className={classes.content}>
           <div>{event}</div>
-          <div>{object1ContentTypeName}</div>
+          {/* <div>{object1ContentTypeName}</div> */}
         </div>
       </Grid>
     </Grid>
   );
 }
-export default function Presentation() {
+export default ({ three }) => {
+  // console.log(sortByDates("xx", three));
   return (
     <div>
       {three.map((item) => (
@@ -151,4 +59,4 @@ export default function Presentation() {
       ))}
     </div>
   );
-}
+};
--- frontend/src/stories/ActivityLog.stories.jsx
+++ frontend/src/stories/ActivityLog.stories.jsx
@@ -27,32 +27,48 @@ const eventTypesWithIds = [
     name: "COMPETENCE_REVIEW_DONE",
   },
 ];
+
+const orderedDates = [
+  "Sunday 21/03/2021",
+  "Thursday 11/03/2021",
+  "Monday 08/03/2021",
+  "Wednesday 03/03/2021",
+  "Tuesday 02/03/2021",
+  "Monday 01/03/2021",
+];
+
 const activity = [
   {
-    id: 116283,
-    eventType: 2,
     actorUser: 352,
     effectedUser: 802,
+    eventColor: "#fb8c00",
+    eventName: "CARD_REVIEW_REQUESTED",
+    eventType: 2,
+    id: 116283,
     object1ContentTypeName: "git_real | pull request review",
     object1Id: 53534,
     object2ContentTypeName: "git_real | repository",
     object2Id: 7250,
   },
   {
-    id: 116282,
-    event_type: 1,
     actorUser: 132,
     effectedUser: 896,
+    eventColor: "#43a047",
+    eventName: "CARD_STARTED",
+    eventType: 1,
+    id: 116282,
     object1ContentTypeName: "curriculum_tracking | recruit project review",
     object1Id: 45489,
     object2ContentTypeName: "curriculum_tracking | recruit project",
     object2Id: 15112,
   },
   {
-    id: 116281,
-    event_type: 1,
     actorUser: 132,
     effectedUser: 1023,
+    eventColor: "#43a047",
+    eventType: 1,
+    id: 116281,
+    eventName: "CARD_STARTED",
     object1ContentTypeName: "curriculum_tracking | recruit project review",
     object1Id: 45488,
     object2ContentTypeName: "curriculum_tracking | recruit project",
@@ -68,6 +84,7 @@ const Template = (args) => <ActivityLog {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  activity,
+  three: activity,
+  orderedDates,
   eventTypesWithIds,
 };
