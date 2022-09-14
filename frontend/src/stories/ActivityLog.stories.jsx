42771b366518fcb13b548808de2fe19f97c16d41
FaithMo <faith.mofokeng@umuzi.org>
2022-09-14 13:10:06 +0200

    cleaned

--- frontend/src/stories/ActivityLog.stories.jsx
+++ frontend/src/stories/ActivityLog.stories.jsx
@@ -2,11 +2,7 @@ import React from "react";
 
 import ActivityLog from "../components/regions/ActivityLog/Presentation";
 import ActivityLogList from "./fixtures/activityLogList.json";
-const date = [
-  " 2022-09-05T10:46:16.221191Z",
-  "2022-09-02T13:46:16.295020Z",
-  "2022-09-01T15:18:03.743668Z",
-];
+
 export default {
   title: "Tilde/ActivityLog",
   component: ActivityLog,
@@ -17,5 +13,4 @@ const Template = (args) => <ActivityLog {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   eventList: ActivityLogList,
-  sortedDate: date,
 };
