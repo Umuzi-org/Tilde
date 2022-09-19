6091359114487b5fb9c68743738d4b58fd50c75e
KhalidUmuzi <khalid.ebrahim@umuzi.org>
2022-09-20 01:54:54 +0200

    added metric filters

--- frontend/src/components/regions/UserActions/UserBurndownStats.jsx
+++ frontend/src/components/regions/UserActions/UserBurndownStats.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   LineChart,
   Line,
@@ -9,7 +9,7 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-import { Typography } from "@material-ui/core";
+import { Typography, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import orange from "@material-ui/core/colors/orange";
 import green from "@material-ui/core/colors/green";
@@ -23,6 +23,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default ({ burnDownSnapshots }) => {
+  const [metricFilter, setMetricFilter] = useState("");
+  console.log(burnDownSnapshots);
   burnDownSnapshots.map(
     (burnDownSnapshot) =>
       (burnDownSnapshot.timestamp = new Date(burnDownSnapshot.timestamp)
@@ -36,38 +38,64 @@ export default ({ burnDownSnapshots }) => {
       <Typography variant="h6" component="h2">
         Burndown
       </Typography>
-      <ResponsiveContainer height={500} width="100%">
+      <Typography variant="h6" component="h1">
+        Filter by:
+        <Button onClick={() => setMetricFilter("TotalCards")}>
+          Total Cards
+        </Button>
+        <Button onClick={() => setMetricFilter("TotalProjects")}>
+          Total Projects
+        </Button>
+        <Button onClick={() => setMetricFilter("CompletedCards")}>
+          Completed Cards
+        </Button>
+        <Button onClick={() => setMetricFilter("CompletedProjects")}>
+          Completed Projects
+        </Button>
+        <Button onClick={() => setMetricFilter("")}>
+          Clear Filter
+        </Button>
+      </Typography>
+      <ResponsiveContainer height={500} width="80%">
         <LineChart data={burnDownSnapshots}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="timestamp" interval="preserveEnd" />
           <YAxis />
           <Tooltip />
           <Legend className={classes.legend} />
-          <Line
-            type="monotone"
-            dataKey="cardsTotalCount"
-            name="Total Cards"
-            stroke={orange[400]}
-            activeDot={{ r: 8 }}
-          />
-          <Line
-            type="monotone"
-            dataKey="projectCardsTotalCount"
-            name="Total Project Cards"
-            stroke={green[400]}
-          />
-          <Line
-            type="monotone"
-            dataKey="cardsInCompleteColumnTotalCount"
-            name="Completed Cards"
-            stroke={blue[400]}
-          />
-          <Line
-            type="monotone"
-            dataKey="projectCardsInCompleteColumnTotalCount"
-            name="Completed Project Cards"
-            stroke={red[400]}
-          />
+          {metricFilter === "TotalCards" || metricFilter === "" ? (
+            <Line
+              type="monotone"
+              dataKey="cardsTotalCount"
+              name="Total Cards"
+              stroke={orange[400]}
+              activeDot={{ r: 8 }}
+            />
+          ) : null}
+          {metricFilter === "TotalProjects" || metricFilter === "" ? (
+            <Line
+              type="monotone"
+              dataKey="projectCardsTotalCount"
+              name="Total Project Cards"
+              stroke={green[400]}
+            />
+          ) : null}
+          {metricFilter === "CompletedCards" || metricFilter === "" ? (
+            <Line
+              type="monotone"
+              dataKey="cardsInCompleteColumnTotalCount"
+              name="Completed Cards"
+              stroke={blue[400]}
+            />
+          ) : null}
+          {metricFilter === "CompletedProjects" || metricFilter === "" ? (
+            <Line
+              type="monotone"
+              dataKey="projectCardsInCompleteColumnTotalCount"
+              name="Completed Project Cards"
+              stroke={red[400]}
+            />
+          ) : null}
         </LineChart>
       </ResponsiveContainer>
     </React.Fragment>
