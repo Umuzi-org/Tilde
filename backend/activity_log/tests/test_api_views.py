8f51cdd0e3b7b1309e948c3ff339e3dc7d824486
Rokhuda <rokhuda@gmail.com>
2022-09-22 01:13:14 +0200

    lte and gte test case

--- backend/activity_log/tests/test_api_views.py
+++ backend/activity_log/tests/test_api_views.py
@@ -77,13 +77,23 @@ class TestActivityLogDayCountViewset(APITestCase, APITestCaseMixin):
         self.assertEqual(response.data[0]["total"], 1)
         self.assertEqual(response.data[0]["date"], str(self.yesterday.date()))
 
+    def test_list_api_filter_by_timestamp_lte(self):
+        url = f"{self.get_list_url()}?timestamp__lte={self.entry_yesterday_1.timestamp}"
+        response = self.client.get(url)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]["total"], 1)
+
+    def test_list_api_filter_by_timestamp_gte(self):
+        url = f"{self.get_list_url()}?timestamp__gte={self.entry_today_1.timestamp}"
+        response = self.client.get(url)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["total"], 2)
+
     def test_list_api_filter_by_timestamp_lte_gte(self):
-        url = f"{self.get_list_url()}?lte={self.entry_today_1.timestamp}&gte={self.entry_yesterday_1.timestamp}"
+        url = f"{self.get_list_url()}?timestamp__lte={self.entry_today_2.timestamp}&timestamp__gte={self.entry_yesterday_2.timestamp}"
         response = self.client.get(url)
         self.assertEqual(len(response.data), 3)
         self.assertEqual(response.data[0]["total"], 2)
-
-        
 
 
 class TestEventTypeViewSet(APITestCase, APITestCaseMixin):
@@ -92,8 +102,6 @@ class TestEventTypeViewSet(APITestCase, APITestCaseMixin):
 
     def verbose_instance_factory(self):
         return factories.EventTypeFactory(description="a party")
-
-  
 
 
 class TestLogEntryViewSet(APITestCase, APITestCaseMixin):
