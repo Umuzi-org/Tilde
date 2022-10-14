b93341d34326759272ff655b7e77169ee147d2a8
Ruddy <ruddy.riba@umuzi.org>
2022-10-14 11:01:46 +0200

    return status OK in empty actions

--- automarker/actions/java/prep-for-kata-self-test.mjs
+++ automarker/actions/java/prep-for-kata-self-test.mjs
@@ -7,14 +7,19 @@ export default class PrepForKataSelfTest extends Action {
   name = "prep for kata self test";
 
   action = async function () {
-    // nothing to do
+    return {
+      status: STATUS_OK,
+    };
   };
 
   testAction = async function ({ destinationPath }) {
+    console.log('ping', destinationPath)
+    
     const scriptPath = "./actions/java/prep-for-kata-self-test.sh";
     const command = `DESTINATION_PATH=${destinationPath} /bin/bash -c ${scriptPath}`;
-    const output = await shell.exec(command).stdout;
-
+    await shell.exec(command).stdout;
+    console.log('clone repo', command)
+    
     return {
       status: STATUS_OK,
     };
