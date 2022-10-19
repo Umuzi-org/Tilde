9e2425eaccbe496eea8346f44e8d0c966aa47427
Ngoakor12 <ngoakor12@gmail.com>
2022-10-19 20:00:33 +0200

    GlobalCodeReviewDashboard story displays as expected

--- frontend/src/stories/GlobalCodeReviewDashboard.stories.jsx
+++ frontend/src/stories/GlobalCodeReviewDashboard.stories.jsx
@@ -1,41 +1,81 @@
 import React from "react";
 import GlobalCodeReviewDashboard from "../components/pages/GlobalCodeReviewDashboard/Presentation";
-
-import codeReviewQueueProjects from "./fixtures/codeReviewQueueProjects.json";
+import { BrowserRouter as Router } from "react-router-dom";
+import competenceReviewProjects from "./fixtures/GlobalCodeReviewDashboard/competenceReviewQueueProjects.json";
+import pullRequestReviewProjects from "./fixtures/GlobalCodeReviewDashboard/pullRequestReviewQueueProjects.json";
 
 export default {
   title: "Tilde/pages/GlobalCodeReviewDashboard",
   component: GlobalCodeReviewDashboard,
 };
 
-const Template = (args) => <GlobalCodeReviewDashboard {...args} />;
+const Template = (args) => (
+  <Router>
+    <GlobalCodeReviewDashboard {...args} />
+  </Router>
+);
 
 export const Primary = Template.bind({});
 
-Primary.args = {
-  competenceReviewQueueProjects: codeReviewQueueProjects,
-  pullRequestReviewQueueProjects: codeReviewQueueProjects,
-  // filterIncludeTags: ["clustering"],
-  // filterExcludeTags: ["data structures"],
-
-  // filterIncludeFlavours: ["python"],
-  // filterExcludeFlavours: ["python"],
-
-  handleChangeFlavourFilter: () => {},
-  handleChangeTagFilter: () => {},
-  applyFilters: () => {},
-  selectedCompetenceOrderFilter: {
-    label: "review request time",
-    sortInAscendingOrder: () => {},
-    sortInDescendingOrder: () => {},
-    isSelected: true,
-    isAscending: false,
-  },
-  selectedPullRequestOrderFilter: {
+const initialPullRequestOrderFilters = [
+  {
     label: "last updated time",
     sortInAscendingOrder: () => {},
     sortInDescendingOrder: () => {},
     isSelected: true,
     isAscending: false,
   },
+];
+const initialCompetenceOrderFilters = [
+  {
+    label: "review request time",
+    sortInAscendingOrder: () => {},
+    sortInDescendingOrder: () => {},
+    isSelected: true,
+    isAscending: false,
+  },
+  {
+    label: "start time",
+    sortInAscendingOrder: () => {},
+    sortInDescendingOrder: () => {},
+    isSelected: false,
+    isAscending: false,
+  },
+  {
+    label: "positive reviews",
+    sortInAscendingOrder: () => {},
+    sortInDescendingOrder: () => {},
+    isSelected: false,
+    isAscending: false,
+  },
+];
+const selectedCompetenceOrderFilter = initialCompetenceOrderFilters[0];
+const selectedPullRequestOrderFilter = initialCompetenceOrderFilters[0];
+
+Primary.args = {
+  competenceReviewQueueProjects: competenceReviewProjects,
+  pullRequestReviewQueueProjects: pullRequestReviewProjects,
+  competenceReviewQueueLoading: false,
+  pullRequestReviewQueueLoading: false,
+  filterIncludeTags: ["clustering"],
+  filterExcludeTags: ["data structures"],
+  filterIncludeFlavours: ["python"],
+  filterExcludeFlavours: ["python"],
+  allFlavours: ["javascript", "python", "react", "java", "django"],
+  allTagNames: ["ncit", "decision-trees", "todo", "ncit", "internet"],
+  allTeamNames: ["A", "B", "C"],
+  filterIncludeAssigneeTeams: ["B"],
+  competenceOrderFilters: initialCompetenceOrderFilters,
+  pullRequestOrderFilters: initialPullRequestOrderFilters,
+  selectedCompetenceOrderFilter: selectedCompetenceOrderFilter,
+  selectedPullRequestOrderFilter: selectedPullRequestOrderFilter,
+  applyFilters: () => true,
+  setPullRequestOrderFilters: () => {},
+  fetchNextCompetenceReviewQueuePage: () => {},
+  fetchNextPullRequestQueuePage: () => {},
+  setCompetenceOrderFilters: () => {},
+  setSelectedCompetenceOrderFilter: () => {},
+  handleChangeFlavourFilter: () => {},
+  handleChangeTagFilter: () => {},
+  handleChangeAssigneeTeamFilter: () => {},
 };
