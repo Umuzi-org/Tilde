fbadfad8c9555d5370a5a574f4524c7cc26859fe
ngoakor12 <ngoakor12@gmail.com>
2022-10-27 14:16:28 +0200

    import matches styleguide

--- frontend/src/components/regions/AddCardReviewModal/Presentation.jsx
+++ frontend/src/components/regions/AddCardReviewModal/Presentation.jsx
@@ -15,7 +15,7 @@ import CardButton from "../../widgets/CardButton";
 import { makeStyles } from "@material-ui/core/styles";
 import StatusHelp from "./StatusHelp";
 import IconButton from "@material-ui/core/IconButton";
-import { FormHelperText } from "@material-ui/core";
+import FormHelperText from "@material-ui/core/FormHelperText";
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
