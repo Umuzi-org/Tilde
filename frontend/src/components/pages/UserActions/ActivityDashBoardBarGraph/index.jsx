472d279854db953ecb4fbc91d7a946d70b7240ef
Sbonelo01 <sbonelomkhize15@gmail.com>
2022-11-08 13:33:50 +0200

    formatting

--- frontend/src/components/pages/UserActions/ActivityDashBoardBarGraph/index.jsx
+++ frontend/src/components/pages/UserActions/ActivityDashBoardBarGraph/index.jsx
@@ -10,33 +10,30 @@ export function ActivityDashboardBarGraphUnconnected({
   eventTypes,
   activityLogDayCounts,
 }) {
-  const x = () => {
-    const userActivityLogs = [];
+  const userActivityLogs = [];
 
-    activityLogDayCounts.forEach((act) => {
-      eventTypes.forEach((ev) => {
-        if (ev.id === act.eventType) {
-          if (
-            !userActivityLogs.some(
-              (userActivityLog) => userActivityLog.date === act.date
-            )
-          ) {
-            const obj = {};
-            obj.date = act.date;
-            userActivityLogs.push(Object.assign(obj, { [ev.name]: act.total }));
-          } else {
-            const dateIndex = userActivityLogs.findIndex(
-              (userActivityLog) => userActivityLog.date === act.date
-            );
-            Object.assign(userActivityLogs[dateIndex], {
-              [ev.name]: act.total,
-            });
-          }
+  activityLogDayCounts.forEach((act) => {
+    eventTypes.forEach((ev) => {
+      if (ev.id === act.eventType) {
+        if (
+          !userActivityLogs.some(
+            (userActivityLog) => userActivityLog.date === act.date
+          )
+        ) {
+          const obj = {};
+          obj.date = act.date;
+          userActivityLogs.push(Object.assign(obj, { [ev.name]: act.total }));
+        } else {
+          const dateIndex = userActivityLogs.findIndex(
+            (userActivityLog) => userActivityLog.date === act.date
+          );
+          Object.assign(userActivityLogs[dateIndex], {
+            [ev.name]: act.total,
+          });
         }
-      });
+      }
     });
-    return userActivityLogs;
-  };
+  });
 
   const eventTypeNames = [];
 
