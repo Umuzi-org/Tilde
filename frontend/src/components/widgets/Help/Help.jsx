6496f733dba403490ebe41f082758f94cf21a2f9
Ngoako Ramokgopa <ngoakor12@gmail.com>
2022-11-09 10:29:03 +0200

    Add review modal upgrade (#628)
    * modal does not grow with large text and visual some updates
    
    * styling cleanup finished
    
    * Help widget matches style guide
    
    * Help widget matches style guide
    
    * replace useMaterialUiFormState with useState
    
    * show main error while writing a review
    
    * emty fields validation
    
    * remove unused code
    
    * update failed api call detection
    
    * remove close icon outline as per sheena's comment
    
    * show required asterix
    
    * use responseData error messages
    
    * helptext is the right color
    
    * refactor individual form field error detection
    
    * import matches styleguide
    
    * refactor individual form field error messages
    
    * fix spelling

--- frontend/src/components/widgets/Help/Help.jsx
+++ frontend/src/components/widgets/Help/Help.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import HelpIcon from "@material-ui/icons/Help";
 
 import Modal from "../Modal";
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-}));
-
 export default function Help({ buttonText, children }) {
-  const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = (event) => {
@@ -34,7 +26,6 @@ export default function Help({ buttonText, children }) {
         color="primary"
         startIcon={<HelpIcon />}
         onClick={handleClick}
-        className={classes.button}
       >
         {buttonText ? buttonText : "help"}
       </Button>
