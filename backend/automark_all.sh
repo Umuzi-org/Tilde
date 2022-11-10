7196661a8ad69cbe6e7ac1dbcbfb33575be5bbf1
Sheena O'Connell <sheena.oconnell@gmail.com>
2022-11-10 12:35:04 +0200

    a better way to run automarkers

--- backend/automark_all.sh
+++ backend/automark_all.sh
@@ -1,63 +1,73 @@
 while true
   do
     echo "Coding aptitude assessment challenge: Task 1"
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 1" "python" 0
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 1" "javascript" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 1" "python" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 1" "javascript" 0
 
     echo "Coding aptitude assessment challenge: Task 2"
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 2" "python" 0
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 2" "javascript" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 2" "python" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 2" "javascript" 0
 
     echo "Coding aptitude assessment challenge: Task 3"
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 3" "python" 0
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 3" "javascript" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 3" "python" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 3" "javascript" 0
 
     echo "Coding aptitude assessment challenge: Task 4"
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 4" "python" 0
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 4" "javascript" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 4" "python" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 4" "javascript" 0
 
     echo "Coding aptitude assessment challenge: Task 5"
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 5" "python" 0
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 5" "javascript" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 5" "python" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 5" "javascript" 0
 
     echo "Coding aptitude assessment challenge: Task 6"
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 6" "python" 0
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 6" "javascript" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 6" "python" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 6" "javascript" 0
 
     echo "Coding aptitude assessment challenge: Task 7"
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 7" "python" 0
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 7" "javascript" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 7" "python" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 7" "javascript" 0
 
     echo "Coding aptitude assessment challenge: Task 8"
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 8" "python" 0
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 8" "javascript" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 8" "python" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 8" "javascript" 0
 
     echo "Coding aptitude assessment challenge: Task 9"
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 9" "python" 0
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 9" "javascript" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 9" "python" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 9" "javascript" 0
 
     echo "Coding aptitude assessment challenge: Task 10"
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 10" "python" 0
-    python manage.py run_automarker "Coding aptitude assessment challenge: Task 10" "javascript" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 10" "python" 0
+    python manage.py run_specific_automarker "Coding aptitude assessment challenge: Task 10" "javascript" 0
 
     echo "simple-calculator part 1"
-    python manage.py run_automarker "simple-calculator part 1" "javascript" 0
-    python manage.py run_automarker "simple-calculator part 1" "python" 0
-    python manage.py run_automarker "simple-calculator part 1" "python,pytest" 0
+    python manage.py run_specific_automarker "simple-calculator part 1" "javascript" 0
+    python manage.py run_specific_automarker "simple-calculator part 1" "python" 0
+    python manage.py run_specific_automarker "simple-calculator part 1" "python,pytest" 0
 
     # echo "Person"
-    # python manage.py run_automarker "Person" "python" 1
-    # python manage.py run_automarker "Person" "javascript" 1
+    python manage.py run_specific_automarker "Person" "python" 1
+    python manage.py run_specific_automarker "Person" "javascript" 1
 
-    # echo "password checker"
+    echo "password checker"
+    python manage.py run_specific_automarker "password-checker" "javascript" 1
+
+    ##################
+    
+    python manage.py run_specific_automarker "Shopping cart calculations" "javascript" 1
+    python manage.py run_specific_automarker "Email random inspirational quote" "javascript" 1
+
+
+
+
     
     # echo "semitone difference - basic algorithm"
-    # python manage.py run_automarker "semitone difference - basic algorithm" "javascript" 1
+    python manage.py run_specific_automarker "semitone difference - basic algorithm" "javascript" 1
 
     # echo "Animals Part 2. Adding Tests"
-    # python manage.py run_automarker "Animals Part 2. Adding Tests" "javascript" 1
+    python manage.py run_specific_automarker "Animals Part 2. Adding Tests" "javascript" 1
 
     # echo "Level 1 coding challenges"
-    # python manage.py run_automarker "Level 1 coding challenges" "javascript" 1
+    python manage.py run_specific_automarker "Level 1 coding challenges" "javascript" 1
 done
 
