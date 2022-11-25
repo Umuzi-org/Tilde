986ccf3667e2e4e57bad421dc02d9ad1944a092d
KhalidUmuzi <100670662+KhalidUmuzi@users.noreply.github.com>
2022-11-25 08:02:09 +0200

    Redo empty line change round 2

--- backend/curriculum_tracking/serializers.py
+++ backend/curriculum_tracking/serializers.py
@@ -749,6 +749,8 @@ class BurnDownSnapShotSerializer(serializers.ModelSerializer):
             "cards_in_complete_column_total_count",
             "project_cards_in_complete_column_total_count",
         ]
+        
+        
 class ReviewTrustSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ReviewTrust
