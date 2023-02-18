5ec4d763939c3f8ef4f00b18202a1c45925d0b26
Sheena O'Connell <sheena.oconnell@gmail.com>
2023-02-18 11:55:40 +0200

    better docker image names

--- k8s-deploy/build-and-push-image.sh
+++ k8s-deploy/build-and-push-image.sh
@@ -3,7 +3,8 @@
 cd ../backend 
 
 GIT_COMMIT="$(git show --format="%h" --no-patch)"
+DATE_STR=$(date '+%Y-%m-%d-t-%H-%M')
 
-docker build -t gcr.io/umuzi-prod/tilde-backend:$GIT_COMMIT -t gcr.io/umuzi-prod/tilde-backend:latest .
-docker push gcr.io/umuzi-prod/tilde-backend:$GIT_COMMIT
+docker build -t gcr.io/umuzi-prod/tilde-backend:$GIT_COMMIT-$DATE_STR -t gcr.io/umuzi-prod/tilde-backend:latest .
+docker push gcr.io/umuzi-prod/tilde-backend:$GIT_COMMIT-$DATE_STR
 docker push gcr.io/umuzi-prod/tilde-backend:latest
