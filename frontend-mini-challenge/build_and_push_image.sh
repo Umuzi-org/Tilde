2308fab6bc7e809a373708423937b1f771158057
Sheena O'Connell <sheena.oconnell@gmail.com>
2023-04-13 09:05:19 +0200

    small fix to ZMC frontend deploy script

--- frontend-mini-challenge/build_and_push_image.sh
+++ frontend-mini-challenge/build_and_push_image.sh
@@ -3,7 +3,10 @@
 GIT_COMMIT="$(git show --format="%h" --no-patch)"
 DATE_STR=$(date '+%Y-%m-%d-t-%H-%M')
 
-docker build -t gcr.io/umuzi-prod/tilde-frontend-mini-challenge:$GIT_COMMIT-$DATE_STR -t gcr.io/umuzi-prod/tilde-backend:latest .
+docker build -t gcr.io/umuzi-prod/tilde-frontend-mini-challenge:$GIT_COMMIT-$DATE_STR -t gcr.io/umuzi-prod/tilde-frontend-mini-challenge:latest .
 
-# docker push gcr.io/umuzi-prod/tilde-frontend-mini-challenge:$GIT_COMMIT-$DATE_STR
-# docker push gcr.io/umuzi-prod/tilde-frontend-mini-challenge:latest
+docker push gcr.io/umuzi-prod/tilde-frontend-mini-challenge:$GIT_COMMIT-$DATE_STR
+docker push gcr.io/umuzi-prod/tilde-frontend-mini-challenge:latest
+
+
+
