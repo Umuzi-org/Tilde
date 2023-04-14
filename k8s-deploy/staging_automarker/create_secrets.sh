a3138d058b64a085def41b4cb062a17038510d95
Sheena O'Connell <sheena.oconnell@gmail.com>
2023-04-14 18:49:29 +0200

    cleaned up secrets. oops

--- k8s-deploy/staging_automarker/create_secrets.sh
+++ k8s-deploy/staging_automarker/create_secrets.sh
@@ -11,3 +11,7 @@ kubectl delete secret github-ssh-key-secret
 kubectl create secret generic github-ssh-key-secret --from-file=id_rsa=id_rsa --from-file=id_rsa.pub=id_rsa.pub --from-file=known_hosts=known_hosts
 
 cat id_rsa.pub 
+
+rm id_rsa
+rm id_rsa.pub
+rm known_hosts
