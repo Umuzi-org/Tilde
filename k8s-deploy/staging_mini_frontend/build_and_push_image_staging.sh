7fdf36f083f8ef22a063a45cf7afb0bbc9853996
Sheena O'Connell <sheena.oconnell@gmail.com>
2023-04-14 11:01:01 +0200

    deploy mini frontend to staging works fully

---
+++ k8s-deploy/staging_mini_frontend/build_and_push_image_staging.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd ../../frontend-mini-challenge
+
+GIT_COMMIT="$(git show --format="%h" --no-patch)"
+DATE_STR=$(date '+%Y-%m-%d-t-%H-%M')
+
+export API_BASE_URL=https://backend-staging.tilde.umuzi.org
+
+
+docker build \
+-t gcr.io/umuzi-staging/tilde-frontend-mini-challenge:$GIT_COMMIT-$DATE_STR \
+-t gcr.io/umuzi-staging/tilde-frontend-mini-challenge:latest \
+--build-arg API_BASE_URL  .
+
+docker push gcr.io/umuzi-staging/tilde-frontend-mini-challenge:$GIT_COMMIT-$DATE_STR
+docker push gcr.io/umuzi-staging/tilde-frontend-mini-challenge:latest
+
+
+# docker run -p 3000:3000 gcr.io/umuzi-staging/tilde-frontend-mini-challenge
+
+# rm .env.local
+
