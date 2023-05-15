8cd7c4ee8cf8799557e3de5a8437870ddcc9bd4b
Sheena O'Connell <sheena.oconnell@gmail.com>
2023-05-15 13:01:50 +0200

    grasshopper not ready for human consumption

---
+++ saturation-tester/database/reset-container-data.sh
@@ -0,0 +1,6 @@
+#!/bin/sh 
+sudo rm -rf gitignore 
+mkdir gitignore 
+mkdir gitignore/grafana_data  
+mkdir gitignore/influxdb
+chmod -R 777 gitignore
--- saturation-tester/staging_urls.sh
+++ saturation-tester/staging_urls.sh
@@ -1,3 +1,4 @@
 #!/bin/sh 
 
 export HOST_FRONTEND_MINI=https://challenge-staging.tilde.umuzi.org
+export HOST_BACKEND_API=https://backend-staging.tilde.umuzi.org/api
