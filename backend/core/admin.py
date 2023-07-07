a884b5a7afaf2ee81ba2e73ed956f8046779a884
ngoakor12 <ngoakor12@gmail.com>
2023-07-07 15:27:41 +0200

    team users deactivate in bulk

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -3,10 +3,38 @@ from . import models
 from guardian.admin import GuardedModelAdmin
 from adminsortable2.admin import SortableInlineAdminMixin
 
+from django import forms
+
+# class UserSetInline(admin.TabularInline):
+#     model = models.User.groups.through
+#     raw_id_fields = ("user",)
+
+
+class UserSetForm(forms.ModelForm):
+    active = forms.BooleanField(required=False)
+
+    class Meta:
+        model = models.User.groups.through
+        fields = ["user", "active"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.pk:
+            self.fields["active"].initial = self.instance.user.active
+
+
+class UserSetFormSet(forms.BaseInlineFormSet):
+    def save_existing(self, form, instance, commit=True):
+        instance.user.active = form.cleaned_data.get("active")
+        instance.user.save()
+        return super().save_existing(form, instance, commit)
+
 
 class UserSetInline(admin.TabularInline):
     model = models.User.groups.through
     raw_id_fields = ("user",)
+    form = UserSetForm
+    formset = UserSetFormSet
 
 
 @admin.register(models.Team)
