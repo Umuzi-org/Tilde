26f38340994961900b9fc3ac8efb9b48d3d7cd24
ngoakor12 <ngoakor12@gmail.com>
2023-08-15 16:22:35 +0200

    remove admin url from api urls

--- backend/core/api_urls.py
+++ backend/core/api_urls.py
@@ -7,9 +7,4 @@ urlpatterns = [
     path("test_logs/", views.test_logs),
     path("test_long_running_request/", views.test_long_running_request),
     path("test_kill_dramatic_worker/", views.test_kill_dramatic_worker),
-    path(
-        "admin/bulk_add_users_to_team/<int:team_id>",
-        views.bulk_add_users_to_team,
-        name="bulk_add_users_to_team",
-    ),
 ]
