45e1775b6285a596fc7e5465f10b641be61d7f4c
ngoakor12 <ngoakor12@gmail.com>
2023-08-22 13:04:04 +0200

    form validation done

--- backend/core/forms.py
+++ backend/core/forms.py
@@ -41,12 +41,22 @@ class BulkAddUsersToTeamForm(forms.Form):
     def clean(self):
         cleaned_data = super().clean()
         email_addresses_str = cleaned_data["email_addresses"]
+        if not email_addresses_str:
+            raise ValidationError(
+                f"No email addresses entered.",
+            )
+
         email_addresses = self.get_email_addresses_from_str(email_addresses_str)
         users = models.User.objects.filter(email__in=email_addresses)
+
         found_users_emails = [user.email for user in users]
         not_found_users_emails = [
             email for email in email_addresses if email not in found_users_emails
         ]
+        if not_found_users_emails:
+            raise ValidationError(
+                f"Users with the following email addresses don't exist: {', '.join(not_found_users_emails)}",
+            )
         # print(f"emails: {email_addresses}")
         # print(f"found users emails: {found_users_emails}")
         # print(f"found users: {users}")
