db0330325b18098b0dd66046643e08107ba45390
Percival Rapha <percival.rapha@gmail.com>
2023-09-28 03:21:46 +0200

    Phew

--- automarker_2/automarker_app/management/commands/check_all_configuration.py
+++ automarker_2/automarker_app/management/commands/check_all_configuration.py
@@ -1,49 +1,74 @@
-from django.core.management.base import BaseCommand
-from automarker_app.lib.marker import get_all_marker_configs
-from automarker_app.lib.constants import CONFIG_STATUS_DEBUG, CONFIG_STATUS_PRODUCTION
-from automarker_app.lib.marker import mark_project, get_steps_final_status
+import concurrent.futures
+
+from django.core.management.base import BaseCommand, CommandParser
+
+from automarker_app.lib.marker import (
+    get_all_marker_configs,
+    mark_project,
+    get_steps_final_status,
+)
+from automarker_app.lib.constants import (
+    CONFIG_STATUS_DEBUG,
+    CONFIG_STATUS_PRODUCTION,
+    STEP_STATUS_PASS,
+    STEP_STATUS_WAITING,
+)
+
 
 from ..utils import print_steps_result
-import os
-
-# System call
-os.system("")
 
 
-# Class of different styles
-class style:
-    GREEN = "\033[32m"
-    WHITE = "\033[37m"
-    RED = "\033[31m"
+DASH_SEPARATOR = "-" * 100
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument("--skip", type=int, nargs="+")
+
     def handle(self, *args, **options):
         good_configs = []
         bad_configs = []
+
+        skip_ids = options.get("skip", [])
+
         for config in get_all_marker_configs():
-            if config.status in [
-                CONFIG_STATUS_DEBUG,
-                CONFIG_STATUS_PRODUCTION,
-            ]:
-                print(style.WHITE, "\n-------------------------\n")
-                print(
-                    style.WHITE,
-                    "CHECKING:",
-                    config.title,
-                    config.flavours,
-                    config.content_item_id,
-                )
-                print(style.WHITE, "\n-------------------------\n")
-                try:
-                    steps = mark_project(
-                        content_item_id=config.content_item_id,
-                        flavours=config.flavours[0],
+            if (
+                config.status
+                in [
+                    CONFIG_STATUS_DEBUG,
+                    CONFIG_STATUS_PRODUCTION,
+                ]
+                and config.content_item_id not in skip_ids
+                and config.flavours[0][0] == "python"
+            ):
+                with concurrent.futures.ProcessPoolExecutor() as executor:
+                    _future = executor.submit(
+                        mark_project,
+                        config.content_item_id,
+                        config.flavours[0],
                         self_test=True,
                         fail_fast=True,
                     )
 
-                    if len(steps) == len(config.steps):
+                    steps = _future.result()
+
+                    if any([step.status != STEP_STATUS_PASS for step in steps]):
+                        bad_configs.append(
+                            (
+                                [
+                                    config.title,
+                                    config.flavours,
+                                    config.content_item_id,
+                                ],
+                                [
+                                    step
+                                    for step in steps
+                                    if step.status
+                                    not in [STEP_STATUS_PASS, STEP_STATUS_WAITING]
+                                ],
+                            )
+                        )
+                    else:
                         good_configs.append(
                             [
                                 config.title,
@@ -51,24 +76,21 @@ class Command(BaseCommand):
                                 config.content_item_id,
                             ]
                         )
-                    # os.system(
-                    #     f"python manage.py check_project_configuration {config.content_item_id} {config.flavours[0][0]}"
-                    # )
 
-                except Exception as e:
-                    bad_configs.append(
-                        [config.title, config.flavours, config.content_item_id, e]
-                    )
         for config in good_configs:
-            print(style.GREEN, "\n-------------------------\n")
-            print(style.GREEN, "PASS:", config)
+            print(DASH_SEPARATOR)
+            print("PASS:", config)
+        print(DASH_SEPARATOR)
 
-        for config in bad_configs:
-            print(style.RED, "\n-------------------------\n")
+        for config, failed_steps in bad_configs:
+            print(DASH_SEPARATOR)
             print(
-                style.RED,
                 "FAIL:",
                 config[:-1],
             )
+            print(DASH_SEPARATOR)
+            print(print_steps_result(failed_steps))
+
             print()
-            print(style.RED, "ERROR:", config[-1])
+            print(DASH_SEPARATOR)
+            print(DASH_SEPARATOR)
