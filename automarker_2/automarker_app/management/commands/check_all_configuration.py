58bf3177da5e268e2320f4e30f045425a34ca644
ngoakor12 <ngoakor12@gmail.com>
2023-09-30 10:42:08 +0200

    clean up output

--- automarker_2/automarker_app/management/commands/check_all_configuration.py
+++ automarker_2/automarker_app/management/commands/check_all_configuration.py
@@ -1,11 +1,8 @@
 import concurrent.futures
-
 from django.core.management.base import BaseCommand, CommandParser
-
 from automarker_app.lib.marker import (
     get_all_marker_configs,
     mark_project,
-    get_steps_final_status,
 )
 from automarker_app.lib.constants import (
     CONFIG_STATUS_DEBUG,
@@ -13,8 +10,6 @@ from automarker_app.lib.constants import (
     STEP_STATUS_PASS,
     STEP_STATUS_WAITING,
 )
-
-
 from ..utils import print_steps_result
 
 
@@ -23,13 +18,15 @@ DASH_SEPARATOR = "-" * 100
 
 class Command(BaseCommand):
     def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument("--skip", type=int, nargs="+")
+        parser.add_argument("--skip", type=int, nargs="+", required=False)
 
     def handle(self, *args, **options):
         good_configs = []
         bad_configs = []
+        skip_ids = []
 
-        skip_ids = options.get("skip", [])
+        if options is not None:
+            skip_ids = options.get("skip", [])
 
         for config in get_all_marker_configs():
             if (
@@ -49,47 +46,38 @@ class Command(BaseCommand):
                         fail_fast=True,
                     )
 
-                    steps = _future.result()
+                steps = _future.result()
 
-                    if any([step.status != STEP_STATUS_PASS for step in steps]):
-                        bad_configs.append(
-                            (
-                                [
-                                    config.title,
-                                    config.flavours,
-                                    config.content_item_id,
-                                ],
-                                [
-                                    step
-                                    for step in steps
-                                    if step.status
-                                    not in [STEP_STATUS_PASS, STEP_STATUS_WAITING]
-                                ],
-                            )
-                        )
-                    else:
-                        good_configs.append(
+                if any([step.status != STEP_STATUS_PASS for step in steps]):
+                    bad_configs.append(
+                        (
                             [
                                 config.title,
                                 config.flavours,
                                 config.content_item_id,
-                            ]
+                            ],
+                            [
+                                step
+                                for step in steps
+                                if step.status
+                                not in [STEP_STATUS_PASS, STEP_STATUS_WAITING]
+                            ],
                         )
+                    )
+                else:
+                    good_configs.append(
+                        [
+                            config.title,
+                            config.flavours,
+                            config.content_item_id,
+                        ]
+                    )
 
         for config in good_configs:
-            print(DASH_SEPARATOR)
-            print("PASS:", config)
-        print(DASH_SEPARATOR)
+            print("\n", DASH_SEPARATOR, "\n")
+            print("GOOD:", config)
 
         for config, failed_steps in bad_configs:
-            print(DASH_SEPARATOR)
-            print(
-                "FAIL:",
-                config[:-1],
-            )
-            print(DASH_SEPARATOR)
-            print(print_steps_result(failed_steps))
-
-            print()
-            print(DASH_SEPARATOR)
-            print(DASH_SEPARATOR)
+            print("\n", DASH_SEPARATOR, "\n")
+            print("BAD:", config)
+            print_steps_result(failed_steps)
