006f87d1a9e970500c5080feb811e585a1cd8077
Percival Rapha <percival.rapha@gmail.com>
2023-10-11 08:59:26 +0200

    config.nix

--- automarker_2/shell.nix
+++ automarker_2/shell.nix
@@ -9,9 +9,14 @@ pkgs.mkShell {
     pkgs.zlib
     pkgs.gcc-unwrapped.lib
     pkgs.stdenv.cc.cc.lib
+    # pkgs.cudatoolkit
+    pkgs.cudnn
   ];
 
+  # shellHook = ''
+  #   export LD_LIBRARY_PATH="${pkgs.cudatoolkit}/lib:${pkgs.cudnn}/lib:${pkgs.gcc}/lib:${pkgs.zlib}/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+  # '';
   shellHook = ''
-    export LD_LIBRARY_PATH="${pkgs.gcc}/lib:${pkgs.zlib}/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${pkgs.cudnn}/lib:${pkgs.gcc}/lib:${pkgs.zlib}/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
   '';
 }
