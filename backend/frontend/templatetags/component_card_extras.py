d5a99807b620b7ceb5064426c0b69b8c8b4ad650
ngoakor12 <ngoakor12@gmail.com>
2023-11-24 15:45:10 +0200

    more date time manipulation

--- backend/frontend/templatetags/component_card_extras.py
+++ backend/frontend/templatetags/component_card_extras.py
@@ -1,10 +1,13 @@
+# Custom template filters according to the docs here: https://docs.djangoproject.com/en/4.2/howto/custom-template-tags/
+
 from django import template
 
 register = template.Library()
 
+
+@register.filter(name="convert_git_url")
 def convert_git_url(repo_name, owner):
+    """
+    Converts a git repo's ssh url to a normal github repo url because we are already storing the ssh format.
+    """
     return f"https://github.com/{owner.replace('.','-')}/{repo_name}"
-
-
-
-register.filter("convert_git_url",convert_git_url)
