92b1e284e0521f1b6d94aa18d6bcfa1d4eccffdd
Percival <57295227+kingraphaii@users.noreply.github.com>
2023-12-14 13:02:13 +0200

    V2 login flow (#674)
    * Usr login flow cleaned up
    
    * PWD Reset
    
    * Fix: remove dangling markup + dry view code
    
    * Cleanup
    
    * Add main_base for sub-bases to inherit from
    
    * Fix: unncessarily huge diff for main_header component
    
    * Refactor frontend auth templates and styles
    
    * Update base templates + DRYer form rendering
    
    * Refactor authentication templates and add alert styles
    
    * Fix: CSRF token + form action

---
+++ backend/frontend/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.core.signing import TimestampSigner
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import SetPasswordForm, AuthenticationForm
+
+from .theme import styles
+
+
+User = get_user_model()
+
+
+class ThemedFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update(
+                {
+                    "class": styles["input_small"],
+                }
+            )
+
+
+class CustomAuthenticationForm(ThemedFormMixin, AuthenticationForm):
+    pass
+
+
+class CustomSetPasswordForm(ThemedFormMixin, SetPasswordForm):
+    pass
+
+
+class ForgotPasswordForm(ThemedFormMixin, forms.Form):
+    email = forms.EmailField()
+
+    signer = TimestampSigner(salt="password.Reset")
+
+    def user_exists(self):
+        return User.objects.filter(email=self.cleaned_data.get("email")).exists()
+
+    def _get_password_reset_token(self):
+        token = self.signer.sign(self.cleaned_data.get("email"))
+        return token
+
+    def get_password_reset_url(self):
+        return reverse_lazy(
+            "user_reset_password",
+            kwargs={"token": self._get_password_reset_token()},
+        )
