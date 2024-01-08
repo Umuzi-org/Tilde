c6bfd5655a73379b9e8fbf09314e21929c6a620d
ngoakor12 <ngoakor12@gmail.com>
2024-01-08 16:54:10 +0200

    login matches new login page

--- backend/frontend/tests/frontend_test_mixin.py
+++ backend/frontend/tests/frontend_test_mixin.py
@@ -47,8 +47,7 @@ class FrontendTestMixin(StaticLiveServerTestCase):
         logs the user in. Assumes the email address ands password are the same.
         self.page will be on the page after login
         """
-        # TODO: Update this so it works with the new login page
-        self.page.goto(f"{self.live_server_url}/admin/")
+        self.page.goto(f"{self.live_server_url}/login/")
         self.page.fill("[name=username]", user.email)
         self.page.fill("[name=password]", user.email)
         self.page.click("text=Log in")
