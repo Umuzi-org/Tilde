3e0db204be6a1bf795bce66acc9a6200194cbe76
ngoakor12 <ngoakor12@gmail.com>
2024-01-19 08:40:47 +0200

    project start test progress

--- backend/frontend/tests/test_card_start_button.py
+++ backend/frontend/tests/test_card_start_button.py
@@ -23,15 +23,18 @@ class TestCardStartButton(FrontendTestMixin):
         self.card.assignees.first().delete()
         self.card.assignees.add(self.user)
 
-    # def make_project_card(self, content_type):
-    #     self.card = AgileCardFactory(
-    #         content_item=ContentItemFactory(
-    #             content_type=content_type,
-    #             project_submission_type=ContentItem.PROJECT,
-    #         ),
-    #         status=AgileCard.READY,
-    #     )
-    #     self.card.assignees.set([self.user])
+    def make_project_card(self):
+        self.card = AgileCardFactory(
+            content_item=ContentItemFactory(
+                content_type=ContentItem.PROJECT,
+                project_submission_type=ContentItem.LINK,
+            ),
+            status=AgileCard.READY,
+        )
+
+        # replace default assignee with our user
+        self.card.assignees.first().delete()
+        self.card.assignees.add(self.user)
 
     def test_start_button_moves_topic_card_to_ip_column(self):
         self.make_topic_card()
@@ -45,19 +48,7 @@ class TestCardStartButton(FrontendTestMixin):
 
             ip_column = self.page.text_content("div#column_IP")
             backlog_column = self.page.text_content("div#column_RB")
-            topic_card_title = self.card.content_item.title
+            project_card_title = self.card.content_item.title
 
-            self.assertIn(topic_card_title, ip_column)
-            self.assertNotIn(topic_card_title, backlog_column)
-
-    # def test_start_button_moves_project_card__to_ip_column(self):
-    #     self.make_project_card(content_type=ContentItem.PROJECT)
-    #     self.make_project_card(content_type=ContentItem.LINK)
-    #     start_button = self.page.get_by_role("button").and_(
-    #         self.page.get_by_title("Start")
-    #     )
-
-    # print(self.card)
-    # print(self.backlog_column)
-    # print(self.ip_column)
-    # print(start_button)
+            self.assertIn(project_card_title, ip_column)
+            self.assertNotIn(project_card_title, backlog_column)
