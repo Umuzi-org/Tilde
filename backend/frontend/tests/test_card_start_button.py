8bd363cbf4f6c90e7416ac77e9d308c64cdcfa80
ngoakor12 <ngoakor12@gmail.com>
2024-01-21 13:13:30 +0200

    working

--- backend/frontend/tests/test_card_start_button.py
+++ backend/frontend/tests/test_card_start_button.py
@@ -1,6 +1,10 @@
 from core.tests.factories import UserFactory
-from curriculum_tracking.tests.factories import AgileCardFactory, ContentItemFactory
-from curriculum_tracking.models import ContentItem, AgileCard
+from curriculum_tracking.tests.factories import (
+    AgileCardFactory,
+    ContentItemFactory,
+    ProjectContentItemFactory,
+)
+from curriculum_tracking.models import ContentItem, AgileCard, RecruitProject
 from .frontend_test_mixin import FrontendTestMixin
 
 
@@ -19,9 +23,7 @@ class TestCardStartButton(FrontendTestMixin):
             status=AgileCard.READY,
         )
 
-        # replace default assignee with our user
-        self.card.assignees.first().delete()
-        self.card.assignees.add(self.user)
+        self.card.assignees.set([self.user])
 
     def make_project_card(self):
         self.card = AgileCardFactory(
@@ -32,14 +34,30 @@ class TestCardStartButton(FrontendTestMixin):
             status=AgileCard.READY,
         )
 
-        # replace default assignee with our user
-        self.card.assignees.first().delete()
-        self.card.assignees.add(self.user)
+        self.card.assignees.set([self.user])
 
     def test_start_button_moves_topic_card_to_ip_column(self):
         self.make_topic_card()
 
         can_start = self.card.request_user_can_start(self.user)
+
+        if can_start:
+            self.page.click("text=Start")
+
+            self.page.wait_for_load_state("networkidle")
+
+            ip_column = self.page.text_content("div#column_IP")
+            backlog_column = self.page.text_content("div#column_RB")
+            topic_card_title = self.card.content_item.title
+
+            self.assertIn(topic_card_title, ip_column)
+            self.assertNotIn(topic_card_title, backlog_column)
+
+    def test_start_button_moves_project_card_to_ip_column(self):
+        self.make_project_card()
+
+        can_start = self.card.request_user_can_start(self.user)
+        print("#can_start", can_start)
 
         if can_start:
             self.page.click("text=Start")
