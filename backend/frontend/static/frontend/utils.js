6c2819688d56c2b9dcde88c27b572587ca66720b
Percival Rapha <percival.rapha@gmail.com>
2024-02-01 15:40:20 +0200

    Remove 5,4xx error handler

--- backend/frontend/static/frontend/utils.js
+++ backend/frontend/static/frontend/utils.js
@@ -5,15 +5,3 @@ function countLoadedPageItems(parentId) {
   const parent = document.getElementById(parentId);
   return parent.childElementCount - 1; // the button is an element too
 }
-
-/*
- * Swaps in the body of error pages returned from htmx requests 
- * for example when an action is forbidden.
- */
-document.addEventListener("htmx:beforeOnLoad", function (event) {
-  const xhr = event.detail.xhr
-  if (xhr.status == 500 || xhr.status == 403 || xhr.status == 404) {
-    event.stopPropagation() // Tell htmx not to process these requests
-    document.children[0].innerHTML = xhr.response // Swap in body of response instead
-  }
-})
