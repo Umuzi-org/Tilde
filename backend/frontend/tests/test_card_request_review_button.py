37f7cc5b451552e29ab23c6f83d1ed04e0d9fedf
Percival Rapha <percival.rapha@gmail.com>
2024-02-13 09:02:20 +0200

    Add review request logging to test_card_request_review_button.py

--- backend/frontend/tests/test_card_request_review_button.py
+++ backend/frontend/tests/test_card_request_review_button.py
@@ -1,6 +1,8 @@
+from activity_log.models import LogEntry
 from core.tests.factories import UserFactory
 from curriculum_tracking.tests.factories import AgileCardFactory, ContentItemFactory
 from curriculum_tracking.models import ContentItem, AgileCard
+import curriculum_tracking.activity_log_entry_creators as creators
 from .frontend_test_mixin import FrontendTestMixin
 
 
@@ -98,3 +100,19 @@ class TestCardRequestReviewButton(FrontendTestMixin):
         self.assertIn(
             "You have outstanding card reviews", self.page.text_content("div#column_IP")
         )
+
+    def test_request_review_button_logs_review_request_event(self):
+        self.make_ip_project_card(ContentItem.LINK)
+
+        self.page.click("text=Request Review")
+
+        self.page.wait_for_load_state("networkidle")
+
+        self.assertEqual(LogEntry.objects.count(), 1)
+        entry = LogEntry.objects.first()
+
+        self.assertEqual(entry.actor_user, self.user)
+        self.assertEqual(entry.effected_user, self.card.assignees.first())
+        self.assertEqual(entry.object_1, self.card.recruit_project)
+        self.assertEqual(entry.object_2, None)
+        self.assertEqual(entry.event_type.name, creators.CARD_REVIEW_REQUESTED)
