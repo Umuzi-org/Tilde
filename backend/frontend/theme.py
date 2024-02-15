20a3fddf384044375aae186893926046b969e8d5
Sbonelo01 <sbonelomkhize15@gmail.com>
2024-02-15 20:38:16 +0200

    cleanup

--- backend/frontend/theme.py
+++ backend/frontend/theme.py
@@ -1,12 +1,11 @@
 PRIMARY_COLOUR = "indigo"
-CONTENT_BTN_COLOUR = "indigo-950"
 
 TEXT_COLOUR = "gray"
 
 styles = {
-    # button
     "button_primary_small": f"rounded bg-{PRIMARY_COLOUR}-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-{PRIMARY_COLOUR}-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-{PRIMARY_COLOUR}-600",
-    "button_content_small": f"rounded bg-{CONTENT_BTN_COLOUR} px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-{CONTENT_BTN_COLOUR} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-{CONTENT_BTN_COLOUR}",
+    #
+    #
     "button_secondary_small": f"rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600 ring-1 ring-inset ring-gray-300",
     #
     "heading1": f"text-2xl font-bold text-{TEXT_COLOUR}-600",
