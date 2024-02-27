d8b7a9bd283055b43869b6a311644c48c9bc91b4
ngoakor12 <ngoakor12@gmail.com>
2024-02-27 19:56:31 +0200

    test logging

--- backend/frontend/tests/test_card_start_button.py
+++ backend/frontend/tests/test_card_start_button.py
@@ -5,6 +5,8 @@ from curriculum_tracking.tests.factories import (
 )
 from curriculum_tracking.models import ContentItem, AgileCard
 from .frontend_test_mixin import FrontendTestMixin
+from activity_log.models import LogEntry
+import curriculum_tracking.activity_log_entry_creators as creators
 
 
 class TestCardStartButton(FrontendTestMixin):
@@ -62,3 +64,19 @@ class TestCardStartButton(FrontendTestMixin):
 
         self.assertIn(project_card_title, ip_column)
         self.assertNotIn(project_card_title, backlog_column)
+
+    def test_start_button_logs_card_started_event(self):
+        self.make_project_card(ContentItem.LINK)
+
+        self.page.click("text=Start")
+
+        self.page.wait_for_load_state("networkidle")
+
+        self.assertEqual(LogEntry.objects.count(), 1)
+        entry = LogEntry.objects.first()
+
+        self.assertEqual(entry.actor_user, self.user)
+        self.assertEqual(entry.effected_user, self.card.assignees.first())
+        self.assertEqual(entry.object_1, self.card.recruit_project)
+        self.assertEqual(entry.object_2, None)
+        self.assertEqual(entry.event_type.name, creators.CARD_STARTED)
