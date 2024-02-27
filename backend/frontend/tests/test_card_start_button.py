9cd85561f209c1ebe6b5e14557d95c12972c5fe4
ngoakor12 <ngoakor12@gmail.com>
2024-02-27 15:49:56 +0200

    tests failing

--- backend/frontend/tests/test_card_start_button.py
+++ backend/frontend/tests/test_card_start_button.py
@@ -2,9 +2,8 @@ from core.tests.factories import UserFactory
 from curriculum_tracking.tests.factories import (
     AgileCardFactory,
     ContentItemFactory,
-    ProjectContentItemFactory,
 )
-from curriculum_tracking.models import ContentItem, AgileCard, RecruitProject
+from curriculum_tracking.models import ContentItem, AgileCard
 from .frontend_test_mixin import FrontendTestMixin
 
 
@@ -39,33 +38,27 @@ class TestCardStartButton(FrontendTestMixin):
     def test_start_button_moves_topic_card_to_ip_column(self):
         self.make_topic_card()
 
-        can_start = self.card.request_user_can_start(self.user)
+        self.page.click("text=Start")
 
-        if can_start:
-            self.page.click("text=Start")
+        self.page.wait_for_load_state("networkidle")
 
-            self.page.wait_for_load_state("networkidle")
+        ip_column = self.page.text_content("div#column_IP")
+        backlog_column = self.page.text_content("div#column_RB")
+        topic_card_title = self.card.content_item.title
 
-            ip_column = self.page.text_content("div#column_IP")
-            backlog_column = self.page.text_content("div#column_RB")
-            topic_card_title = self.card.content_item.title
-
-            self.assertIn(topic_card_title, ip_column)
-            self.assertNotIn(topic_card_title, backlog_column)
+        self.assertIn(topic_card_title, ip_column)
+        self.assertNotIn(topic_card_title, backlog_column)
 
     def test_start_button_moves_project_card_to_ip_column(self):
         self.make_project_card(ContentItem.LINK)
 
-        can_start = self.card.request_user_can_start(self.user)
+        self.page.click("text=Start")
 
-        if can_start:
-            self.page.click("text=Start")
+        self.page.wait_for_load_state("networkidle")
 
-            self.page.wait_for_load_state("networkidle")
+        ip_column = self.page.text_content("div#column_IP")
+        backlog_column = self.page.text_content("div#column_RB")
+        project_card_title = self.card.content_item.title
 
-            ip_column = self.page.text_content("div#column_IP")
-            backlog_column = self.page.text_content("div#column_RB")
-            project_card_title = self.card.content_item.title
-
-            self.assertIn(project_card_title, ip_column)
-            self.assertNotIn(project_card_title, backlog_column)
+        self.assertIn(project_card_title, ip_column)
+        self.assertNotIn(project_card_title, backlog_column)
