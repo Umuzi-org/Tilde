afafdeaefdf7bd75938174805a6f1d8d02a6837a
Raymond-Mawina <mawina.projects@gmail.com>
2024-03-07 01:00:54 +0200

    added tests 3 more tests focused on the link_submission form updates

--- backend/frontend/tests/test_course_component_details_pages.py
+++ backend/frontend/tests/test_course_component_details_pages.py
@@ -34,7 +34,7 @@ class TestLinkProjectDetailsPage(FrontendTestMixin):
             email="learner_reviewer@umuzi.org",
         )
 
-        recruit_project = RecruitProjectFactory(
+        self.recruit_project = RecruitProjectFactory(
             recruit_users=[self.user],
             reviewer_users=[learner_reviewer],
             content_item=content_item,
@@ -46,17 +46,17 @@ class TestLinkProjectDetailsPage(FrontendTestMixin):
             ),
         )
 
-        AgileCardFactory(
+        self.agile_card = AgileCardFactory(
             content_item=content_item,
             status=AgileCard.IN_PROGRESS,
-            recruit_project=recruit_project,
+            recruit_project=self.recruit_project,
         )
 
     def test_link_project_page_displays_correct_details(self):
         self.make_ip_project_card(ContentItem.LINK)
 
         self.link_project_url = self.reverse_url(
-            "course_component_details", kwargs={"project_id": 1}
+            "course_component_details", kwargs={"project_id": self.recruit_project.id}
         )
         self.page.goto(self.link_project_url)
 
@@ -69,7 +69,58 @@ class TestLinkProjectDetailsPage(FrontendTestMixin):
         self.assertIn("Feb. 13, 2024, 2:06 p.m.", body)
         self.assertIn("learner_reviewer@umuzi.org", body)
         self.assertIn(
-            "https://raw.githubusercontent.com/Umuzi-org/tech-department/master/content/projects/tdd/simple-calculator/part-1/_index.md",
+            self.recruit_project.content_url,
             body,
         )
         self.assertIn("No link submitted yet", body)
+
+    def test_link_submission_form_correctly_updates_link_submission_after_link_update(
+        self,
+    ):
+        self.make_ip_project_card(ContentItem.LINK)
+
+        self.link_project_url = self.reverse_url(
+            "course_component_details", kwargs={"project_id": self.recruit_project.id}
+        )
+        self.page.goto(self.link_project_url)
+
+        body = self.page.text_content("body")
+        self.assertIn("No link submitted yet", body)
+
+        self.page.get_by_label("Submission link").fill("https://google.com")
+        self.page.click("text=Submit Link")
+
+        body = self.page.text_content("body")
+        self.assertIn("https://google.com", body)
+
+    def test_link_submission_form_displays_correct_error_message_when_form_is_submitted_with_no_input(
+        self,
+    ):
+        self.make_ip_project_card(ContentItem.LINK)
+
+        self.link_project_url = self.reverse_url(
+            "course_component_details", kwargs={"project_id": self.recruit_project.id}
+        )
+        self.page.goto(self.link_project_url)
+
+        self.page.get_by_label("Submission link").fill("")
+        self.page.click("text=Submit Link")
+
+        body = self.page.text_content("body")
+        self.assertIn("This field is required", body)
+
+    def test_link_submission_form_displays_correct_error_message_when_form_is_submitted_with_invalid_input(
+        self,
+    ):
+        self.make_ip_project_card(ContentItem.LINK)
+
+        self.link_project_url = self.reverse_url(
+            "course_component_details", kwargs={"project_id": self.recruit_project.id}
+        )
+        self.page.goto(self.link_project_url)
+
+        self.page.get_by_label("Submission link").fill("http://google")
+        self.page.click("text=Submit Link")
+
+        body = self.page.text_content("body")
+        self.assertIn("Enter a valid URL", body)
