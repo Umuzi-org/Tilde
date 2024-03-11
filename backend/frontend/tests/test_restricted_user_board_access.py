c24789bb5d3d1175a7b4701c0eb60b3004adf5fc
Percival Rapha <percival.rapha@gmail.com>
2024-03-11 19:20:55 +0200

    fix invalid emails

--- backend/frontend/tests/test_restricted_user_board_access.py
+++ backend/frontend/tests/test_restricted_user_board_access.py
@@ -10,14 +10,14 @@ class TestUserBoardAuthorization(FrontendTestMixin):
 
         self.viewed_user_team = TeamFactory()
         self.viewed_user = UserFactory(
-            email="learner.umuzi.org",
+            email="learner@umuzi.org",
         )
         self.viewed_user.set_password(self.viewed_user.email)
         self.viewed_user.save()
         self.viewed_user_team.user_set.add(self.viewed_user)
 
         self.user_without_access = UserFactory(
-            email="user_without_access.umuzi.org",
+            email="user_without_access@umuzi.org",
         )
         self.user_without_access.set_password(self.user_without_access.email)
         self.user_without_access.save()
@@ -39,7 +39,7 @@ class TestUserBoardAuthorization(FrontendTestMixin):
 
     def _setup_user_with_manage_card_permission(self):
         self.user_with_manage_card_permission = UserFactory(
-            email="user_with_manage_card_permission.umuzi.org"
+            email="user_with_manage_card_permission@umuzi.org"
         )
         self.user_with_manage_card_permission.set_password(
             self.user_with_manage_card_permission.email
@@ -53,7 +53,7 @@ class TestUserBoardAuthorization(FrontendTestMixin):
 
     def _setup_user_with_view_permission(self):
         self.user_with_view_permission = UserFactory(
-            email="user_with_view_permission.umuzi.org"
+            email="user_with_view_permission@umuzi.org"
         )
         self.user_with_view_permission.set_password(
             self.user_with_view_permission.email
@@ -67,7 +67,7 @@ class TestUserBoardAuthorization(FrontendTestMixin):
 
     def _setup_user_with_review_cards_permission(self):
         self.user_with_review_cards_permission = UserFactory(
-            email="user_with_review_cards_permission.umuzi.org"
+            email="user_with_review_cards_permission@umuzi.org"
         )
         self.user_with_review_cards_permission.set_password(
             self.user_with_review_cards_permission.email
@@ -80,7 +80,7 @@ class TestUserBoardAuthorization(FrontendTestMixin):
         )
 
     def _setup_trusted_user(self):
-        self.trusted_reviewer = UserFactory(email="trusted_reviewer.umuzi.org")
+        self.trusted_reviewer = UserFactory(email="trusted_reviewer@umuzi.org")
         self.trusted_reviewer.set_password(self.trusted_reviewer.email)
         self.trusted_reviewer.save()
         assign_perm(
