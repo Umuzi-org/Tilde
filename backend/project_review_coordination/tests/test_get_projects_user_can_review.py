555d52e0860f6415341e1d33bd011a09ea2fba24
ngoakor12 <ngoakor12@gmail.com>
2024-03-17 16:14:20 +0200

    add tests

---
+++ backend/project_review_coordination/tests/test_get_projects_user_can_review.py
@@ -0,0 +1,63 @@
+from django.test import TestCase
+from curriculum_tracking.tests.factories import (
+    AgileCardFactory,
+    ContentItemFactory,
+    RecruitProjectReviewFactory,
+)
+from core.tests.factories import UserFactory
+from curriculum_tracking.models import ContentItem, AgileCard
+from curriculum_tracking.constants import NOT_YET_COMPETENT, COMPETENT
+from django.utils import timezone
+from datetime import timedelta
+from social_auth.tests.factories import SocialProfileFactory, GithubOAuthTokenFactory
+from git_real.constants import GIT_REAL_BOT_USERNAME, GITHUB_DATETIME_FORMAT
+
+from project_review_coordination.models import ProjectReviewBundleClaim
+
+
+class get_projects_user_can_review_Tests(TestCase):
+    def setUp(self) -> None:
+        self.user = UserFactory()
+        self.project_user_has_reviewed = AgileCardFactory(
+            content_item=ContentItemFactory(
+                content_type=ContentItem.PROJECT,
+                project_submission_type=ContentItem.LINK,
+            ),
+            status=AgileCard.READY,
+        )
+
+        self.project_user_has_reviewed.start_project()
+        self.project_user_has_reviewed.recruit_project.request_review()
+
+        RecruitProjectReviewFactory(
+            status=COMPETENT,
+            recruit_project=self.project_user_has_reviewed.recruit_project,
+            reviewer_user=self.user,
+        )
+
+        projects_user_has_not_reviewed = [
+            AgileCardFactory(
+                content_item=ContentItemFactory(
+                    content_type=ContentItem.PROJECT,
+                    project_submission_type=ContentItem.LINK,
+                ),
+                status=AgileCard.READY,
+            )
+            for _ in range(2)
+        ]
+
+        for project in projects_user_has_not_reviewed:
+            project.start_project()
+            project.recruit_project.request_review()
+
+        self.projects_in_review = projects_user_has_not_reviewed + [
+            self.project_user_has_reviewed
+        ]
+
+    def test_get_projects_user_can_review_returns_the_right_projects(self):
+        projects__user_can_review = (
+            ProjectReviewBundleClaim.get_projects_user_can_review(self.user)
+        )
+
+        assert self.project_user_has_reviewed not in projects__user_can_review
+        assert len(self.projects_in_review) != len(projects__user_can_review)
