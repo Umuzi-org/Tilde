d4bbe1284e20868566807ac0271d52822219885a
Raymond-Mawina <mawina.projects@gmail.com>
2024-03-26 12:42:23 +0200

    refactored the test and changed the name

--- backend/curriculum_tracking/tests/test_TopicProgress.py
+++ backend/curriculum_tracking/tests/test_TopicProgress.py
@@ -5,6 +5,7 @@ from curriculum_tracking.tests import factories
 
 class TestTopicProgress(TestCase):
 
-    def test_duration_calculated_correctly(self):
+    def test_duration_returns_none_when_there_log_entries(self):
         topic = factories.TopicProgressFactory()
         self.assertIn("duration", dir(topic))
+        self.assertEquals(topic.duration, None)
