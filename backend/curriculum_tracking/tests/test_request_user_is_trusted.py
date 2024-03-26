03202c4587ba1c44c059facda35ffe63f14a4141
ngoakor12 <ngoakor12@gmail.com>
2024-03-26 13:40:37 +0200

    tests working

--- backend/curriculum_tracking/tests/test_request_user_is_trusted.py
+++ backend/curriculum_tracking/tests/test_request_user_is_trusted.py
@@ -8,41 +8,29 @@ class request_user_is_trusted_Tests(TestCase):
     def setUp(self):
         self.user = core_factories.UserFactory()
 
-        self.card_user_is_trusted_on_js_flavour = factories.AgileCardFactory(
+        self.card_user_is_trusted_on_js_flavours = factories.AgileCardFactory(
             status=AgileCard.IN_PROGRESS, flavours=[["javascript"]]
         )
-        self.card_user_is_trusted_on_py_flavour = factories.AgileCardFactory(
+        self.card_user_is_trusted_on_py_flavours = factories.AgileCardFactory(
             status=AgileCard.IN_PROGRESS, flavours=[["python"]]
         )
-        self.card_user_is_trusted_on_no_flavour = factories.AgileCardFactory(
+        self.card_user_is_trusted_on_no_flavours = factories.AgileCardFactory(
             status=AgileCard.IN_PROGRESS, flavours=[]
         )
 
-        trusted_cards = [
-            self.card_user_is_trusted_on_js_flavour,
-            self.card_user_is_trusted_on_py_flavour,
-            self.card_user_is_trusted_on_no_flavour,
+        self.trusted_cards = [
+            self.card_user_is_trusted_on_js_flavours,
+            self.card_user_is_trusted_on_py_flavours,
+            self.card_user_is_trusted_on_no_flavours,
         ]
 
-        for trusted_card in trusted_cards:
-            trust = factories.ReviewTrustFactory(
+        for trusted_card in self.trusted_cards:
+            factories.ReviewTrustFactory(
                 content_item=trusted_card.content_item,
                 flavours=[o.name for o in trusted_card.flavours.all()],
                 user=self.user,
             )
 
-        self.card_user_is_not_trusted_on_js_flavour = factories.AgileCardFactory(
-            status=AgileCard.IN_PROGRESS
-        )
-        self.card_user_is_not_trusted_on_py_flavour = factories.AgileCardFactory(
-            status=AgileCard.IN_PROGRESS
-        )
-        self.card_user_is_not_trusted_on_no_flavour = factories.AgileCardFactory(
-            status=AgileCard.IN_PROGRESS
-        )
-
-    def test_request_user_is_trusted_on_flavoured_cards(self):
-        pass
-
-    def test_request_user_is_trusted_on_not_flavoured_cards(self):
-        pass
+    def test_request_user_is_trusted_on_cards(self):
+        for card in self.trusted_cards:
+            self.assertTrue(card.request_user_is_trusted(self.user))
