bfe7fd279674bc35570b1aeb8c0ac2efaa259739
Raymond-Mawina <mawina.projects@gmail.com>
2024-03-26 07:56:49 +0200

    unused import in the topicprogress test

--- backend/curriculum_tracking/tests/test_TopicProgress.py
+++ backend/curriculum_tracking/tests/test_TopicProgress.py
@@ -3,9 +3,6 @@ from django.test import TestCase
 from curriculum_tracking.tests import factories
 
 
-from core.models import User
-
-
 class TestTopicProgress(TestCase):
 
     def test_duration_calculated_correctly(self):
