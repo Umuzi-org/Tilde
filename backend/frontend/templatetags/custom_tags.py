9e3ca76e8a2cfe332689b495e0666eba0468f4c5
ngoakor12 <ngoakor12@gmail.com>
2024-04-05 22:09:00 +0200

    use user initial avatar

--- backend/frontend/templatetags/custom_tags.py
+++ backend/frontend/templatetags/custom_tags.py
@@ -2,7 +2,8 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('frontend/user/board/partial_user_avatar.html')
-def user_avatar(name):
+
+@register.inclusion_tag("frontend/user/board/partial_user_avatar.html")
+def user_avatar(name, many=False):
     initial = name[0].upper()
-    return {'initial': initial}
+    return {"initial": initial, "many": many}
