e37c60d85fc891a174f994d2b14c8efa45926c84
Sheena O'Connell <sheena.oconnell@gmail.com>
2024-04-17 08:59:00 +0200

    upgraded prov accepted model

--- backend/selection_bootcamps/models.py
+++ backend/selection_bootcamps/models.py
@@ -24,4 +24,23 @@ class Bootcamp(models.Model):
 class ProvisionalGroup(models.Model):
     """This is for a team that is either provisionally accepted or bridge"""
 
+    BRIDGE = "Bridge"
+    PROVISIONALLY_ACCEPTED = "Prov accepted"
+
+    group_type_choices = [
+        (BRIDGE, BRIDGE),
+        (PROVISIONALLY_ACCEPTED, PROVISIONALLY_ACCEPTED),
+    ]
+
+    active = models.BooleanField(default=True)
+    start_date = models.DateField(blank=True, null=True)
+    stream = models.ForeignKey(
+        Stream, on_delete=models.CASCADE
+    )  # eg: Bootcamp Data Science
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    paid = models.BooleanField()
+    group_type = models.CharField(choices=group_type_choices, max_length=14)
+
+    def __str__(self):
+        return f"{self.team} [{self.stream}]"
