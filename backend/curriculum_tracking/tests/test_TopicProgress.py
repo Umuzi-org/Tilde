d79a02a40e8275f4cdc8e658946cad507bbdcd40
Raymond-Mawina <mawina.projects@gmail.com>
2024-04-17 06:42:41 +0200

    Revert "added tests for edgecases for duration tests"
    This reverts commit 750623b5b7f15a625ab7c42ef421d1e53dce78db.

--- backend/curriculum_tracking/tests/test_TopicProgress.py
+++ backend/curriculum_tracking/tests/test_TopicProgress.py
@@ -7,59 +7,17 @@ from curriculum_tracking.models import AgileCard, ContentItem
 
 
 class duration_Tests(TestCase):
-    DATETIME_NONE_TYPEERROR_MESSAGE = "TypeError: unsupported operand type(s) for -: 'datetime.datetime' and 'NoneType'"
-
-    def make_project_card(self):
-        self.card = factories.AgileCardFactory(
+    def test_returns_correct_value(self):
+        card = factories.AgileCardFactory(
             content_item=factories.ContentItemFactory(content_type=ContentItem.TOPIC),
             status=AgileCard.READY,
         )
 
-    def test_returns_correct_value(self):
-        self.make_project_card()
-
-        self.card.topic_progress.start_time = datetime(
+        card.topic_progress.start_time = datetime(
             2024, 2, 12, 14, 6, 17, 373514, tzinfo=timezone.utc
         )
-        self.card.topic_progress.end_time = datetime(
+        card.topic_progress.end_time = datetime(
             2024, 2, 12, 15, 6, 17, 373514, tzinfo=timezone.utc
         )
 
-        self.assertEquals(self.card.topic_progress.duration, timedelta(seconds=3600))
-
-    def test_raises_typeerror_when_starttime_is_empty(self):
-        self.make_project_card()
-
-        self.card.topic_progress.start_time = None
-        self.card.topic_progress.end_time = datetime(
-            2024, 2, 12, 15, 6, 17, 373514, tzinfo=timezone.utc
-        )
-
-        self.assertRaisesMessage(
-            TypeError,
-            self.DATETIME_NONE_TYPEERROR_MESSAGE,
-        )
-
-    def test_raises_typeerror_when_endtime_is_empty(self):
-        self.make_project_card()
-
-        self.card.topic_progress.start_time = datetime(
-            2024, 2, 12, 15, 6, 17, 373514, tzinfo=timezone.utc
-        )
-        self.card.topic_progress.end_time = None
-
-        self.assertRaisesMessage(
-            TypeError,
-            self.DATETIME_NONE_TYPEERROR_MESSAGE,
-        )
-
-    def test_raises_typeerror_when_both_starttime_and_endtime_are_empty(self):
-        self.make_project_card()
-
-        self.card.topic_progress.start_time = None
-        self.card.topic_progress.end_time = None
-
-        self.assertRaisesMessage(
-            TypeError,
-            self.DATETIME_NONE_TYPEERROR_MESSAGE,
-        )
+        self.assertEquals(card.topic_progress.duration, timedelta(seconds=3600))
