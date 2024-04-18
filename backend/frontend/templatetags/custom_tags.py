81c513a4200bde5a01b9ca09235fb2cd66c6e51d
Percival Rapha <percival.rapha@gmail.com>
2024-04-18 14:28:38 +0200

    Checkpoint: highlighted markdown

--- backend/frontend/templatetags/custom_tags.py
+++ backend/frontend/templatetags/custom_tags.py
@@ -1,4 +1,6 @@
 from django import template
+import markdown
+
 
 register = template.Library()
 
@@ -7,3 +9,12 @@ register = template.Library()
 def user_avatar(user):
     initial = user.email[0].upper()
     return {"initial": initial}
+
+@register.filter(name='markdownify')
+def markdownify(raw_text):
+    return markdown.markdown(raw_text.strip(), safe_mode='escape', extensions=[
+        'markdown.extensions.fenced_code',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.tables',
+        'pymdownx.inlinehilite'
+        ],).strip()
