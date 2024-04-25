75cc003d40fe2b61d4528953b3ada0be986a523e
TheMaja <majawonke@gmail.com>
2024-04-25 12:03:41 +0200

    partial user avatar moved out of the user directory

--- backend/frontend/templatetags/custom_tags.py
+++ backend/frontend/templatetags/custom_tags.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("frontend/user/partial_user_avatar.html")
+@register.inclusion_tag("frontend/partial_user_avatar.html")
 def user_avatar(user):
     initial = user.email[0].upper()
     return {"initial": initial}
