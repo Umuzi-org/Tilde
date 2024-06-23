7791404d4e469268b8b7db1adb17fe1d98210da2
Percival Rapha <percival.rapha@gmail.com>
2024-06-24 01:44:29 +0200

    remove md highlighting code

--- backend/frontend/templatetags/custom_tags.py
+++ backend/frontend/templatetags/custom_tags.py
@@ -1,5 +1,4 @@
 from django import template
-import markdown
 
 
 register = template.Library()
@@ -9,12 +8,3 @@ register = template.Library()
 def user_avatar(user):
     initial = user.email[0].upper()
     return {"initial": initial}
-
-@register.filter(name='markdownify')
-def markdownify(raw_text):
-    return markdown.markdown(raw_text.strip(), safe_mode='escape', extensions=[
-        'markdown.extensions.fenced_code',
-        'markdown.extensions.codehilite',
-        'markdown.extensions.tables',
-        'pymdownx.inlinehilite'
-        ],).strip()
