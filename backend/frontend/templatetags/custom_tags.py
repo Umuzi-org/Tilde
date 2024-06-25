ef36d3aa9d5a9755974fd5f1dd3febfe90aa7d57
Sbonelo01 <sbonelomkhize15@gmail.com>
2024-06-25 16:09:37 +0200

    reviewer list dropdown on agile card

--- backend/frontend/templatetags/custom_tags.py
+++ backend/frontend/templatetags/custom_tags.py
@@ -2,8 +2,12 @@ from django import template
 
 register = template.Library()
 
-
 @register.inclusion_tag("frontend/partial_user_avatar.html")
-def user_avatar(user):
+def user_avatar(user, size="default"):
+    size_css_classes = {
+        "default": "w-[30px] h-[30px]",
+        "small": "w-[15px] h-[15px] text-xs",
+    }
     initial = user.email[0].upper()
-    return {"initial": initial}
+    return {"initial": initial, "size": size_css_classes[size]}
+
