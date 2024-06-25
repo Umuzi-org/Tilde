6e60bed9d2110652a55850b46f2508ab75213dd9
ngoakor12 <ngoakor12@gmail.com>
2024-06-25 15:20:03 +0200

    test finding user with their github

--- backend/core/tests/test_user_search.py
+++ backend/core/tests/test_user_search.py
@@ -16,23 +16,34 @@ class TeamSearchTestCase(TestCase):
         self.lucy_chen = UserFactory(
             first_name="lucy", last_name="chen", email="lucy.c@yahoo.com", active=True
         )
+        SocialProfileFactory(user=self.lucy_chen, github_name="lucy_goosey")
 
         self.users = User.objects.all()
 
     def test_search_term_first_name_john(self):
         search_term = "jOHn"
         users = User.get_users_from_search_term(search_term, self.users)
+        self.assertTrue(len(users), 1)
         self.assertIn(self.john_nolan, users)
         self.assertNotIn(self.lucy_chen, users)
 
     def test_search_term_last_name_chen(self):
         search_term = "CHEN"
         users = User.get_users_from_search_term(search_term, self.users)
+        self.assertEqual(len(users), 1)
         self.assertIn(self.lucy_chen, users)
         self.assertNotIn(self.john_nolan, users)
 
-    def test_search_term_email_chen(self):
-        search_term = "CHEN"
+    def test_search_term_email_c_at_yahoo(self):
+        search_term = "c@yahoo"
         users = User.get_users_from_search_term(search_term, self.users)
+        self.assertEqual(len(users), 1)
+        self.assertIn(self.lucy_chen, users)
+        self.assertNotIn(self.john_nolan, users)
+
+    def test_search_term_github_lucy_goosey(self):
+        search_term = "lucy_goosey"
+        users = User.get_users_from_search_term(search_term, self.users)
+        self.assertEqual(len(users), 1)
         self.assertIn(self.lucy_chen, users)
         self.assertNotIn(self.john_nolan, users)
