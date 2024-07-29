d971c9b417fce9060deeec74968d5b12ec81bcdc
Sbonelo <37469171+Sbonelo01@users.noreply.github.com>
2024-07-29 11:45:46 +0200

    Make project and topic page look good (#763)
    * details page looks better
    
    * add see more text
    
    * add review button link fix
    
    * adding media queries
    
    * cleanup
    
    * media queries polish for the reviews section
    
    * cleanup
    
    * slight change in position
    
    * remove "if condition" on custom tags
    
    * removed trailing learner emails, made review text small and added padding to reviewers
    
    * small pading fix
    
    * added small padding to "no reviews yet" text to display when there are no reviewe
    
    * formatting fix
    
    * black

--- backend/frontend/templatetags/custom_tags.py
+++ backend/frontend/templatetags/custom_tags.py
@@ -10,9 +10,12 @@ def user_avatar(user, size="default"):
         "default": "w-[30px] h-[30px]",
         "small": "w-[15px] h-[15px] text-xs",
     }
-    initial = user.email[0].upper()
+    if hasattr(user, "email"):
+        initial = user.email[0].upper()
+    else:
+        initial =  user[0].upper()
+        
     return {"initial": initial, "size": size_css_classes[size]}
-
 
 @register.filter(name="markdownify")
 def markdownify(raw_text):
@@ -34,3 +37,53 @@ def markdownify(raw_text):
             "pymdownx.superfences",
         ],
     ).strip()
+
+@register.filter
+def match_color_with_event(event_type):
+    class_mapping = {
+        'CARD_STARTED': 'text-green-400',
+        'CARD_MOVED_TO_COMPLETE': 'text-yellow-400',
+        'CARD_REVIEW_REQUESTED': 'text-orange-400',
+        'CARD_REVIEW_CANCELLED': 'text-gray-400',
+        'CARD_MOVED_TO_REVIEW_FEEDBACK': 'text-red-400',
+        'COMPETENCE_REVIEW_DONE': 'text-blue-400',
+        'PR_REVIEWED': 'text-purple-400',
+        'GIT_PUSH': 'text-green-700',
+    }
+
+    for key, value in class_mapping.items():
+        if key in event_type:
+            return value
+    return ''
+
+@register.filter
+def match_text_with_event(event_type):
+    event_mapping = {
+        'CARD_MOVED_TO_COMPLETE': 'card moved to complete',
+        'CARD_STARTED': 'card started',
+        'COMPETENCE_REVIEW_DONE': 'competence review done',
+        'CARD_REVIEW_REQUESTED': 'card review requested',
+        'CARD_STOPPED': 'card stopped',
+        'CARD_REVIEW_REQUEST_CANCELLED': 'card review request cancelled',
+        'CARD_MOVED_TO_REVIEW_FEEDBACK': 'card moved to review feedback',
+        'PR_REVIEWED': 'PR reviewed',
+        'GIT_PUSH': 'git push',
+        'not yet competent': 'not yet competent',
+        'competent': 'competent',
+        'excellent': 'excellent',
+        'red flag': 'red flag'
+    }
+    
+    for key in event_mapping:
+        if key in event_type:
+            return event_mapping[key]
+    
+    return ''
+
+@register.filter
+def display_trusted_reviewer_icon(event_type):
+    if 'trusted=True' in event_type:
+        return 'fa-solid fa-circle-check'
+    if 'trusted=False' in event_type:
+        return 'fa-regular fa-circle-check'
+    return ''
