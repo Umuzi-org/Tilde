90262d63af540a46e0e03ae6dd551a3437836f1b
ngoakor12 <ngoakor12@gmail.com>
2024-08-04 19:39:51 +0200

    link nav data structure mostly there

--- backend/frontend/navigation.py
+++ backend/frontend/navigation.py
@@ -1,18 +1,60 @@
 navigation = {
-    "user": {"url": "", "permissions": []},
-    "team": {"url": "", "permissions": []},
+    "users_and_teams_nav": {},
+    "user_board": {
+        "user_board":{
+            "url_name": "user_board",
+            "label": "Board",
+            "permissions": [],
+        },
+        "review_perfomance":{
+            "url_name": "user_board",
+            "label": "Review perfomance",
+            "permissions": [],
+        },
+    },
+    "user_review_trust_list": {},
+    "team_dashboard": {
+        "team_dashboard":{
+            "url_name": "team_dashboard",
+            "label": "Dashboard",
+            "permissions": [],
+        },
+        "card_summary":{
+            "url_name": "team_dashboard",
+            "label": "Card summary",
+            "permissions": [],
+        },
+    },
+    "view_partial_team_user_progress_chart": {},
+    "project_review_coordination": {
+        "project_review_coordination_unclaimed": {
+            "url_name": "project_review_coordination_unclaimed",
+            "label": "Unclaimed bundles",
+            "permissions": [],
+        },
+        "project_review_coordination_my_claims": {
+            "url_name": "project_review_coordination_my_claims",
+            "label": "My claims",
+            "permissions": [],
+        },
+        "project_review_coordination_all_claims": {
+            "url_name": "project_review_coordination_all_claims",
+            "label": "All claims",
+            "permissions": [],
+        },
+    },
+    "dashboard_project_review_health": {},
 }
-
 
 """
 MAIN TASKS
-- create data structure - not done
+- create data structure - mostly done
 - simplify code using data structure - not done
 - active links should get highlighted - not done
   - main nav links highlights - done
   - secondary nav links highlights - done
   - active parent links should also be highlighted when instance link is active - not done
-  - will need to pass in a list of links for each nav header to keep things dry - not done
+  - will need to pass in a list of links for each nav header to keep things dry - done
 
 Potential new tasks:
 - Have instance overview pages. E.g user profile with all info about user at /user/<id>. Same with team.
